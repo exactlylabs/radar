5e115498830dede77b691d1ef6ce7b19a29a1aa9
Michael M. Wasser <untoldone@users.noreply.github.com>
2021-11-22 16:07:52 -0800

    switch to leaflet js

--- Gemfile
+++ Gemfile
@@ -58,7 +58,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'pundit'
 gem 'geocoder'
-gem 'leaflet-rails'
 gem 'sshkey'
 gem 'google-cloud-storage'
 
--- config/initializers/leaflet.rb
+++
@@ -1,3 +0,0 @@
-Leaflet.tile_layer = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-Leaflet.attribution = "&copy; <a href=\"https://openstreetmap.org/copyright\">OpenStreetMap contributors</a>"
-Leaflet.max_zoom = 18
