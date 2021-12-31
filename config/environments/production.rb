1223a6168c5541e450536e2e82ea8e552235d927
Michael M. Wasser <untoldone@users.noreply.github.com>
2021-12-31 10:54:55 -0800

    adding default url for action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,10 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true
   }
+  config.action_mailer.default_url_options = {
+    host: 'radar.exactlylabs.com',
+    protocol: 'https'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
