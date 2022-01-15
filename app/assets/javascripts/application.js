e4507c80e8507f4ab51ec8f1fd55a51a11e54202
Michael M. Wasser <untoldone@users.noreply.github.com>
2022-01-14 16:31:18 -0800

    adding pod wip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,10 +5,28 @@
 *= require scripts.bundle
 */
 
+var initSelect2 = function () {
+  var elements = [].slice.call(document.querySelectorAll('[data-control="select2"], [data-kt-select2="true"]'));
+
+  elements.map(function (element) {
+    var options = {
+      dir: document.body.getAttribute('direction')
+    };
+
+    if (element.getAttribute('data-hide-search') == 'true') {
+      options.minimumResultsForSearch = Infinity;
+    }
+
+    $(element).select2(options);
+  });
+}
+
 function initialize() {
   KTMenu.init();
   KTToggle.init();
+  initSelect2();
 }
 
 document.addEventListener("DOMContentLoaded", initialize);
 document.addEventListener("turbo:load", initialize);
+document.addEventListener("turbo:frame-load", initialize);
