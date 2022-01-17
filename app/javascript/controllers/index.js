69a7945e042dac5f8620646847108d8e40e7a0cf
Michael M. Wasser <untoldone@users.noreply.github.com>
2022-01-17 15:23:35 -0800

    styling measurements

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
+import ClientsController from "./clients_controller.js"
+application.register("clients", ClientsController)
+
+import HelloController from "./hello_controller.js"
 application.register("hello", HelloController)
