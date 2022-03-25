28eac541a6af9a3f8d0d5a8385933e71b08285e8
Rafael Mendes <rafael@exactlylabs.com>
2022-03-25 13:58:15 -0300

    bulk_load is now configurable to set the database name as well as the path where to load the files

--- backend/README.md
+++ backend/README.md
@@ -2,3 +2,17 @@ example of creating shape files:
 
 # Create GeoJSON simplified GeoJSON file from US Tribal Tracts
 ogr2ogr -f GeoJSON output.geojson tl_2021_us_ttract.shp -simplify 0.0001
+
+
+### Setup
+
+1. Install [clickhouse](https://clickhouse.com/#quick-start) database
+
+2. Create a new Database or use the default one:
+    * `clickhouse-client --query "CREATE DATABASE mlab"`
+
+3. Run all migrations:
+    * backend/scripts/run_migrations.sh --database mlab
+
+4. Then run backend/run bulk_load.sh
+
--- backend/bulk_load.sh
+++ backend/bulk_load.sh
@@ -1,19 +1,73 @@
 #!/bin/bash
 
-FILES=$(find ../../mlab-processor/output/geocode/*  -printf "%f\n")
+set -e
 
-clickhouse-client --query="alter table tests DELETE WHERE id is not null"
+function usage() {
+echo """
+Usage: bulk_load.sh [options] files_path
+
+Required Arguments:
+
+  files_path: path to the output directory containing the expected avro files
+
+Optional Arguments:
+
+  -d or --database <db_name> : Database name to connect to. [default: default]
+"""
+}
+
+while :; do
+    case $1 in
+        -h|--help)
+            usage
+            exit 0
+        ;;
+        -d|--database) 
+            if [ "$2" ]; then
+                database=$2
+                shift
+            else
+                echo 'ERROR: --database requires a non-empty option argument'
+                exit 1
+            fi
+        ;;
+        *) break
+    esac
+    shift
+done
+
+if [ -z "$1" ]; then
+  echo "ERROR: this script expects one required argument: files_path"
+  usage
+  exit 1
+else
+  filespath=$1
+fi
+
+database=${database:-default}
+
+FILES=$(find $filespath/geocode/*.avro  -printf "%f\n")
+
+clickhouse-client  --database $database --query="alter table tests DELETE WHERE id is not null"
 
 for file in $FILES
 do
-  cat ../../mlab-processor/output/geocode/$file | clickhouse-client --query="INSERT INTO tests FORMAT Parquet"
+  echo -ne "Inserting geocode/$file... "
+  cat $filespath/geocode/$file | clickhouse-client --database $database --query="INSERT INTO tests FORMAT Avro"
+  echo -ne "\033[0;32mOK\033[0m"
+  echo
 done
 
-FILES=$(find ../../mlab-processor/output/reverse/*  -printf "%f\n")
+FILES=$(find $filespath/reverse/*.avro  -printf "%f\n")
 
-clickhouse-client --query="alter table test_geos DELETE WHERE id is not null"
+clickhouse-client --database $database --query="alter table test_geos DELETE WHERE id is not null"
 
 for file in $FILES
 do
-  cat ../../mlab-processor/output/reverse/$file | clickhouse-client --query="INSERT INTO test_geos FORMAT Parquet"
+  echo -ne "Inserting reverse/$file... "
+  cat $filespath/reverse/$file | clickhouse-client --database $database --query="INSERT INTO test_geos FORMAT Avro"
+  echo -ne "\033[0;32mOK\033[0m"
+  echo
 done
+
+echo All Done :)
