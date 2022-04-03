1624e3486ada3e8848260422801ca6f3dafa91b8
Michael M. Wasser <untoldone@users.noreply.github.com>
2022-04-02 20:48:18 -0700

    adding expected upload / download speed to locations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_07_222439) do
+ActiveRecord::Schema.define(version: 2022_04_03_033946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 2022_03_07_222439) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "expected_mbps_up"
+    t.decimal "expected_mbps_down"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
