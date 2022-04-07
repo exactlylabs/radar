a5ab0b2821ada17014625a96b7d80bea30a2702d
Rafael Mendes <rafael@exactlylabs.com>
2022-04-07 17:10:22 -0300

    modified run_migrations script to use the memory-related configurations

--- backend/scripts/run_migrations.sh
+++ backend/scripts/run_migrations.sh
@@ -26,7 +26,12 @@ FILES=$(find $SCRIPT_DIR/../migrations/*.sql -printf "%f\n")
 for file in $FILES
 do
     echo -ne "$\033[0mApplying $file...\r"
-    cat $SCRIPT_DIR/../migrations/$file | clickhouse-client --database ${database:-default}
+    cat $SCRIPT_DIR/../migrations/$file | clickhouse-client \
+        --max_memory_usage=16000000000 \
+        --join_algorithm='partial_merge' \
+        --max_bytes_before_external_group_by=5000000000 \
+        --database=${database:-default}
+        
     echo -ne "Applying $file... \033[0;32mOK\r"
     echo
 done
