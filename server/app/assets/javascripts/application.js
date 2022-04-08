e9533301c70866fd25b27b0643da2d3b2580a501
Rafael Mendes <rafael@exactlylabs.com>
2022-04-08 13:43:23 -0300

    moved all files/directories except setupscripts to a server directory

--- server/app/assets/javascripts/application.js
+++ server/app/assets/javascripts/application.js
@@ -5,11 +5,33 @@
 *= require scripts.bundle
 */
 
+var initSelect2 = function () {
+  var elements = [].slice.call(document.querySelectorAll('[data-control="select2"], [data-kt-select2="true"]'));
+
+  elements.map(function (element) {
+    var options = {
+      dir: document.body.getAttribute('direction')
+    };
+
+    if (element.getAttribute('data-hide-search') == 'true') {
+      options.minimumResultsForSearch = Infinity;
+    }
+
+    $(element).select2(options);
+  });
+}
+
 function initialize() {
   KTMenu.initGlobalHandlers();
   KTToggle.init();
+  initSelect2();
   KTApp.initBootstrapTooltips();
+}
+
+function frameInit() {
+  initSelect2();
 }
 
 document.addEventListener("DOMContentLoaded", initialize);
 document.addEventListener("turbo:load", initialize);
+document.addEventListener("turbo:frame-load", frameInit);
