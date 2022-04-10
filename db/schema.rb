881178f8dbd62abfd84833731696f1bdf67f7e68
Michael M. Wasser <untoldone@users.noreply.github.com>
2022-04-09 17:51:01 -0700

    adding devise lockable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_03_033946) do
+ActiveRecord::Schema.define(version: 2022_04_10_004851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,9 @@ ActiveRecord::Schema.define(version: 2022_04_03_033946) do
     t.string "last_name"
     t.boolean "terms"
     t.boolean "superuser", default: false
+    t.integer "failed_attempts", default: 0
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
