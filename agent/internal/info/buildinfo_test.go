eee0a0c1cf7812c8e989392f9f9cd160d7e55da9
Rafael Mendes <rafael@exactlylabs.com>
2022-04-21 21:14:34 -0300

    version information package

---
+++ agent/internal/info/buildinfo_test.go
@@ -0,0 +1,51 @@
+package info
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestInfoGetMajor(t *testing.T) {
+	i := &Info{
+		Version: "1.0.0",
+	}
+	if i.Major() != 1 {
+		t.Fatalf("expected 1, got %v", i.Major())
+	}
+}
+
+func TestInfoGetMajorMultipleDigits(t *testing.T) {
+	i := &Info{
+		Version: "12.0.0",
+	}
+	if i.Major() != 12 {
+		t.Fatalf("expected 12, got %v", i.Major())
+	}
+}
+
+func TestInfoGetMinor(t *testing.T) {
+	i := Info{
+		Version: "1.4.2",
+	}
+	if i.Minor() != 4 {
+		t.Fatalf("expected 4, got %v", i.Minor())
+	}
+}
+
+func TestInfoGetPatch(t *testing.T) {
+	i := Info{
+		Version: "1.4.2",
+	}
+	if i.Patch() != 2 {
+		t.Fatalf("expected 2, got %v", i.Patch())
+	}
+}
+
+func TestBuildTime(t *testing.T) {
+	expected := time.Date(2022, 2, 10, 0, 0, 0, 0, time.UTC)
+	var bt buildTime = buildTime(fmt.Sprintf("%d", expected.Unix()))
+	if bt.Time() != expected {
+		t.Fatalf("expected %v, got %v", expected, bt.Time())
+	}
+}
