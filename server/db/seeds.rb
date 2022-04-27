789b2953d544014b41c2ea1593fc1ebb3e170fce
Rafael Mendes <rafael@exactlylabs.com>
2022-04-27 17:20:22 -0300

    added seeds to create Default Group and add it to all clients that doesn't have an update_group already

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create, if not exists, the Default Group and add it to any client that has no group
+UpdateGroup.where(:name => "Default Group").first_or_create do |group|
+    Client.where(update_group: nil).each do |client|
+        client.update_group = group
+        client.save
+    end
+end
