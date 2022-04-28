d791378e7b76f70cff7de54a2bf10ee7cdc3797e
Rafael Mendes <rafael@exactlylabs.com>
2022-04-28 19:13:51 -0300

    merged all migrations into single one

--- server/db/migrate/20220420232112_create_client_versions.rb
+++ server/db/migrate/20220420232112_create_client_versions.rb
@@ -1,5 +1,6 @@
 class CreateClientVersions < ActiveRecord::Migration[6.1]
   def change
+    # ClientVersion model
     create_table :client_versions do |t|
       t.string :version, index: { unique: true }
 
@@ -8,5 +9,17 @@ class CreateClientVersions < ActiveRecord::Migration[6.1]
 
     add_reference :clients, :client_version, foreign_key: true
     add_column :clients, :raw_version, :string, null: true, default: nil
+
+    # UpdateGroup model
+    create_table :update_groups do |t|
+      t.string :name
+      t.references :client_version, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_reference :clients, :update_group, foreign_key: true
+
+    
   end
 end
