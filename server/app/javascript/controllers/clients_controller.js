c4c1c541667852f6612b6715d6651166822eaa92
Rafael Mendes <rafael@exactlylabs.com>
2022-05-05 16:20:14 -0300

    Client js controller to handle clipboard clicks and show a success message

--- server/app/javascript/controllers/clients_controller.js
+++ server/app/javascript/controllers/clients_controller.js
@@ -1,7 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
-
 // Connects to data-controller="clients"
 export default class extends Controller {
-  connect() {
+  static targets = [ "message" ]
+  
+  handleClipboardClick(e) {
+    navigator.clipboard.writeText(e.params["value"]);
+    this.messageTarget.classList.add("fade-out")
+  }
+
+  messageTargetConnected(obj) {
+    obj.classList.add("clipboard-message")
+    obj.onanimationend = function handleFadeOutFinished(e) {
+      e.target.classList.remove("fade-out")
+    }
   }
 }
