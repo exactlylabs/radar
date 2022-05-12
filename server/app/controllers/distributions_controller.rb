6c1930c30d92cebacac5a14988465453a76060a8
Rafael Mendes <rafael@exactlylabs.com>
2022-05-12 20:24:28 -0300

    latest_version flag added to client_version and new controllers for client_versions and distributions, for downloading the latest version

---
+++ server/app/controllers/distributions_controller.rb
@@ -0,0 +1,31 @@
+class DistributionsController < ApplicationController
+    before_action :set_version!, only: %i[ download ]
+
+    # GET /client-versions/:client_version_id/distributions/:id/download
+    def download
+        @dist = @version.distribution_by_name(params[:id])
+        if !@dist
+            head(404)
+        else
+            response.headers["Content-Type"] = @dist.signed_binary.content_type
+            response.headers["Content-Disposition"] = "attachment; #{@dist.binary.filename}"
+
+            @dist.signed_binary.download do |chunk|
+                response.stream.write(chunk)
+            end
+        end
+    ensure
+        response.stream.close
+    end
+
+
+    private
+
+    def set_version!
+        if params[:client_version_id] == "stable"
+            @version = ClientVersion.stable
+        else
+            @version = ClientVersion.find(params[:client_version_id])
+        end
+    end
+end
