e8aaac2aca6b91ef328775232e5a9f6451db0d21
Rafael Mendes <rafael@exactlylabs.com>
2022-05-12 21:02:29 -0300

    route to distribution download

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -58,6 +58,14 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :client_versions do
+    resources :distributions do
+      member do
+        get 'download'
+      end
+    end
+  end
+
   post 'geocode', to: 'geocode#code'
   get 'dashboard', to: 'dashboard#index'
   
