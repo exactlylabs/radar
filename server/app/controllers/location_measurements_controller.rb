e36afd274a5d59212e323f6c0bda7dba03356212
iuke <edamm@itba.edu.ar>
2022-05-16 09:10:50 -0300

    improving measurements table

--- server/app/controllers/location_measurements_controller.rb
+++ server/app/controllers/location_measurements_controller.rb
@@ -6,8 +6,18 @@ class LocationMeasurementsController < ApplicationController
 
   # GET /measurements or /measurements.json
   def index
-    @measurements = @location.measurements.order(created_at: :desc).then(&paginate)
-    @total = @location.measurements.length
+    @style = params[:style]
+    @range = get_date_range(params[:range])
+    if @style.present? and @range.present?
+      @measurements = @location.measurements.where(style: @style).where(created_at: @range[0]..@range[1]).order(created_at: :desc).then(&paginate)
+    elsif @range.present?
+      @measurements = @location.measurements.where(created_at: @range[0]..@range[1]).order(created_at: :desc).then(&paginate)
+    elsif @style.present?
+      @measurements = @location.measurements.where(style: @style).order(created_at: :desc).then(&paginate)
+    else
+      @measurements = @location.measurements.order(created_at: :desc).then(&paginate)
+    end
+    @total = @measurements.length
     puts @total
     respond_to do |format|
       format.html { render "index", locals: { measurements: @measurements, total: @total } }
@@ -24,5 +34,20 @@ class LocationMeasurementsController < ApplicationController
   private
     def set_location
       @location = current_user.locations.find(params[:location_id])
+    end
+
+    def get_date_range(range)
+      case range
+      when 'last-week'
+        [Time.now - 7.day, Time.now]
+      when 'last-month'
+        [Time.now - 30.day, Time.now]
+      when 'last-six-months'
+        [Time.now - 180.day, Time.now]
+      when 'last-year'
+        [Time.now - 365.day, Time.now]
+      else
+        [Date.today]
+      end
     end
 end
