1ee9ae9d2a76e04893e6010ccdcc8023246c85ab
Rafael Mendes <rafael@exactlylabs.com>
2022-05-24 20:31:52 -0300

    new API controllers to expose a REST api. Currently for client_versions and distributions

---
+++ server/app/controllers/api/v1/distributions_controller.rb
@@ -0,0 +1,49 @@
+module Api
+  module V1
+    class DistributionsController < ApiController
+      before_action :set_version!
+      def index
+        @distributions = @version.distributions
+      end
+
+      def show
+        @distribution = Distribution.find(params[:id])
+      end
+
+      def create
+        @distribution = Distribution.new distribution_params
+        @distribution.client_version = @version
+        if @distribution.save
+          render :show, status: :created
+        else
+          render_error_for @distribution
+        end
+      end
+
+      def update
+        @distribution = Distribution.find(params[:id])
+        if @distribution.update distribution_params
+          render :show, status: :ok
+        else
+          render_error_for @distribution
+        end
+      end
+
+      private
+  
+      def set_version!
+        p params
+        if params[:client_version_id] == "stable"
+          @version = ClientVersion.stable
+        else
+          @version = ClientVersion.find(params[:client_version_id])
+        end
+      end
+
+      def distribution_params
+        params.require(:distribution).permit(:name, :binary, :signed_binary)
+      end
+
+    end
+  end
+end
