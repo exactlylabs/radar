7697cab6a46493795106d8fb8cf312670c227c46
Rafael Mendes <rafael@exactlylabs.com>
2022-05-24 17:00:51 -0300

    included base_url into FipsGeocoderCli module

--- server/lib/fips/fips_geocoder.rb
+++ server/lib/fips/fips_geocoder.rb
@@ -1,6 +1,12 @@
 module FipsGeocoderCli
+  @base_url = "http://127.0.0.1:5000"
+
+  def self.base_url=(url)
+    @base_url = url
+  end
+
   def self.get_fips_codes(lat, long)
-    uri = URI("#{Rails.configuration.fips_geocoder_url}/api/v1/fips")
+    uri = URI("#{@base_url}/api/v1/fips")
     params = {"latitude": lat, "longitude": long}
     uri.query = URI.encode_www_form(params)
     begin
