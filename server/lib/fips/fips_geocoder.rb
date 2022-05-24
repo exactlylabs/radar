05292e50a05cea9640d121e87b6064c975bdad36
Rafael Mendes <rafael@exactlylabs.com>
2022-05-24 14:22:30 -0300

    FipsGeocoderCli helper

---
+++ server/lib/fips/fips_geocoder.rb
@@ -0,0 +1,19 @@
+module FipsGeocoderCli
+  def self.get_fips_codes(lat, long)
+    uri = URI("#{Rails.configuration.fips_geocoder_url}/api/v1/fips")
+    params = {"latitude": lat, "longitude": long}
+    uri.query = URI.encode_www_form(params)
+    begin
+      res = Net::HTTP.get_response(uri)
+    rescue
+      return nil, nil
+    end
+    if res.is_a?(Net::HTTPSuccess)
+      data = JSON.parse(res.body)
+      state_fips = data["state_fips"] != "" ? data["state_fips"] : nil
+      county_fips = data["county_fips"] != "" ? data["county_fips"] : nil
+      return state_fips, county_fips
+    end
+    return nil, nil
+  end
+end
