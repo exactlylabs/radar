b7905a284622f05c06f5fbff9becf55b16a97766
Rafael Mendes <rafael@exactlylabs.com>
2022-05-25 18:24:44 -0300

    new ci_scripts folder with create_version

---
+++ ci_scripts/create_version.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+AUTH="Basic $(echo -n $RADAR_TEST_USER:$RADAR_TEST_PASSWORD | base64)"
+STATUS_CODE=$(curl -s -X POST \
+  -H 'Accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: '"$AUTH"'' \
+  -d '{"version": "'"$VERSION"'"}' \
+  --output /dev/stderr \
+  --write-out '%{http_code}' \
+  $RADAR_URL/api/v1/client_versions)
+
+
+if [[ $STATUS_CODE -ne 201 && $STATUS_CODE -ne 303 ]]; then
+  echo "request failed with status: $STATUS_CODE"
+  exit 1
+fi
