c536c052c0c57c9d7fcebc5a6bcf05b56d68fda8
Rafael Mendes <rafael@exactlylabs.com>
2022-05-25 19:55:43 -0300

    checking status code from upload, to fail job in case wrong status is returned

--- ci_scripts/upload.sh
+++ ci_scripts/upload.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 AUTH="Basic $(echo -n $RADAR_TEST_USER:$RADAR_TEST_PASSWORD | base64)"
-curl -XPOST \
+STATUS_CODE=$(curl -XPOST \
   -H 'Accept: application/json' \
   -H 'Authorization: '"$AUTH"'' \
   -F distribution[binary]=@/tmp/dist/$BIN_NAME \
   -F distribution[signed_binary]=@/tmp/dist/${BIN_NAME}_signed \
   -F distribution[name]=$DIST_NAME \
-  $RADAR_URL/api/v1/client_versions/$VERSION/distributions
+  --output /dev/stderr \
+  --write-out '%{http_code}' \
+  $RADAR_URL/api/v1/client_versions/$VERSION/distributions)
+
+if [[ $STATUS_CODE -ne 201 && $STATUS_CODE -ne 303 ]]; then
+  echo "request failed with status: $STATUS_CODE"
+  exit 1
+fi
