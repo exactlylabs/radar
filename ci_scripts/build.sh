4809cfa4db89df939180f69838bc973c2662613f
Rafael Mendes <rafael@exactlylabs.com>
2022-05-25 18:54:09 -0300

    new ci script build.sh

---
+++ ci_scripts/build.sh
@@ -0,0 +1,9 @@
+cd agent
+echo -n $PROVIDER_SIGNING_CERT | base64 -d > binCert.crt
+echo -n $PROVIDER_SIGNING_KEY | base 64 -d > binKey.key
+echo -n $ROOT_CA | base64 -d > internal/update/rootCA.pem
+./scripts/build_and_sign.sh \
+            --os $OS \
+            --arch $ARCH \
+            -o dist/$BIN_NAME \
+            $VERSION binCert.crt binKey.key
