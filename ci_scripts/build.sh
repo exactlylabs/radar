bc4959009eb65f2bebe5491aaed24116ae1f9389
Rafael Mendes <rafael@exactlylabs.com>
2022-05-25 21:57:34 -0300

    updated ci_scripts to add .exe to the binary name in case of windows OS

--- ci_scripts/build.sh
+++ ci_scripts/build.sh
@@ -1,7 +1,17 @@
+#!/usr/bin/env bash
+
+
 cd agent
 echo -n $PROVIDER_SIGNING_CERT | base64 -d > binCert.crt
 echo -n $PROVIDER_SIGNING_KEY | base64 -d > binKey.key
 echo -n $ROOT_CA | base64 -d > internal/update/rootCA.pem
+
+if [ "$OS" = "windows" ]; then
+  if [[ ! "$BIN_NAME" == *.exe ]]; then
+    BIN_NAME="${BIN_NAME}.exe"
+  fi
+fi
+
 ./scripts/build_and_sign.sh \
             --os $OS \
             --arch $ARCH \
