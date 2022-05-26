86731c6e6eb9031f97350261221e26fba59c9f8e
Rafael Mendes <rafael@exactlylabs.com>
2022-05-26 20:45:23 -0300

    upload_distribution script

---
+++ scripts/upload_distribution.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [[ "$DIST_NAME" == windows-* ]]; then
+  if [[ ! "$BIN_NAME" == *.exe ]]; then
+    BIN_NAME="${BIN_NAME}.exe"
+  fi
+fi
+
+AUTH="Basic $(echo -n $RADAR_USER:$RADAR_PASSWORD | base64)"
+STATUS_CODE=$(curl -s -XPOST \
+  -H 'Accept: application/json' \
+  -H 'Authorization: '"$AUTH"'' \
+  -F distribution[binary]=@agent/dist/$BIN_NAME \
+  -F distribution[signed_binary]=@agent/dist/${BIN_NAME}_signed \
+  -F distribution[name]=$DIST_NAME \
+  --output /dev/stderr \
+  --write-out '%{http_code}' \
+  $RADAR_URL/api/v1/client_versions/$VERSION/distributions)
+
+if [[ $STATUS_CODE -ne 201 && $STATUS_CODE -ne 303 ]]; then
+  echo "request failed with status: $STATUS_CODE"
+  exit 1
+fi
