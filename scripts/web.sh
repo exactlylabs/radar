3991ace502e4f1885d8e495bc617177dda1f0511
Rafael Mendes <rafael@exactlylabs.com>
2022-06-02 13:49:54 -0300

    removed line from web.sh that forgot to remove on the PR

--- scripts/web.sh
+++ scripts/web.sh
@@ -3,8 +3,6 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# If using RVM, we need to source in the script, due to it not working when running through a script.
-[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 
 cd $SCRIPT_DIR/../server
 
