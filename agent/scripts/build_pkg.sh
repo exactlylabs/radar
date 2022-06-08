b243905d92f7cf976c48076e5a14c6bd181db59a
Rafael Mendes <rafael@exactlylabs.com>
2022-06-08 12:17:57 -0700

    mac pkg build script

---
+++ agent/scripts/build_pkg.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# Commands and files took from: https://vincent.bernat.ch/en/blog/2013-autoconf-osx-packaging
+
+set -e
+
+# Builds MacOS .pkg file
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PACKAGE_DIR=$SCRIPT_DIR/../packaging/mac
+DESTDIR=$PACKAGE_DIR/_build
+OUTPUT_DIR=$SCRIPT_DIR/..
+
+rm -rf $DESTDIR
+
+mkdir -p $DESTDIR
+
+# Call build for the specified package from the environment variables
+make build BIN_NAME=radar-agent OUTPUT_DIR=$DESTDIR/pkg/usr/local/bin
+
+# Copy launchd file
+mkdir -p $DESTDIR/pkg/Library/LaunchDaemons/
+cp $PACKAGE_DIR/com.exactlylabs.radar.agent.plist $DESTDIR/pkg/Library/LaunchDaemons/
+
+cp -r $PACKAGE_DIR/resources $DESTDIR
+cp -r $PACKAGE_DIR/scripts $DESTDIR
+VERSION=${VERSION} \
+envsubst < $PACKAGE_DIR/distribution.xml > $DESTDIR/distribution.xml
+
+# Generate the bundle with all files to be copied
+pkgbuild \
+    --root $DESTDIR/pkg \
+    --identifier com.exactlylabs.radar.agent \
+    --version ${VERSION} \
+    --scripts $DESTDIR/scripts \
+    $DESTDIR/agent_pkg.pkg 
+
+
+
+# Now, build the product, with build scripts + resources, as the final .pkg file
+
+productbuild \
+    --distribution $DESTDIR/distribution.xml \
+    --resources $DESTDIR/resources \
+    --package-path $DESTDIR \
+    --version ${VERSION} \
+    $OUTPUT_DIR/radar-agent_${VERSION}.pkg
+
+rm -rf $DESTDIR
