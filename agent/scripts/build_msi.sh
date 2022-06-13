241b0aea4e4ac83f3144643ccfe2bf039f2995b8
Rafael Mendes <rafael@exactlylabs.com>
2022-06-13 14:00:56 -0300

    windows packaging

---
+++ agent/scripts/build_msi.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Creates a .msi package for windows
+# Requires: msitools
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PACKAGE_DIR=$SCRIPT_DIR/../packaging/windows
+DESTDIR=$PACKAGE_DIR/_build
+OUTPUT_DIR=$SCRIPT_DIR/..
+
+if [ -z $VERSION ]; then
+  VERSION="1.0.0"
+fi
+
+# We only support amd64 for windows, due to ookla
+make build VERSION=$VERSION OS=windows ARCH=amd64 BIN_NAME=radar-agent.exe OUTPUT_DIR=$DESTDIR
+
+mkdir -p $DESTDIR
+
+cp $PACKAGE_DIR/RadarAgent.wxs $DESTDIR
+sed -i 's/${VERSION}/'$VERSION'/g' $DESTDIR/RadarAgent.wxs
+
+# Now generate the .msi file
+wixl -a x64 -o $OUTPUT_DIR/RadarAgent.msi $DESTDIR/RadarAgent.wxs
+
+echo "Generated msi file at $OUTPUT_DIR/RadarAgent.msi"
