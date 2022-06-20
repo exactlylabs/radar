80bc0773dd3c6a27b70eec3dac10187ddaa30ba5
Rafael Mendes <rafael@exactlylabs.com>
2022-06-20 19:42:20 -0300

    process_measurement_job stores the total downloaded/uploaded bytes

--- server/app/jobs/process_measurement_job.rb
+++ server/app/jobs/process_measurement_job.rb
@@ -6,10 +6,19 @@ class ProcessMeasurementJob < ApplicationJob
     when "NDT7"
       data = measurement.result.download.split("\n")
       extended_info = nil
+      download_total_bytes = 0
+      upload_total_bytes = 0
+
       data.reverse.each do |line|
         if line.include?("measurement") && line.include?("TCPInfo")
           last_download = JSON.parse(line)
           extended_info = last_download["Value"]["TCPInfo"]
+        elsif line.include?("measurement") && line.include?("download") && line.include?("NumBytes") && measurement.download_total_bytes.nil?
+          last_download = JSON.parse(line)
+          measurement.download_total_bytes = last_download["Value"]["AppInfo"]["NumBytes"]
+        elsif line.include?("measurement") && line.include?("upload") && line.include?("NumBytes") && measurement.upload_total_bytes.nil?
+          last_upload = JSON.parse(line)
+          measurement.upload_total_bytes = last_upload["Value"]["AppInfo"]["NumBytes"]
         end
       end
 
@@ -31,7 +40,8 @@ class ProcessMeasurementJob < ApplicationJob
       measurement.upload = (result["upload"]["bandwidth"] / (1000.0 * 1000.0)) * 8
       measurement.latency = result["ping"]["latency"]
       measurement.jitter = result["ping"]["jitter"]
-
+      measurement.download_total_bytes = result["download"]["bytes"]
+      measurement.upload_total_bytes = result["upload"]["bytes"]
       measurement.save
     end
   end
