702dbd6b8979c42feb66754aed0598c2a2c89b88
Rafael Mendes <rafael@exactlylabs.com>
2022-07-29 21:40:26 -0300

    Changes to image scripts to remove login, rainbow and other stuff

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -2,8 +2,9 @@
 
 set -e
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-IMAGE_LOCATION=./radar.img
+IMAGE_LOCATION=${SCRIPT_DIR}/dist/radar.img
 
 if [[ ! -f "$IMAGE_LOCATION" ]]; then
     echo "Image not found at '$IMAGE_LOCATION' as expected"
