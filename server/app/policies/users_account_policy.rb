7e8129cb24ab7b55e88642a46346d6920200a932
iuke <edamm@itba.edu.ar>
2022-08-01 14:00:41 -0400

    PR comments + some bug fixing

--- server/app/policies/users_account_policy.rb
+++ server/app/policies/users_account_policy.rb
@@ -1,7 +1,7 @@
 class UsersAccountPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.where(account_id: @user_account.account.id)
+      scope.where(account_id: @user_account.account_id)
     end
   end
 end
