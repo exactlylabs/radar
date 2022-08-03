1578f5250fa785e857a60a8e6f2e18185bcb3145
iuke <edamm@itba.edu.ar>
2022-08-03 11:31:13 -0400

    fix for client issues found + new ones encountered

--- server/app/policies/account_policy.rb
+++ server/app/policies/account_policy.rb
@@ -1,7 +1,7 @@
 class AccountPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      User.find(user_account.user_id).accounts
+      User.find(@user_account.user_id).accounts
     end
   end
 end
