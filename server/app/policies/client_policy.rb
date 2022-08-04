9ed73c1ee97720a0cc04da4b6eb6c53711d2e609
Rafael Mendes <rafael@exactlylabs.com>
2022-08-03 21:19:45 -0300

    removed debug code

--- server/app/policies/client_policy.rb
+++ server/app/policies/client_policy.rb
@@ -1,7 +1,5 @@
 class ClientPolicy < ApplicationPolicy
     def access_watchdog?
-        puts "AAAAAA"
-        puts "User is #{@current_user}"
         @user_account && @user_account.user.superuser? && @record.has_watchdog_update?
     end
 end
