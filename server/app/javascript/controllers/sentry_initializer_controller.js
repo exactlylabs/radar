412c686cabc5df164e61005326e6383a6b63f00f
iuke <edamm@itba.edu.ar>
2022-08-10 10:37:04 -0400

    wip: sentry changes from PR

--- server/app/javascript/controllers/sentry_initializer_controller.js
+++ server/app/javascript/controllers/sentry_initializer_controller.js
@@ -8,11 +8,40 @@ import * as Sentry from "@sentry/browser";
   application.js.
  */
 export default class SentryInitializerController extends Controller {
+
+  static targets = [
+    "hiddenUserIdInput"
+  ];
+
   connect() {
-    const userElement = document.querySelector("#user_id_hidden");
-    Sentry.setUser({
-      id: userElement.getAttribute("data-id"),
-      email: userElement.getAttribute("data-email"),
-    });
+    if(this.isProduction) {
+      const userElement = document.querySelector("#user_id_hidden");
+      Sentry.setUser({
+        id: userElement.getAttribute("data-id"),
+        email: userElement.getAttribute("data-email"),
+      });
+    }
+  }
+
+  switchAccount(e) {
+    if(this.isProduction) {
+      const selectedAccountId = e.target.id.split("@")[1];
+      if (selectedAccountId) {
+        Sentry.setContext("account", {id: selectedAccountId});
+      }
+    }
+  }
+
+  clearUserContext() {
+    if(this.isProduction) {
+      const userElement = this.hiddenUserIdInputTarget;
+      userElement.setAttribute("data-id", null);
+      userElement.setAttribute("data-email", null);
+      Sentry.setUser(null);
+    }
+  }
+
+  get isProduction() {
+    return document.head.querySelector("meta[name=rails_env]").content === "production"
   }
 }
