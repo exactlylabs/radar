a20a7d63b45f440219a214765fa2363240e1b8e6
Michael M. Wasser <untoldone@users.noreply.github.com>
2022-08-11 12:37:59 -0700

    fix exporting measurements to stream csv as soon as rows are available

--- server/app/controllers/measurements_controller.rb
+++ server/app/controllers/measurements_controller.rb
@@ -13,7 +13,14 @@ class MeasurementsController < ApplicationController
     @measurements = Measurement.all
     authorize @measurements
     respond_to do |format|
-      format.csv { send_data @measurements.to_csv, filename: "measurements.csv" }
+      format.csv do
+        headers["X-Accel-Buffering"] = "no"
+        headers["Cache-Control"] = "no-cache"
+        headers["Content-Type"] = "text/csv; charset=utf-8"
+        headers["Content-Disposition"] = 'attachment; filename="measurements.csv"'
+        headers["Last-Modified"] = Time.zone.now.ctime.to_s
+        self.response_body = @measurements.to_csv_enumerator
+      end
     end
   end
 
@@ -21,7 +28,14 @@ class MeasurementsController < ApplicationController
     @measurements = Measurement.where(style: "NDT7")
     authorize @measurements
     respond_to do |format|
-      format.csv { send_data @measurements.to_ndt7_csv, filename: "measurements-extended.csv" }
+      format.csv do
+        headers["X-Accel-Buffering"] = "no"
+        headers["Cache-Control"] = "no-cache"
+        headers["Content-Type"] = "text/csv; charset=utf-8"
+        headers["Content-Disposition"] = 'attachment; filename="measurements-extended.csv"'
+        headers["Last-Modified"] = Time.zone.now.ctime.to_s
+        self.response_body = @measurements.to_ndt7_csv_enumerator
+      end
     end
   end
 end
