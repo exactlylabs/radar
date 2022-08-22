48d2f6feef3f3a9a2c482af6901e4a7e86ba44d7
Rafael Mendes <rafael@exactlylabs.com>
2022-08-22 20:28:31 -0300

    staging webpack config set to production

--- client_app/webpack/webpack.staging.js
+++ client_app/webpack/webpack.staging.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = {
-  mode: 'staging',
+  mode: 'production',
   // source-map - A full SourceMap is emitted as a separate file.
   // It adds a reference comment to the bundle so development tools know where to find it.
   devtool: 'source-map',
