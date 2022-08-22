ae403f74d1e1c09766dc1fd512f3092344c03596
iuke <edamm@itba.edu.ar>
2022-08-22 18:55:13 -0300

    wip: all results page

--- client_app/src/components/AllResultsPage/AllResultsPage.jsx
+++ client_app/src/components/AllResultsPage/AllResultsPage.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { CircularProgress, Grid, Paper } from '@mui/material';
+import { CircularProgress } from '@mui/material';
 import { MyButton } from '../common/MyButton';
 import { CircleMarker, MapContainer, Popup, TileLayer } from 'react-leaflet';
 import { STEPS } from '../../constants';
 import SpeedResultsBox from './SpeedResultsBox';
-import { Box } from '@mui/system';
 import { DOWNLOAD_SPEED_LOW_TO_MID_THRESHOLD, SPEED_FILTERS } from '../../utils/speeds';
 import { mapTileAttribution, mapTileUrl } from '../../utils/map';
 import { getAllSpeedTests } from '../../utils/apiRequests';
 import { MyMap } from '../common/MyMap';
+
+const mapWrapperStyle = {
+  width: '100%',
+  display: 'relative'
+}
 
 const AllResultsPage = ({ givenLocation, setStep, maxHeight }) => {
   const [results, setResults] = useState(null);
@@ -65,7 +69,7 @@ const AllResultsPage = ({ givenLocation, setStep, maxHeight }) => {
   const goToSpeedTest = () => setStep(STEPS.SPEED_TEST);
 
   return (
-    <div style={{ textAlign: 'center', paddingTop: 10, paddingBottom: 10 }}>
+    <div style={{ textAlign: 'center' }}>
       {loading && <CircularProgress size={25} />}
       {!loading && error && <p>{error}</p>}
       {!loading && results !== null && results.length === 0 && (
@@ -75,50 +79,44 @@ const AllResultsPage = ({ givenLocation, setStep, maxHeight }) => {
         </div>
       )}
       {!loading && results !== null && results.length > 0 && (
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={10}>
-            <Box component={Paper} style={{ padding: 10 }}>
-              <MapContainer
-                center={givenLocation ? givenLocation : [0,0]}
-                zoom={givenLocation ? 20 : 2}
-                scrollWheelZoom
-                style={{ height: `calc(${maxHeight} - 70px - 173px - 53px`, margin: 'auto' }}
+        <div style={mapWrapperStyle}>
+          <MapContainer
+            center={givenLocation ? givenLocation : [0,0]}
+            zoom={givenLocation ? 20 : 3}
+            scrollWheelZoom
+            style={{ height: `calc(${maxHeight} - 70px - 173px - 53px`, margin: 0 }}
+          >
+            <MyMap />
+            <TileLayer attribution={mapTileAttribution} url={mapTileUrl} />
+            {filteredResults.map(measurement => (
+              <CircleMarker
+                key={measurement.id}
+                radius={5}
+                center={[measurement.latitude , measurement.longitude]}
+                pathOptions={getColor(measurement)}
               >
-                <MyMap />
-                <TileLayer attribution={mapTileAttribution} url={mapTileUrl} />
-                {filteredResults.map(measurement => (
-                  <CircleMarker
-                    key={measurement.id}
-                    radius={5}
-                    center={[measurement.latitude , measurement.longitude]}
-                    pathOptions={getColor(measurement)}
-                  >
-                    <Popup>
-                      {measurement.upload_avg !== null && measurement.upload_avg !== undefined ? (
-                        <p>{`Upload: ${measurement.upload_avg.toFixed(3)} Mbps`}</p>
-                      ) : (
-                        <p>Upload: N/A</p>
-                      )}
-                      {measurement.download_avg !== null && measurement.download_avg !== undefined ? (
-                        <p>{`Download: ${measurement.download_avg.toFixed(3)} Mbps`}</p>
-                      ) : (
-                        <p>Download: N/A</p>
-                      )}
-                      {measurement.loss !== null && measurement.loss !== undefined ? (
-                        <p>{`Loss: ${measurement.loss}%`}</p>
-                      ) : (
-                        <p>Loss: N/A</p>
-                      )}
-                    </Popup>
-                  </CircleMarker>
-                ))}
-              </MapContainer>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={2}>
-            <SpeedResultsBox selectedFilter={selectedFilter} setSelectedFilter={setFilter} />
-          </Grid>
-        </Grid>
+                <Popup>
+                  {measurement.upload_avg !== null && measurement.upload_avg !== undefined ? (
+                    <p>{`Upload: ${measurement.upload_avg.toFixed(3)} Mbps`}</p>
+                  ) : (
+                    <p>Upload: N/A</p>
+                  )}
+                  {measurement.download_avg !== null && measurement.download_avg !== undefined ? (
+                    <p>{`Download: ${measurement.download_avg.toFixed(3)} Mbps`}</p>
+                  ) : (
+                    <p>Download: N/A</p>
+                  )}
+                  {measurement.loss !== null && measurement.loss !== undefined ? (
+                    <p>{`Loss: ${measurement.loss}%`}</p>
+                  ) : (
+                    <p>Loss: N/A</p>
+                  )}
+                </Popup>
+              </CircleMarker>
+            ))}
+          </MapContainer>
+          <SpeedResultsBox selectedFilter={selectedFilter} setSelectedFilter={setFilter} />
+        </div>
       )}
     </div>
   );
