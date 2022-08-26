4911e860f34325add3e9b60adff7b198e94303c8
iuke <edamm@itba.edu.ar>
2022-08-26 17:18:29 -0300

    better version for edit/delete account modal management

--- server/app/javascript/controllers/modal_controller.js
+++ server/app/javascript/controllers/modal_controller.js
@@ -12,6 +12,19 @@ export default class extends Controller {
     $(this.element).modal('show');
   }
 
+  /**
+   * This method is strictly for redirecting to home
+   * after account deletion, which had to be handled this
+   * way because of the request being triggered from a turbo
+   * frame, and not being able to redirect from actual controller.
+   * @param e: FormSubmission event
+   */
+  redirectHomeOnSuccess(e) {
+    if(e.detail.success) {
+      window.location.href = "/dashboard";
+    }
+  }
+
   submit(e) {
     if (e.detail.success) {
       this.hideModal();
