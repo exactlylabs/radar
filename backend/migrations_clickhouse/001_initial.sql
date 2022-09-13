35e5621847388abe10521e13c93e8501b20b9694
Rafael Mendes <rafael@exactlylabs.com>
2022-09-13 18:16:23 -0300

    fixes to clickhouse migrations

--- backend/migrations_clickhouse/001_initial.sql
+++ backend/migrations_clickhouse/001_initial.sql
@@ -37,27 +37,31 @@ CREATE TABLE IF NOT EXISTS measurements (
     geospace_id UUID,
 )
 ENGINE = MergeTree
-ORDER BY time;
+PRIMARY KEY (upload, geospace_id, asn_id)
+ORDER BY (upload, geospace_id, asn_id, time);
 
 -- Materialized Views
 
 CREATE MATERIALIZED VIEW summary_year
 ENGINE=MergeTree
-ORDER BY (upload, geospace_id, asn_id)
+ORDER BY (upload, geospace_id, asn_id, year)
 POPULATE
 AS
 SELECT
     quantileExact(0.5)(m_ip.med_ip_mbps) as med_mbps,
     quantileExact(0.5)(m_ip.med_ip_rtt) as med_min_rtt,
-    CountIf(med_ip_mbps < 25) as bad,
-    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100) as normal,
-    CountIf(med_ip_mbps >= 100) as good,
+    CountIf(med_ip_mbps < 25 AND upload=0) as bad_download,
+    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100 and upload=0) as normal_download,
+    CountIf(med_ip_mbps >= 100 AND upload=0) as good_download,
+    CountIf(med_ip_mbps < 3 AND upload=1) as bad_upload,
+    CountIf(med_ip_mbps >= 3  AND med_ip_mbps < 20 AND upload=1) as normal_upload,
+    CountIf(med_ip_mbps >= 20 AND upload=1) as good_upload,
     CountIf(upload=1) as total_upload_samples,
     CountIf(upload=0) as total_download_samples,
     m_ip.geospace_id,
     m_ip.asn_id,
     m_ip.upload,
-    m_ip.year
+    m_ip.year as year
 FROM (
     SELECT 
         m.geospace_id, 
@@ -73,15 +77,18 @@ GROUP BY m_ip.geospace_id, m_ip.asn_id, m_ip.upload, m_ip.year;
 
 CREATE MATERIALIZED VIEW summary_geospace_year
 ENGINE=MergeTree
-ORDER BY (upload, geospace_id)
+ORDER BY (upload, geospace_id, year)
 POPULATE
 AS
 SELECT
     quantileExact(0.5)(m_ip.med_ip_mbps) as med_mbps,
     quantileExact(0.5)(m_ip.med_ip_rtt) as med_min_rtt,
-    CountIf(med_ip_mbps < 25) as bad,
-    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100) as normal,
-    CountIf(med_ip_mbps >= 100) as good,
+    CountIf(med_ip_mbps < 25 AND upload=0) as bad_download,
+    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100 and upload=0) as normal_download,
+    CountIf(med_ip_mbps >= 100 AND upload=0) as good_download,
+    CountIf(med_ip_mbps < 3 AND upload=1) as bad_upload,
+    CountIf(med_ip_mbps >= 3  AND med_ip_mbps < 20 AND upload=1) as normal_upload,
+    CountIf(med_ip_mbps >= 20 AND upload=1) as good_upload,
     CountIf(upload=1) as total_upload_samples,
     CountIf(upload=0) as total_download_samples,
     m_ip.geospace_id,
@@ -105,22 +112,25 @@ GROUP BY m_ip.geospace_id, m_ip.upload, m_ip.year;
 
 CREATE MATERIALIZED VIEW summary_semester
 ENGINE=MergeTree
-ORDER BY (upload, geospace_id, asn_id)
+ORDER BY (upload, geospace_id, asn_id, year, semester)
 POPULATE
 AS
 SELECT
     quantileExact(0.5)(m_ip.med_ip_mbps) as med_mbps,
     quantileExact(0.5)(m_ip.med_ip_rtt) as med_min_rtt,
-    CountIf(med_ip_mbps < 25) as bad,
-    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100) as normal,
-    CountIf(med_ip_mbps >= 100) as good,
+    CountIf(med_ip_mbps < 25 AND upload=0) as bad_download,
+    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100 and upload=0) as normal_download,
+    CountIf(med_ip_mbps >= 100 AND upload=0) as good_download,
+    CountIf(med_ip_mbps < 3 AND upload=1) as bad_upload,
+    CountIf(med_ip_mbps >= 3  AND med_ip_mbps < 20 AND upload=1) as normal_upload,
+    CountIf(med_ip_mbps >= 20 AND upload=1) as good_upload,
     CountIf(upload=1) as total_upload_samples,
     CountIf(upload=0) as total_download_samples,
     m_ip.geospace_id,
     m_ip.asn_id,
     m_ip.upload,
-    m_ip.year,
-    m_ip.semester
+    m_ip.year as year,
+    m_ip.semester as semester
 FROM (
     SELECT 
         m.geospace_id, 
@@ -137,21 +147,24 @@ GROUP BY m_ip.geospace_id, m_ip.asn_id, m_ip.upload, m_ip.year, m_ip.semester;
 
 CREATE MATERIALIZED VIEW summary_geospace_semester
 ENGINE=MergeTree
-ORDER BY (upload, geospace_id)
+ORDER BY (upload, geospace_id, year, semester)
 POPULATE
 AS
 SELECT
     quantileExact(0.5)(m_ip.med_ip_mbps) as med_mbps,
     quantileExact(0.5)(m_ip.med_ip_rtt) as med_min_rtt,
-    CountIf(med_ip_mbps < 25) as bad,
-    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100) as normal,
-    CountIf(med_ip_mbps >= 100) as good,
+    CountIf(med_ip_mbps < 25 AND upload=0) as bad_download,
+    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100 and upload=0) as normal_download,
+    CountIf(med_ip_mbps >= 100 AND upload=0) as good_download,
+    CountIf(med_ip_mbps < 3 AND upload=1) as bad_upload,
+    CountIf(med_ip_mbps >= 3  AND med_ip_mbps < 20 AND upload=1) as normal_upload,
+    CountIf(med_ip_mbps >= 20 AND upload=1) as good_upload,
     CountIf(upload=1) as total_upload_samples,
     CountIf(upload=0) as total_download_samples,
     m_ip.geospace_id,
     m_ip.upload,
-    m_ip.year,
-    m_ip.semester
+    m_ip.year as year,
+    m_ip.semester as semester
 FROM (
     SELECT 
         m.geospace_id, 
@@ -171,22 +184,25 @@ GROUP BY m_ip.geospace_id, m_ip.upload, m_ip.year, m_ip.semester;
 
 CREATE MATERIALIZED VIEW summary_month
 ENGINE=MergeTree
-ORDER BY (upload, geospace_id, asn_id)
+ORDER BY (upload, geospace_id, asn_id, year, month)
 POPULATE
 AS
 SELECT
     quantileExact(0.5)(m_ip.med_ip_mbps) as med_mbps,
     quantileExact(0.5)(m_ip.med_ip_rtt) as med_min_rtt,
-    CountIf(med_ip_mbps < 25) as bad,
-    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100) as normal,
-    CountIf(med_ip_mbps >= 100) as good,
+    CountIf(med_ip_mbps < 25 AND upload=0) as bad_download,
+    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100 and upload=0) as normal_download,
+    CountIf(med_ip_mbps >= 100 AND upload=0) as good_download,
+    CountIf(med_ip_mbps < 3 AND upload=1) as bad_upload,
+    CountIf(med_ip_mbps >= 3  AND med_ip_mbps < 20 AND upload=1) as normal_upload,
+    CountIf(med_ip_mbps >= 20 AND upload=1) as good_upload,
     CountIf(upload=1) as total_upload_samples,
     CountIf(upload=0) as total_download_samples,
     m_ip.geospace_id,
     m_ip.asn_id,
     m_ip.upload,
-    m_ip.year,
-    m_ip.month
+    m_ip.year as year,
+    m_ip.month as month
 FROM (
     SELECT 
         m.geospace_id, 
@@ -203,21 +219,24 @@ GROUP BY m_ip.geospace_id, m_ip.asn_id, m_ip.upload, m_ip.year, m_ip.month;
 
 CREATE MATERIALIZED VIEW summary_geospace_month
 ENGINE=MergeTree
-ORDER BY (upload, geospace_id)
+ORDER BY (upload, geospace_id, year, month)
 POPULATE
 AS
 SELECT
     quantileExact(0.5)(m_ip.med_ip_mbps) as med_mbps,
     quantileExact(0.5)(m_ip.med_ip_rtt) as med_min_rtt,
-    CountIf(med_ip_mbps < 25) as bad,
-    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100) as normal,
-    CountIf(med_ip_mbps >= 100) as good,
+    CountIf(med_ip_mbps < 25 AND upload=0) as bad_download,
+    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100 and upload=0) as normal_download,
+    CountIf(med_ip_mbps >= 100 AND upload=0) as good_download,
+    CountIf(med_ip_mbps < 3 AND upload=1) as bad_upload,
+    CountIf(med_ip_mbps >= 3  AND med_ip_mbps < 20 AND upload=1) as normal_upload,
+    CountIf(med_ip_mbps >= 20 AND upload=1) as good_upload,
     CountIf(upload=1) as total_upload_samples,
     CountIf(upload=0) as total_download_samples,
     m_ip.geospace_id,
     m_ip.upload,
-    m_ip.year,
-    m_ip.month
+    m_ip.year as year,
+    m_ip.month as month
 FROM (
     SELECT 
         m.geospace_id, 
@@ -236,22 +255,25 @@ GROUP BY m_ip.geospace_id, m_ip.upload, m_ip.year, m_ip.month;
 
 CREATE MATERIALIZED VIEW summary_week
 ENGINE=MergeTree
-ORDER BY (upload, geospace_id, asn_id)
+ORDER BY (upload, geospace_id, asn_id, year, week)
 POPULATE
 AS
 SELECT
     quantileExact(0.5)(m_ip.med_ip_mbps) as med_mbps,
     quantileExact(0.5)(m_ip.med_ip_rtt) as med_min_rtt,
-    CountIf(med_ip_mbps < 25) as bad,
-    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100) as normal,
-    CountIf(med_ip_mbps >= 100) as good,
+    CountIf(med_ip_mbps < 25 AND upload=0) as bad_download,
+    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100 and upload=0) as normal_download,
+    CountIf(med_ip_mbps >= 100 AND upload=0) as good_download,
+    CountIf(med_ip_mbps < 3 AND upload=1) as bad_upload,
+    CountIf(med_ip_mbps >= 3  AND med_ip_mbps < 20 AND upload=1) as normal_upload,
+    CountIf(med_ip_mbps >= 20 AND upload=1) as good_upload,
     CountIf(upload=1) as total_upload_samples,
     CountIf(upload=0) as total_download_samples,
     m_ip.geospace_id,
     m_ip.asn_id,
     m_ip.upload,
-    m_ip.year,
-    m_ip.week,
+    m_ip.year as year,
+    m_ip.week as week
 FROM (
     SELECT 
         m.geospace_id, 
@@ -268,21 +290,24 @@ GROUP BY m_ip.geospace_id, m_ip.asn_id, m_ip.upload, m_ip.year, m_ip.week;
 
 CREATE MATERIALIZED VIEW summary_geospace_week
 ENGINE=MergeTree
-ORDER BY (upload, geospace_id)
+ORDER BY (upload, geospace_id, year, week)
 POPULATE
 AS
 SELECT
     quantileExact(0.5)(m_ip.med_ip_mbps) as med_mbps,
     quantileExact(0.5)(m_ip.med_ip_rtt) as med_min_rtt,
-    CountIf(med_ip_mbps < 25) as bad,
-    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100) as normal,
-    CountIf(med_ip_mbps >= 100) as good,
+    CountIf(med_ip_mbps < 25 AND upload=0) as bad_download,
+    CountIf(med_ip_mbps >= 25 AND med_ip_mbps < 100 and upload=0) as normal_download,
+    CountIf(med_ip_mbps >= 100 AND upload=0) as good_download,
+    CountIf(med_ip_mbps < 3 AND upload=1) as bad_upload,
+    CountIf(med_ip_mbps >= 3  AND med_ip_mbps < 20 AND upload=1) as normal_upload,
+    CountIf(med_ip_mbps >= 20 AND upload=1) as good_upload,
     CountIf(upload=1) as total_upload_samples,
     CountIf(upload=0) as total_download_samples,
     m_ip.geospace_id,
     m_ip.upload,
-    m_ip.year,
-    m_ip.week
+    m_ip.year as year,
+    m_ip.week as week
 FROM (
     SELECT 
         m.geospace_id, 
@@ -295,3 +320,4 @@ FROM (
     GROUP BY m.geospace_id, m.upload, m.ip, year, week
 ) m_ip 
 GROUP BY m_ip.geospace_id, m_ip.upload, m_ip.year, m_ip.week;
+
