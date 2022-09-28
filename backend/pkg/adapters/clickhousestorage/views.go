4332ffdbd232031894dc5d886905789a9e686fc1
Rafael Mendes <rafael@exactlylabs.com>
2022-09-28 13:52:32 -0300

    us_asns_tmp view fixed

--- backend/pkg/adapters/clickhousestorage/views.go
+++ backend/pkg/adapters/clickhousestorage/views.go
@@ -316,12 +316,9 @@ ENGINE=MergeTree
 ORDER BY (geospace_id)
 POPULATE
 AS
-SELECT DISTINCT ON (
-    id,
-    asn,
-    organization
-)
+SELECT DISTINCT ON (m.asn_id, m.geospace_id) m.asn_id, m.geospace_id, a.asn, a.organization
 FROM measurements m
+JOIN asns a ON a.id = m.asn_id
 WHERE m.geospace_id IS NOT NULL;
 `,
 }
