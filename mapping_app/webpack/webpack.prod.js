921637d1b0def4ec68e2a24022728c5e85cfdbbc
iuke <edamm@itba.edu.ar>
2022-10-05 19:58:58 -0300

    quick fix on webpack vars

--- mapping_app/webpack/webpack.prod.js
+++ mapping_app/webpack/webpack.prod.js
@@ -12,7 +12,7 @@ module.exports = {
       REACT_APP_API_BASE_URL: JSON.stringify('https://radar.exactlylabs.com'),
       REACT_APP_SENTRY_DSN: JSON.stringify('https://8baaf015e1f84ed282a25a291f44e2ab@o1197382.ingest.sentry.io/6754277'),
       MAPBOX_ACCESS_TOKEN: JSON.stringify('pk.eyJ1IjoiZXhhY3RseWxhYnMiLCJhIjoiY2w3OXJqcXhjMG1vbzQycGxidHNqdXRtcCJ9.BTDEZoZFcVnMMftMm33EMw'),
-      MAPBOX_TILESET_URL: JSON.stringify('https://api.mapbox.com/styles/v1/exactlylabs/cl7iwvbaz000l15mmms6da3kx/tiles/512/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXhhY3RseWxhYnMiLCJhIjoiY2w3OXJqcXhjMG1vbzQycGxidHNqdXRtcCJ9.BTDEZoZFcVnMMftMm33EMw')
+      MAPBOX_TILESET_URL: JSON.stringify('https://api.mapbox.com/styles/v1/exactlylabs/cl80e3l7c000e14n4o8elyzhk/tiles/512/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXhhY3RseWxhYnMiLCJhIjoiY2w3OXJqcXhjMG1vbzQycGxidHNqdXRtcCJ9.BTDEZoZFcVnMMftMm33EMw')
     },
     new WebpackObfuscatorPlugin({
       sourceMap: true,
