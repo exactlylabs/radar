1869a93781ff4d4e191c4cd27df6ee1b5bbf2101
ignaciograsso <igrasso@itba.edu.ar>
2022-10-05 12:04:00 -0300

    flutter app for speed test created

--- mobile_client_app/client_mobile_app/lib/main.dart
+++ mobile_client_app/client_mobile_app/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui' as ui;
+
+import 'package:client_mobile_app/speed_test_gauge.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -75,41 +78,25 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: SpeedTestGauge(
+            minSpeed: 0,
+            maxSpeed: 100,
+            activeGaugeGradientColor: ui.Gradient.linear(
+              const Offset(0, 10),
+              const Offset(0, 270),
+              <Color>[
+                Colors.yellow,
+                Colors.red,
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
