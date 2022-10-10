4494fca23d286a1b586e9ac3d50e6b6847c16295
Rafael Mendes <rafael@exactlylabs.com>
2022-10-10 17:44:32 -0300

    fix error in migration

--- backend/migrations/003_measurements.sql
+++ backend/migrations/003_measurements.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS measurements (
     has_access_token BOOLEAN,
     access_token_sig VARCHAR,
     asn_id UUID,
-    geospace_id UUID,
+    geospace_id UUID
 )
 ENGINE = MergeTree
 PRIMARY KEY (upload, geospace_id, asn_id)
