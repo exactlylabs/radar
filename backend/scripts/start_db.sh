2559e515815e26cf9bf504daa6792c15545c23f5
Rafael Mendes <rafael@exactlylabs.com>
2022-10-11 17:08:50 -0300

    new script to start the database in development environment through docker-compose

--- backend/scripts/start_db.sh
+++ backend/scripts/start_db.sh
@@ -4,7 +4,10 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-$SCRIPT_DIR/start_db.sh
+cd $SCRIPT_DIR
+
+docker-compose up -d
 
 
-go run cmd/ingest/main.go $@
+DB_PORT=9001 ./migrate.sh
+cd $SCRIPT_DIR/..
