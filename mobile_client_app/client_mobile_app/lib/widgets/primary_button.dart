08c94f873935e5da5b10b2ca777cd1fa4a26335a
ignaciograsso <igrasso@itba.edu.ar>
2022-10-21 15:34:04 -0300

    primary button widget and goback_and_continue button widget

---
+++ mobile_client_app/client_mobile_app/lib/widgets/primary_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  const PrimaryButton({
+    super.key,
+    required this.child,
+    this.color,
+    this.onPressed,
+  });
+
+  final VoidCallback? onPressed;
+  final Color? color;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        disabledBackgroundColor: color?.withOpacity(0.5),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(26.0)),
+      ),
+      onPressed: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: child,
+      ),
+    );
+  }
+}
