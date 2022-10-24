dd4b9789d2082a1a03d71388dc2548651db4fc51
ignaciograsso <igrasso@itba.edu.ar>
2022-10-24 16:00:20 -0300

    step indicator widget implemented

--- mobile_client_app/client_mobile_app/lib/presentations/home_page_body.dart
+++ mobile_client_app/client_mobile_app/lib/presentations/home_page_body.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:client_mobile_app/presentations/map/map_web_view_page.dart';
 import 'package:client_mobile_app/presentations/speed_test/widgets/goback_and_continue_buttons.dart';
-import 'package:client_mobile_app/widgets/primary_button.dart';
+import 'package:client_mobile_app/presentations/speed_test/widgets/steps_indicator.dart';
+import 'package:client_mobile_app/resources/images.dart';
 import 'package:flutter/material.dart';
 
 class HomePageBody extends StatelessWidget {
@@ -14,13 +17,34 @@ class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (pageIdx == _SPEED_TEST_PAGE_IDX) {
-      return Center(
-          child: Column(
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Text('Speed Test'),
           GoBackAndContinueButtons(),
+          const SizedBox(height: 300),
+          FutureBuilder(
+            future: _loadCheckImageInfo(context),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return CustomPaint(
+                    painter: StepsIndicator(
+                      imageInfo: snapshot.data as ImageInfo,
+                      currentStep: 2,
+                      totalSteps: 4,
+                      textColor: Theme.of(context).colorScheme.primary.withOpacity(0.3),
+                      currentTextColor: Theme.of(context).colorScheme.onPrimary,
+                      stepColor: Theme.of(context).colorScheme.surface,
+                      currentStepColor: Theme.of(context).colorScheme.secondary,
+                    ),
+                    child: Container());
+              } else {
+                return Container();
+              }
+            },
+          ),
         ],
-      ));
+      );
     } else if (pageIdx == _YOUR_RESULTS_PAGE_IDX) {
       return const Center(child: Text('Your Results'));
     } else if (pageIdx == _MAP_PAGE_IDX) {
@@ -28,6 +52,14 @@ class HomePageBody extends StatelessWidget {
     } else {
       return const Center(child: Text('Speed Test'));
     }
+  }
+
+  Future<ImageInfo> _loadCheckImageInfo(BuildContext context) async {
+    AssetImage assetImage = const AssetImage(Images.check);
+    ImageStream stream = assetImage.resolve(createLocalImageConfiguration(context));
+    Completer<ImageInfo> completer = Completer();
+    stream.addListener(ImageStreamListener((imageInfo, _) => completer.complete(imageInfo)));
+    return completer.future;
   }
 
   static const int _SPEED_TEST_PAGE_IDX = 0;
