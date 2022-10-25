85abea29fcf95ccfbc29867831fa3f271e2dca34
ignaciograsso <igrasso@itba.edu.ar>
2022-10-25 10:09:16 -0300

    added navigation cubit to multibloc provider

--- mobile_client_app/client_mobile_app/lib/presentations/home_page.dart
+++ mobile_client_app/client_mobile_app/lib/presentations/home_page.dart
@@ -12,70 +12,64 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<NavigationCubit>(
-      create: (context) => NavigationCubit(),
-      child: BlocBuilder<NavigationCubit, NavigationState>(
-        builder: (context, state) {
-          return Scaffold(
+    return BlocBuilder<NavigationCubit, NavigationState>(
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Theme.of(context).backgroundColor,
+          appBar: AppBar(
             backgroundColor: Theme.of(context).backgroundColor,
-            appBar: AppBar(
-              backgroundColor: Theme.of(context).backgroundColor,
-              title: Image.asset(
-                Images.logoDark,
-                fit: BoxFit.contain,
+            title: Image.asset(Images.logoDark, fit: BoxFit.contain),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(Images.speedTest),
+                activeIcon: Image.asset(Images.speedTestSelected),
+                label: 'Speed Test',
               ),
-            ),
-            bottomNavigationBar: BottomNavigationBar(
-              items: [
-                BottomNavigationBarItem(
-                  icon: Image.asset(Images.speedTest),
-                  activeIcon: Image.asset(Images.speedTestSelected),
-                  label: 'Speed Test',
-                ),
-                BottomNavigationBarItem(
-                  icon: Image.asset(Images.yourResults),
-                  activeIcon: Image.asset(Images.yourResultsSelected),
-                  label: 'Your Results',
-                ),
-                BottomNavigationBarItem(
-                  icon: Image.asset(Images.map),
-                  activeIcon: Image.asset(Images.mapSelected),
-                  label: 'Map',
-                ),
-              ],
-              type: BottomNavigationBarType.fixed,
-              currentIndex: state.currentIndex,
-              onTap: (index) => context.read<NavigationCubit>().changeTab(index),
-            ),
-            body: PageTransitionSwitcher(
-              duration: const Duration(milliseconds: 3000),
-              reverse: true,
-              transitionBuilder: (child, animation, secondaryAnimation) {
-                return FadeThroughTransition(
-                  animation: animation,
-                  secondaryAnimation: secondaryAnimation,
-                  child: child,
-                );
-              },
-              child: state.currentIndex == 0
-                  ? Center(child: Text('Speed Test'))
-                  : state.currentIndex == 1
-                      ? Container(
-                          // color: Colors.blue,
-                          child: const Center(
-                            child: Text('Your Results'),
-                          ),
-                        )
-                      : Container(
-                          // color: Colors.red,
-                          child: const Center(
-                            child: Text('Map'),
-                          ),
+              BottomNavigationBarItem(
+                icon: Image.asset(Images.yourResults),
+                activeIcon: Image.asset(Images.yourResultsSelected),
+                label: 'Your Results',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(Images.map),
+                activeIcon: Image.asset(Images.mapSelected),
+                label: 'Map',
+              ),
+            ],
+            type: BottomNavigationBarType.fixed,
+            currentIndex: state.currentIndex,
+            onTap: (index) => context.read<NavigationCubit>().changeTab(index),
+          ),
+          body: PageTransitionSwitcher(
+            duration: const Duration(milliseconds: 3000),
+            reverse: true,
+            transitionBuilder: (child, animation, secondaryAnimation) {
+              return FadeThroughTransition(
+                animation: animation,
+                secondaryAnimation: secondaryAnimation,
+                child: child,
+              );
+            },
+            child: state.currentIndex == 0
+                ? Center(child: Text('Speed Test'))
+                : state.currentIndex == 1
+                    ? Container(
+                        // color: Colors.blue,
+                        child: const Center(
+                          child: Text('Your Results'),
                         ),
-            ),
-          );
-        },
-      ),
+                      )
+                    : Container(
+                        // color: Colors.red,
+                        child: const Center(
+                          child: Text('Map'),
+                        ),
+                      ),
+          ),
+        );
+      },
     );
   }
 }
