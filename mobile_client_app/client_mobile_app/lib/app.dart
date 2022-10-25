e34fba12e6a5588ea012065efa89dbc13acad1e7
ignaciograsso <igrasso@itba.edu.ar>
2022-10-25 10:09:16 -0300

    added navigation cubit to multibloc provider

--- mobile_client_app/client_mobile_app/lib/app.dart
+++ mobile_client_app/client_mobile_app/lib/app.dart
@@ -1,10 +1,28 @@
 import 'package:client_mobile_app/core/flavors/app_config.dart';
+import 'package:client_mobile_app/core/navigation_bloc/navigation_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:client_mobile_app/resources/theme.dart';
 import 'package:client_mobile_app/presentations/home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => NavigationCubit()),
+      ],
+      child: const AppBuilder(),
+    );
+  }
+}
+
+class AppBuilder extends StatelessWidget {
+  const AppBuilder({
     Key? key,
   }) : super(key: key);
 
