a924a6f4d1a3d8c64e8fa25765bce68762aebfb9
ignaciograsso <igrasso@itba.edu.ar>
2022-10-27 12:28:06 -0300

    location step finished

--- mobile_client_app/client_mobile_app/lib/presentations/speed_test/steps/location_step.dart
+++ mobile_client_app/client_mobile_app/lib/presentations/speed_test/steps/location_step.dart
@@ -1,13 +1,13 @@
-import 'package:client_mobile_app/core/models/location.dart';
-import 'package:client_mobile_app/presentations/speed_test/speed_test_bloc/speed_test_cubit.dart';
-import 'package:client_mobile_app/presentations/speed_test/widgets/agree_to_terms.dart';
-import 'package:client_mobile_app/presentations/speed_test/widgets/current_location_button.dart';
-import 'package:client_mobile_app/presentations/speed_test/widgets/error_message.dart';
-import 'package:client_mobile_app/presentations/speed_test/widgets/goback_and_continue_buttons.dart';
-import 'package:client_mobile_app/presentations/speed_test/widgets/location_input_field.dart';
-import 'package:client_mobile_app/presentations/speed_test/widgets/title_and_subtitle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:client_mobile_app/resources/strings.dart';
+import 'package:client_mobile_app/core/models/location.dart';
+import 'package:client_mobile_app/presentations/speed_test/widgets/error_message.dart';
+import 'package:client_mobile_app/presentations/speed_test/widgets/agree_to_terms.dart';
+import 'package:client_mobile_app/presentations/speed_test/widgets/title_and_subtitle.dart';
+import 'package:client_mobile_app/presentations/speed_test/widgets/location_input_field.dart';
+import 'package:client_mobile_app/presentations/speed_test/widgets/current_location_button.dart';
+import 'package:client_mobile_app/presentations/speed_test/speed_test_bloc/speed_test_cubit.dart';
 
 class LocationStep extends StatelessWidget {
   const LocationStep({
@@ -27,41 +27,28 @@ class LocationStep extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.8,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const TitleAndSubtitle(
-                  title: "What's your location?",
-                  subtitle: "We’ll show your speed test results on the map.",
-                ),
-                const SizedBox(height: 30.0),
-                LocationInputField(),
-                SizedBox(height: 15.0),
-                CurrentLocationButton(),
-              ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            TitleAndSubtitle(
+              title: Strings.locationStepTitle,
+              subtitle: Strings.locationStepSubtitle,
             ),
-            if (locationError != null || termsError != null) ErrorMessage(message: locationError ?? termsError!),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                AgreeToTerms(
-                  agreed: termsAccepted,
-                  onAgreed: (value) => context.read<SpeedTestCubit>().setTermsAccepted(value ?? false),
-                ),
-                const SizedBox(height: 35.0),
-                GoBackAndContinueButtons(onContinuePressed: () => context.read<SpeedTestCubit>().nextStep()),
-              ],
-            ),
+            SizedBox(height: 30.0),
+            LocationInputField(),
+            CurrentLocationButton(),
           ],
         ),
-      ),
+        if (locationError != null || termsError != null) ErrorMessage(message: locationError ?? termsError!),
+        AgreeToTerms(
+          agreed: termsAccepted,
+          onAgreed: (value) => context.read<SpeedTestCubit>().setTermsAccepted(value ?? false),
+        ),
+      ],
     );
   }
 }
--- mobile_client_app/client_mobile_app/lib/resources/strings.dart
+++ mobile_client_app/client_mobile_app/lib/resources/strings.dart
@@ -3,4 +3,6 @@ class Strings {
   static const String speedTestLabel = 'Speed Test';
   static const String yourResultsLabel = 'Your Results';
   static const String mapLabel = 'Map';
+  static const String locationStepTitle = "What's your location?";
+  static const String locationStepSubtitle = "We’ll show your speed test results on the map.";
 }
