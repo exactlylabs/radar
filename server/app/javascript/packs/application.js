dcaf2828323617723add538027fe7e3da1f4859e
iuke <edamm@itba.edu.ar>
2022-10-28 14:50:53 -0300

    drag & drop support + update turbolinks + testing map importation order

--- server/app/javascript/packs/application.js
+++ server/app/javascript/packs/application.js
@@ -5,11 +5,9 @@
 
 import Rails from "@rails/ujs"
 import "@hotwired/turbo-rails"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "controllers"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
