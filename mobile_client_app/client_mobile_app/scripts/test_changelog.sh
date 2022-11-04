d16dd2f43634f2894dca72cc6761800682589b51
Rafael Mendes <rafael@exactlylabs.com>
2022-11-03 21:35:53 -0300

    test_changelog.sh with a better error message

--- mobile_client_app/client_mobile_app/scripts/test_changelog.sh
+++ mobile_client_app/client_mobile_app/scripts/test_changelog.sh
@@ -7,6 +7,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 VC=$(cat pubspec.yaml | sed -ne 's/version: [0-9]\.[0-9]\.[0-9]+//p')
 
 if [ ! -f  "${SCRIPT_DIR}/../android/fastlane/metadata/en-US/changelogs/${VC}.txt" ]; then
-    echo "Missing Metadata for Version Control ${VC}"
+    echo "Changelog ${SCRIPT_DIR}/../android/fastlane/metadata/en-US/changelogs/${VC}.txt not found"
 exit 1
 fi
