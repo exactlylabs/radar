179fcc158481a5f3fac3b9df20a3e03491f7ff92
ignaciograsso <igrasso@itba.edu.ar>
2022-11-04 11:14:50 -0300

    changes to the argument names in the utils file.

--- mobile_client_app/client_mobile_app/lib/core/utils/utils.dart
+++ mobile_client_app/client_mobile_app/lib/core/utils/utils.dart
@@ -1,36 +1,36 @@
 import 'dart:async';
 
-import 'package:client_mobile_app/core/flavors/app_config.dart';
-import 'package:client_mobile_app/core/flavors/string_resource/i_string_resource.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:client_mobile_app/core/flavors/app_config.dart';
+import 'package:client_mobile_app/core/flavors/string_resource/i_string_resource.dart';
 
 class Utils {
-  static Future<HydratedStorage> buildStorage() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    final storage = await HydratedStorage.build(storageDirectory: await getTemporaryDirectory());
-    return storage;
-  }
-
-  static Future<void> runWithCrashReporting(AppConfig stgConfig, IStringResource stgStringResources) async {
+  static Future<void> runWithCrashReporting(AppConfig config, IStringResource stringResources) async {
     HydratedBlocOverrides.runZoned(
       () async {
         _runWithCrashReporting(
-          codeToExecute: () => runApp(stgConfig),
-          appNamePrefix: stgStringResources.APP_NAME_PREFIX,
-          dsn: stgStringResources.SENTRY_FLUTTER_KEY,
+          codeToExecute: () => runApp(config),
+          appNamePrefix: stringResources.APP_NAME_PREFIX,
+          dsn: stringResources.SENTRY_FLUTTER_KEY,
         );
       },
-      storage: await Utils.buildStorage(),
+      storage: await _buildStorage(),
     );
   }
 
-  static Future<void> run(AppConfig devConfig) async {
+  static Future<void> run(AppConfig config) async {
     HydratedBlocOverrides.runZoned(
-      () => runApp(devConfig),
-      storage: await Utils.buildStorage(),
+      () => runApp(config),
+      storage: await _buildStorage(),
     );
+  }
+
+  static Future<HydratedStorage> _buildStorage() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    final storage = await HydratedStorage.build(storageDirectory: await getTemporaryDirectory());
+    return storage;
   }
 
   static void _runWithCrashReporting({
