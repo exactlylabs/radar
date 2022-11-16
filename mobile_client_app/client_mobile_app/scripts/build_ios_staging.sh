db5843af90d18e4333e0a3075e629ea53f92011c
Rafael Mendes <rafael@exactlylabs.com>
2022-11-16 20:48:11 -0300

    ios_staging script and changes to fastlane

--- mobile_client_app/client_mobile_app/scripts/build_ios_staging.sh
+++ mobile_client_app/client_mobile_app/scripts/build_ios_staging.sh
@@ -9,12 +9,15 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WORKSPACE=${SCRIPT_DIR}/../workspace
 APPDIR=${SCRIPT_DIR}/..
 BUILD=$(date +%s)
-( cd ${APPDIR} && flutter build ipa --build-number ${BUILD} --flavor dev -t lib/main_dev.dart --release --no-codesign )
+( cd ${APPDIR} && flutter build ipa --build-number ${BUILD} --flavor staging -t lib/main_staging.dart --release --no-codesign )
 
 # flutter build ipa -t lib/main_dev.dart --flavor dev --no-codesign --release
 # This generates a .xcarchive file at build/ios/archive/Runner.xcarchive
 
 
-mkdir -p ${WORKSPACE}/ios/dev
+mkdir -p ${WORKSPACE}/ios/staging
 export VERSION_NAME=$(cat ${APPDIR}/pubspec.yaml | sed -ne 's/version: \([0-9]\+\.[0-9]\+\.[0-9]\+\)+[0-9]\+/\1/p')
-cp -r ${APPDIR}/build/ios/iphoneos/Runner.app ${WORKSPACE}/ios/dev/Runner${VERSION_NAME}+${BUILD}.app
+
+( cd ${APPDIR} && fastlane ios sign_staging )
+
+# cp -r ${APPDIR}/build/ios/iphoneos/Runner.app ${WORKSPACE}/ios/staging/Runner${VERSION_NAME}+${BUILD}.app
