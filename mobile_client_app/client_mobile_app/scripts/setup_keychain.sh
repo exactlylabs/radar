d72440d1184c2edbbfa68b30b783bb9ddcfcd0f7
Rafael Mendes <rafael@exactlylabs.com>
2022-11-16 21:13:48 -0300

    changes to setup_keychain.sh

--- mobile_client_app/client_mobile_app/scripts/setup_keychain.sh
+++ mobile_client_app/client_mobile_app/scripts/setup_keychain.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-MY_KEYCHAIN="RadarKeyChain.keychain"
+MY_KEYCHAIN="RadarIOSKeyChain.keychain"
 security create-keychain -p "" "$MY_KEYCHAIN"
 security list-keychains -d user -s "$MY_KEYCHAIN" $(security list-keychains -d user | sed s/\"//g)
 security set-keychain-settings "$MY_KEYCHAIN"
@@ -8,3 +8,5 @@ security unlock-keychain -p "" "$MY_KEYCHAIN"
 
 security import $P12_KEY_PATH -k "$MY_KEYCHAIN" -P "" -T "/usr/bin/codesign" -T "/usr/bin/productbuild" -T "/usr/bin/xcodebuild"
 security import $CERT_PATH -k "$MY_KEYCHAIN" -P "" -T "/usr/bin/codesign" -T "/usr/bin/productbuild" -T "/usr/bin/xcodebuild"
+
+security set-key-partition-list -S apple-tool:,apple: -s -k "" $MY_KEYCHAIN
