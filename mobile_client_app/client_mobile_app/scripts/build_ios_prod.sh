9fea0bd56d8f61e45ccbec2d1aad5ccb2226a7e0
Rafael Mendes <rafael@exactlylabs.com>
2022-11-17 18:20:30 -0300

    build_ios_x scripts updated

--- mobile_client_app/client_mobile_app/scripts/build_ios_prod.sh
+++ mobile_client_app/client_mobile_app/scripts/build_ios_prod.sh
@@ -10,9 +10,9 @@ WORKSPACE=${SCRIPT_DIR}/../workspace
 APPDIR=${SCRIPT_DIR}/..
 BUILD=$(date +%s)
 
-( cd ${APPDIR} && flutter build ipa --build-number ${BUILD} --flavor dev -t lib/main_dev.dart --release --no-codesign )
-mkdir -p ${WORKSPACE}/ios/dev
+( cd ${APPDIR} && flutter build ipa --build-number ${BUILD} --flavor prod -t lib/main_prod.dart --release --no-codesign )
+mkdir -p ${WORKSPACE}/ios/prod
 export VERSION_NAME=$(cat ${APPDIR}/pubspec.yaml | sed -nre 's/version: ([0-9]+\.[0-9]+\.[0-9]+)\+[0-9]+/\1/p')
-( cd ${APPDIR}/ios && fastlane ios sign_dev )
+( cd ${APPDIR}/ios && fastlane ios sign_prod )
 
-cp -r ${APPDIR}/build/ios/iphoneos/Runner.app ${WORKSPACE}/ios/dev/Runner${VERSION_NAME}+${BUILD}.app
+cp -r ${APPDIR}/ios/Runner.ipa ${WORKSPACE}/ios/prod/Runner${VERSION_NAME}+${BUILD}.ipa
