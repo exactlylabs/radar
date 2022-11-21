7b5caf14b0ae5b8af7b9b89b07dd220acfb8218a
Rafael Mendes <rafael@exactlylabs.com>
2022-11-21 16:02:29 -0300

    clickhousedb pkg service

---
+++ backend/pkg/services/clickhousedb/db.go
@@ -0,0 +1,36 @@
+package clickhousedb
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"github.com/exactlylabs/mlab-mapping/backend/pkg/services/errors"
+)
+
+type ChStorageOptions struct {
+	Username       string
+	Password       string
+	Host           string
+	Port           int
+	DBName         string
+	MaxConnections int
+}
+
+func Open(opts ChStorageOptions) (driver.Conn, error) {
+	conn, err := clickhouse.Open(&clickhouse.Options{
+		Auth: clickhouse.Auth{
+			Database: opts.DBName,
+			Username: opts.Username,
+			Password: opts.Password,
+		},
+		Addr:         []string{fmt.Sprintf("%s:%d", opts.Host, opts.Port)},
+		MaxOpenConns: opts.MaxConnections,
+		ReadTimeout:  time.Hour,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "clickhouseStorage#Open Open")
+	}
+	return conn, nil
+}
