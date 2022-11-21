b4d413f22b47fa296d183c4575e772978f60a1fe
Rafael Mendes <rafael@exactlylabs.com>
2022-11-21 16:13:38 -0300

    new setup_shapes cmd cli, that populates the database with all geospaces as well as downloading the necessary shape files and doing the GeoJSON/vector tiles conversions

---
+++ backend/cmd/setup_shapes/internal/db.go
@@ -0,0 +1,146 @@
+package internal
+
+import (
+	"log"
+	"reflect"
+
+	"github.com/exactlylabs/mlab-mapping/backend/pkg/app/ports/namespaces"
+	"github.com/exactlylabs/mlab-mapping/backend/pkg/app/ports/storages"
+	"github.com/exactlylabs/mlab-mapping/backend/pkg/services/errors"
+	"github.com/paulmach/orb/geojson"
+	"github.com/paulmach/orb/planar"
+)
+
+var codesMap = map[namespaces.Namespace][]string{
+	namespaces.US_STATE:  {"GEOID", "NAME"},
+	namespaces.US_COUNTY: {"GEOID", "NAME"},
+	namespaces.US_TTRACT: {"ORG_CODE", "IND_NAME"},
+}
+
+var geospaces = map[string]*storages.DetailedGeospace{}
+
+type DBGeospaceFeature struct {
+	Geospace *storages.DetailedGeospace
+	Feature  *geojson.Feature
+}
+
+func PopulateDB(s storages.GeospaceStorage, ns namespaces.Namespace, fc geojson.FeatureCollection) ([]DBGeospaceFeature, error) {
+	populated := make([]DBGeospaceFeature, 0)
+	if len(geospaces) == 0 {
+		it, err := s.All(nil, nil)
+		if err != nil {
+			return nil, errors.Wrap(err, "internal.PopulateDB All")
+		}
+		for it.HasNext() {
+			g, err := it.GetRow()
+			if err != nil {
+				return nil, errors.Wrap(err, "internal.PopulateDB GetRow")
+			}
+			geospaces[string(g.Namespace)+g.GeoId] = g
+		}
+	}
+	for _, feature := range fc.Features {
+		codeMap := codesMap[ns]
+		rawCode := feature.Properties[codeMap[0]]
+		if rawCode == nil {
+			log.Println(ns, "shape with no data... skipping it")
+			continue
+		}
+		code := rawCode.(string)
+		rawName := feature.Properties[codeMap[1]]
+		if rawName == nil {
+			log.Println(ns, "shape with no data... skipping it")
+			continue
+		}
+		name := rawName.(string)
+		centroid, _ := planar.CentroidArea(feature.Geometry)
+		var g *storages.DetailedGeospace
+		var err error
+		switch ns {
+		case namespaces.US_STATE:
+			g, err = saveGeospace(s, &storages.Geospace{
+				Name:      &name,
+				GeoId:     code,
+				Namespace: namespaces.US_STATE,
+				Centroid:  [2]float64{centroid.Lat(), centroid.Lon()},
+			})
+			if err != nil {
+				panic(err)
+			}
+		case namespaces.US_COUNTY:
+			stateFips := code[:2]
+			parent, err := getGeospace(s, namespaces.US_STATE, stateFips)
+			if err != nil {
+				panic(err)
+			}
+			countyG := &storages.Geospace{
+				Namespace: namespaces.US_COUNTY,
+				Name:      &name,
+				GeoId:     code,
+				Centroid:  [2]float64{centroid.Lat(), centroid.Lon()},
+			}
+			if parent != nil {
+				countyG.ParentId = &parent.Id
+			}
+			g, err = saveGeospace(s, countyG)
+			if err != nil {
+				panic(err)
+			}
+		case namespaces.US_TTRACT:
+			g, err = saveGeospace(s, &storages.Geospace{
+				Name:      &name,
+				GeoId:     code,
+				Namespace: namespaces.US_TTRACT,
+				Centroid:  [2]float64{centroid.Lat(), centroid.Lon()},
+			})
+			if err != nil {
+				panic(err)
+			}
+		}
+		if g != nil {
+			populated = append(populated, DBGeospaceFeature{
+				Geospace: g,
+				Feature:  feature,
+			})
+		}
+	}
+	return populated, nil
+}
+
+func saveGeospace(storage storages.GeospaceStorage, g *storages.Geospace) (dg *storages.DetailedGeospace, err error) {
+	if dg, err = getGeospace(storage, g.Namespace, g.GeoId); err == storages.ErrGeospaceNotFound {
+		if err := storage.Create(g); err != nil {
+			return nil, errors.Wrap(err, "internal.saveGeospace Create")
+		}
+		dg, err = storage.Get(g.Id)
+		if err != nil {
+			return nil, errors.Wrap(err, "internal.saveGeospace Get")
+		}
+	} else {
+		g.Id = dg.Id
+		if !reflect.DeepEqual(g, dg.AsGeospace()) {
+			if err := storage.Update(g); err != nil {
+				return nil, errors.Wrap(err, "internal.saveGeospace Update")
+			}
+			dg.Name = g.Name
+			dg.Namespace = g.Namespace
+			dg.Centroid = g.Centroid
+			dg.GeoId = g.GeoId
+		}
+	}
+	geospaces[string(g.Namespace)+g.GeoId] = dg
+	return dg, nil
+}
+
+func getGeospace(storage storages.GeospaceStorage, ns namespaces.Namespace, geoId string) (*storages.DetailedGeospace, error) {
+	if cached, exists := geospaces[string(ns)+geoId]; exists {
+		return cached, nil
+	}
+	dg, err := storage.GetByGeoId(ns, geoId)
+	if err == storages.ErrGeospaceNotFound {
+		return nil, err
+	} else if err != nil {
+		return nil, errors.Wrap(err, "internal.getGeospace GetByGeoId")
+	}
+	return dg, nil
+}
---
+++ backend/cmd/setup_shapes/main.go
@@ -0,0 +1,155 @@
+/*
+Set up shapes for the following Namespaces
+	* US_STATE
+	* US_COUNTIES
+	* US_TRIBAL_TRACTS
+	* MULTI_LAYERED
+
+The shapes are in .geojson and .mbtiles formats. Additionally, this binary inserts the Features of these shapes into the DB as Geospace objects
+*/
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+
+	"github.com/exactlylabs/mlab-mapping/backend/cmd/setup_shapes/internal"
+	"github.com/exactlylabs/mlab-mapping/backend/pkg/adapters/clickhousestorages"
+	"github.com/exactlylabs/mlab-mapping/backend/pkg/app/ports/namespaces"
+	"github.com/exactlylabs/mlab-mapping/backend/pkg/config"
+	"github.com/exactlylabs/mlab-mapping/backend/pkg/services/clickhousedb"
+	"github.com/joho/godotenv"
+)
+
+var importOrder = []namespaces.Namespace{namespaces.US_STATE, namespaces.US_COUNTY, namespaces.US_TTRACT}
+var shapeUrls = map[namespaces.Namespace]string{
+	namespaces.US_STATE:  "https://www2.census.gov/geo/tiger/TIGER2021/STATE/tl_2021_us_state.zip",
+	namespaces.US_COUNTY: "https://www2.census.gov/geo/tiger/TIGER2021/COUNTY/tl_2021_us_county.zip",
+	namespaces.US_TTRACT: "https://www.sciencebase.gov/catalog/file/get/4f4e4a2ee4b07f02db61576c?facet=Indian_Reservations",
+}
+var envName = flag.String("env-file", ".env", "Name of the environment variables file")
+var outputDir = flag.String("output-dir", "geos", "Directory where all files will be stored")
+
+func main() {
+	flag.Parse()
+	godotenv.Load(*envName)
+	internal.ValidateRequiredBinary("tippecanoe")
+	internal.ValidateRequiredBinary("ogr2ogr")
+	internal.ValidateRequiredBinary("tile-join")
+	conf := config.GetConfig()
+	if err := os.MkdirAll(*outputDir, os.FileMode(0775)); err != nil {
+		panic(err)
+	}
+	nWorkers := runtime.NumCPU()
+	if conf.ClickhouseStorageNWorkers != "" {
+		n, err := strconv.Atoi(conf.ClickhouseStorageNWorkers)
+		if err != nil {
+			panic(err)
+		}
+		nWorkers = n
+	}
+	db, err := clickhousedb.Open(clickhousedb.ChStorageOptions{
+		DBName:         conf.DBName,
+		Username:       conf.DBUser,
+		Password:       conf.DBPassword,
+		Host:           conf.DBHost,
+		Port:           conf.DBPort(),
+		MaxConnections: nWorkers,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	storage := clickhousestorages.NewGeospaceStorage(db)
+	storage.Open()
+	defer storage.Close()
+	for _, ns := range importOrder {
+		log.Println("Downloading files for Namespace", ns)
+		shapeFile := shapePath(string(ns))
+		geoJsonFile := geoJSONPath(string(ns))
+		if err := internal.DownloadShapeFile(shapeFile, shapeUrls[ns]); err != nil {
+			panic(err)
+		}
+		log.Println("Shapefile stored at", shapeFile)
+		if err := internal.ShapeToGeoJSON(shapeFile, geoJsonFile, internal.DefaultSimplify); err != nil {
+			panic(err)
+		}
+		log.Println("GeoJSON generated and stored at", geoJsonFile)
+		fc, err := internal.LoadGeoJSON(geoJsonFile)
+		if err != nil {
+			panic(err)
+		}
+		log.Println("GeoJSON file loaded")
+		geospaces, err := internal.PopulateDB(storage, ns, fc)
+		if err != nil {
+			panic(err)
+		}
+		log.Println("Imported", len(geospaces), ns)
+		if err := internal.ReWriteGeoJSONFromGeospaces(geoJsonFile, fc, geospaces); err != nil {
+			panic(err)
+		}
+		log.Println("GeoJSON re-written with Geospaces data")
+	}
+
+	//  Now, create the tilesets to each namespace
+	stateGeoJSONPath := geoJSONPath(string(namespaces.US_STATE))
+	stateMBTilesPath := mbtilesPath(string(namespaces.US_STATE))
+	stateMBTilesMultiPath := mbtilesPath(string(namespaces.US_STATE) + "_MULTI")
+	stateLayerName := string(namespaces.US_STATE)
+
+	countyGeoJSONPath := geoJSONPath(string(namespaces.US_COUNTY))
+	countyMBTilesPath := mbtilesPath(string(namespaces.US_COUNTY))
+	countyMBTilesMultiPath := mbtilesPath(string(namespaces.US_COUNTY) + "_MULTI")
+	countyLayerName := string(namespaces.US_COUNTY)
+
+	ttractGeoJSONPath := geoJSONPath(string(namespaces.US_TTRACT))
+	ttractMBTilesPath := mbtilesPath(string(namespaces.US_TTRACT))
+	ttractLayerName := string(namespaces.US_TTRACT)
+
+	multiMBTilesPath := mbtilesPath(string(namespaces.MULTI_LAYERED))
+
+	// States zoom 0 to 12
+	if err := internal.GeoJSONToTilesets(stateGeoJSONPath, stateMBTilesPath, stateLayerName, 0, 12); err != nil {
+		panic(err)
+	}
+	// States zoom 0 to 5 to use by the MultiLayered
+	if err := internal.GeoJSONToTilesets(stateGeoJSONPath, stateMBTilesMultiPath, stateLayerName, 0, 5); err != nil {
+		panic(err)
+	}
+	// Counties zoom 0 to 12
+	if err := internal.GeoJSONToTilesets(countyGeoJSONPath, countyMBTilesPath, countyLayerName, 0, 12); err != nil {
+		panic(err)
+	}
+	// Counties zoom 6 to 12
+	if err := internal.GeoJSONToTilesets(countyGeoJSONPath, countyMBTilesMultiPath, countyLayerName, 6, 12); err != nil {
+		panic(err)
+	}
+	// Tribal Tracts zoom 0 to 12
+	if err := internal.GeoJSONToTilesets(ttractGeoJSONPath, ttractMBTilesPath, ttractLayerName, 0, 12); err != nil {
+		panic(err)
+	}
+
+	if err := internal.MergeTilesets(multiMBTilesPath, stateMBTilesMultiPath, countyMBTilesMultiPath); err != nil {
+		panic(err)
+	}
+
+	// Remove auxiliary files
+	os.Remove(stateMBTilesMultiPath)
+	os.Remove(countyMBTilesMultiPath)
+}
+
+func geoJSONPath(filename string) string {
+	return filepath.Join(*outputDir, filename+".geojson")
+}
+
+func shapePath(filename string) string {
+	return filepath.Join(*outputDir, filename+".zip")
+}
+
+func mbtilesPath(filename string) string {
+	return filepath.Join(*outputDir, filename+".mbtiles")
+}
