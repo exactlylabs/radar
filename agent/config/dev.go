a756053ae468fcac55c7801ff42592646881e772
Rafael Mendes <rafael@exactlylabs.com>
2022-11-23 14:10:38 -0300

    Removed unused fields from config package due to the removal of the default hourly test

--- agent/config/dev.go
+++ agent/config/dev.go
@@ -3,6 +3,5 @@ package config
 var DevConfig = &Config{
 	ServerURL: "http://127.0.0.1:3000",
 	PingFreq:  "15",
-	TestFreq:  "120",
 	SentryDsn: "",
 }
--- agent/config/prod.go
+++ agent/config/prod.go
@@ -3,6 +3,5 @@ package config
 var ProdConfig = &Config{
 	ServerURL: "https://radar.exactlylabs.com",
 	PingFreq:  "15",
-	TestFreq:  "3600",
 	SentryDsn: "https://23c5c9069e774f3abda19c4f9993ad93@o1197382.ingest.sentry.io/6369668",
 }
