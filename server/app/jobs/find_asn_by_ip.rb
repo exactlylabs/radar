47c90f76789519a9a8ca85cff7feb5ce0ac1431b
Rafael Mendes <rafael@exactlylabs.com>
2022-12-20 19:20:36 -0300

    find_asn_by_ip job, triggered by after_commit from both client and measurement models, if the IP has changed

---
+++ server/app/jobs/find_asn_by_ip.rb
@@ -0,0 +1,46 @@
+require "#{Rails.root}/lib/geotools/asns.rb"
+
+class FindAsnByIp < ApplicationJob
+    queue_as :default
+
+    def perform(obj)
+        # populates the "autonomous_system" attribute of a model object,
+        # it expects the same model to also have an "ip" attribute
+        if !obj.ip.present?
+            puts "IP NOT FOUND"
+            obj.autonomous_system = nil
+            obj.save!
+            return
+        end
+
+        geoAS = GeoTools::asn_from_ip obj.ip
+        if !geoAS.present?
+            puts "GEOAS NOT FOUND"
+            obj.autonomous_system = nil
+            obj.save!
+            return
+        end
+
+        as = AutonomousSystem.find_by_asn(geoAS.asn)
+        if as.nil?
+            # Create one
+            org = AutonomousSystemOrg.find_by_org_id(geoAS.organization.id)
+            if org.nil?
+                org = AutonomousSystemOrg.create(
+                    name: geoAS.organization.name,
+                    org_id: geoAS.organization.id,
+                    country: geoAS.organization.country,
+                    source: geoAS.organization.source,
+                )
+            end
+            as = AutonomousSystem.create(
+                name: geoAS.name,
+                autonomous_system_org: org,
+                asn: geoAS.asn,
+            )
+        end
+
+        obj.autonomous_system = as
+        obj.save!
+    end
+end
