42d5b9346b6f1ef04e36b66f1cdd8edf8f5fc13e
Rafael Mendes <rafael@exactlylabs.com>
2023-01-02 16:28:30 -0300

    fix cors issue

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["CORS_ALLOWED_ORIGINS"] || 'http://localhost:9999'
+    origins (ENV["CORS_ALLOWED_ORIGINS"] || 'http://localhost:9999').split(",").map(&:strip)
     resource '/client_api/v1/*',
              methods: [:get, :post, :options],
              headers: :any
