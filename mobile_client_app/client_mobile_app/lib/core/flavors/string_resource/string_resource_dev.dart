be6c3a63f57c50e4f3be284277d040e67c83cdef
ignaciograsso <igrasso@itba.edu.ar>
2023-01-17 11:41:50 -0300

    added meanClientMbps to AppInfo model

--- mobile_client_app/client_mobile_app/lib/core/flavors/string_resource/string_resource_dev.dart
+++ mobile_client_app/client_mobile_app/lib/core/flavors/string_resource/string_resource_dev.dart
@@ -2,7 +2,7 @@ import 'package:client_mobile_app/core/flavors/string_resource/i_string_resource
 
 class StringResourceDev implements IStringResource {
   @override
-  String SERVER_ENDPOINT = 'https://9b37-179-40-95-39.ngrok.io';
+  String SERVER_ENDPOINT = 'http://localhost:3000/';
 
   @override
   String APP_NAME = 'Radar Dev';
--- mobile_client_app/ndt7_client/lib/models/client_response.dart
+++ mobile_client_app/ndt7_client/lib/models/client_response.dart
@@ -31,18 +31,30 @@ class AppInfo {
   AppInfo(
     this.elapsedTime,
     this.numBytes,
+    this.meanClientMbps,
   );
 
   Map<String, dynamic> toJson() => {
         'ElapsedTime': elapsedTime,
         'NumBytes': numBytes,
+        'MeanClientMbps': meanClientMbps,
       };
 
   factory AppInfo.fromJson(Map<String, dynamic> json) => AppInfo(
         json['ElapsedTime'] as int,
         json['NumBytes'] as int,
+        _meanMbps(json['ElapsedTime'] as int, json['NumBytes'] as int),
       );
+
+  static double _meanMbps(int elapsedTime, int numBytes) {
+    final time = elapsedTime / 1e6;
+    double speed = numBytes / time;
+    speed *= 8;
+    speed /= 1e6;
+    return speed;
+  }
 
   final int elapsedTime;
   final int numBytes;
+  final double meanClientMbps;
 }
