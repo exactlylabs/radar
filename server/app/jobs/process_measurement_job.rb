db7e9218d99c820e3e6d5b00ed09e53bfc4fb6c0
Rafael Mendes <rafael@exactlylabs.com>
2023-01-18 16:19:40 -0300

    fix measurements job bug where all measurements total_bytes attributes were set as nil

--- server/app/jobs/process_measurement_job.rb
+++ server/app/jobs/process_measurement_job.rb
@@ -3,18 +3,16 @@ class ProcessMeasurementJob < ApplicationJob
 
   def perform(measurement)
     FindAsnByIp.perform_now measurement
-    measurement.download_total_bytes = 0.0
-    measurement.upload_total_bytes = 0.0
     case measurement.style
     when "NDT7"
       data = measurement.result.download.split("\n")
       extended_info = nil
-
       data.reverse.each do |line|
         row = JSON.parse(line)
         if row["Key"] == "measurement" && row["Value"]["TCPInfo"] && extended_info.nil?
           extended_info = row["Value"]["TCPInfo"]
-        elsif row["Key"] == "measurement" && row["Value"]["Test"] == "upload" && row["Value"]["AppInfo"] && measurement.upload_total_bytes.nil?
+        end
+        if row["Key"] == "measurement" && row["Value"]["Test"] == "upload" && row["Value"]["AppInfo"] && measurement.upload_total_bytes.nil?
           measurement.upload_total_bytes = row["Value"]["AppInfo"]["NumBytes"] * 1.1 # From tests, it seems that the real value is 5-10% of what the test returns
         elsif row["Key"] == "measurement" && row["Value"]["Test"] == "download" && row["Value"]["AppInfo"] && measurement.download_total_bytes.nil?
           measurement.download_total_bytes = row["Value"]["AppInfo"]["NumBytes"] * 1.1 # From tests, it seems that the real value is 3-10% of what the test returns
@@ -30,7 +28,8 @@ class ProcessMeasurementJob < ApplicationJob
       
       measurement.processed = true
       measurement.processed_at = Time.now
-
+      measurement.download_total_bytes = 0 if measurement.download_total_bytes.nil?
+      measurement.upload_total_bytes = 0 if measurement.upload_total_bytes.nil?
       measurement.save
       measurement.client.add_bytes!(measurement.created_at, measurement.download_total_bytes + measurement.upload_total_bytes)
     when "OOKLA"
@@ -42,6 +41,8 @@ class ProcessMeasurementJob < ApplicationJob
       measurement.jitter = result["ping"]["jitter"]
       measurement.download_total_bytes = result["download"]["bytes"] * 1.1 # From tests, it seems that the real value is 3-10% of what the test returns
       measurement.upload_total_bytes = result["upload"]["bytes"] * 1.1 # From tests, it seems that the real value is 5-10% of what the test returns
+      measurement.download_total_bytes = 0 if measurement.download_total_bytes.nil?
+      measurement.upload_total_bytes = 0 if measurement.upload_total_bytes.nil?
       measurement.save
       
       measurement.client.add_bytes!(measurement.created_at, measurement.download_total_bytes + measurement.upload_total_bytes)
