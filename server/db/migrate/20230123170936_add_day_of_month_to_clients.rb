9b00a9919411169573ed1e92d3dbcf25acadfd11
Rafael Mendes <rafael@exactlylabs.com>
2023-01-24 15:20:55 -0300

    added comments to migration as well

--- server/db/migrate/20230123170936_add_day_of_month_to_clients.rb
+++ server/db/migrate/20230123170936_add_day_of_month_to_clients.rb
@@ -1,5 +1,5 @@
 class AddDayOfMonthToClients < ActiveRecord::Migration[6.1]
   def change
-    add_column :clients, :data_cap_day_of_month, :integer, default: 1
+    add_column :clients, :data_cap_day_of_month, :integer, default: 1 # Use -1 to last day of the month
   end
 end
