7b6d4e180865b0565037db258174dc958216f1a4
Rafael Mendes <rafael@exactlylabs.com>
2023-01-24 18:53:22 -0300

    in_use field

---
+++ server/db/custom_seeds/seed_set_clients_in_use.rb
@@ -0,0 +1,11 @@
+# Custom seed file for running once and populating
+
+# To run, execute the following command:
+# `rails runner db/custom_seeds/seed_set_clients_in_use.rb` inside the
+# /server directory.
+
+
+# Client.all.each do |client|
+#     # Set any client that has been online for more than 1 hour as in_use
+#     client.last_ping
+# end
