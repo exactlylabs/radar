ee93507bb78288a671919b20c47d8cc2413b5f48
Rafael Mendes <rafael@exactlylabs.com>
2023-01-24 15:27:03 -0300

    merged new migrations from this branch into a single file

--- server/db/migrate/20230123183304_create_location_labels.rb
+++ server/db/migrate/20230123183304_create_location_labels.rb
@@ -5,5 +5,13 @@ class CreateLocationLabels < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
+    create_table :location_groups do |t|
+      t.string :name
+      t.references :location_label, foreign_key: true
+      t.references :account, foreign_key: true
+
+      t.timestamps
+    end
+    add_reference :locations, :location_group, foreign_key: true
   end
 end
