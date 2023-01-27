c590ae162221aeb6de0c9a8382e30b39f703a26e
iuke <edamm@itba.edu.ar>
2023-01-27 11:43:08 -0300

    added tooltip with user timezone info

--- server/app/javascript/controllers/timezone_controller.js
+++ server/app/javascript/controllers/timezone_controller.js
@@ -1,7 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  
+  /**
+   * By using this controller, we can identify user's timezone with more precision than native Rails Time API
+   * This is intended to be used specifically in tooltips for all of our tables which include time-related columns.
+   */
   connect() {
-    this.element.innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    /**
+     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/resolvedOptions#:~:text=as%20default%20values.-,timeZone,-The%20value%20provided
+     * Intl.DateTimeFormat().resolvedOptions().timeZone returns users' browser timezone value.
+     * An example of possible output: America/Buenos_Aires
+     * An example of possible output: UTC
+     */
+    let tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    if(!!tz) {
+      tz = tz.replaceAll('_', ' ');
+      tz = tz.replaceAll('/', ', ');
+      this.element.setAttribute('title', tz);
+      this.element.setAttribute('data-bs-original-title', tz);
+    }
   }
 }
