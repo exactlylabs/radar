d5519bc2056a9be71aab8626d6e19f6ea3b222bd
iuke <edamm@itba.edu.ar>
2023-01-30 17:00:33 -0300

    quick change on wifi

--- public_website/src/components/MobilePage/MobilePageCarrousel/MobilePageCarrousel.tsx
+++ public_website/src/components/MobilePage/MobilePageCarrousel/MobilePageCarrousel.tsx
@@ -71,7 +71,7 @@ const MobilePageCarrousel = (): ReactElement => {
     <div style={styles.MobilePageCarrousel}>
       <div style={styles.TextContainer(isSmall)}>
         <p className={'fw-bold'} style={styles.Header}>Radar for Mobile</p>
-        <p className={'fw-extra-bold'} style={styles.Title}>Test your connectivity outdoors or your wifi at home.</p>
+        <p className={'fw-extra-bold'} style={styles.Title}>Test your connectivity outdoors or your Wi-Fi at home.</p>
         <p className={'fw-medium'} style={styles.Subtitle}>Radar lets you run speed tests outdoors and indoors, compare results over time, and explore your neighborhood to get a better idea of how broadband looks like around you.</p>
       </div>
       { isSmall ?
