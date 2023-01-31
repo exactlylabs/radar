ddacb1e42a427e79dc83a281493b328bfbff26c9
Rafael Mendes <rafael@exactlylabs.com>
2023-01-31 18:42:03 -0300

    config package loads godotenv

--- agent/config/dev.go
+++ agent/config/dev.go
@@ -1,8 +1,9 @@
 package config
 
 var DevConfig = &Config{
-	ServerURL: "http://127.0.0.1:3000",
-	PingFreq:  "15",
-	SentryDsn: "",
-	CRLUrl:    "file://./dist/certs/rootCRL.crl.pem",
+	Environment: "Dev",
+	ServerURL:   "http://127.0.0.1:3000",
+	PingFreq:    "15",
+	SentryDsn:   "",
+	CRLUrl:      "file://./dist/certs/rootCRL.crl.pem",
 }
