35d80e05696d97e4258fc50f0a71af2d2db84252
Rafael Mendes <rafael@exactlylabs.com>
2023-02-01 15:35:43 -0300

    in_service field added to Client model

--- server/db/custom_seeds/seed_set_clients_in_use.rb
+++ server/db/custom_seeds/seed_set_clients_in_use.rb
@@ -1,11 +1,31 @@
-# Custom seed file for running once and populating
+# Custom seed file for running once
 
 # To run, execute the following command:
 # `rails runner db/custom_seeds/seed_set_clients_in_use.rb` inside the
 # /server directory.
 
+# Set as in use all pods with measurements every day for the last 7 days
+query = %{
+WITH meas_by_day as (
+    SELECT 
+    measurements.client_id, 
+    EXTRACT(DAY FROM measurements.created_at) as measurement_day
+  FROM clients 
+  JOIN measurements on measurements.client_id = clients.id 
+  WHERE measurements.created_at > NOW() - INTERVAL '7 day'
+  ), daily_count as (
+    SELECT 
+      client_id, 
+      measurement_day,
+      ROW_NUMBER() OVER (
+        PARTITION BY client_id
+      ) as rn
+    FROM meas_by_day
+    GROUP BY 1, 2
+  )
+  
+  UPDATE clients SET in_service = true WHERE id IN (SELECT client_id FROM daily_count WHERE rn >= 1);
+}
+res = ActiveRecord::Base.connection.exec_query(query)
 
-# Client.all.each do |client|
-#     # Set any client that has been online for more than 1 hour as in_use
-#     client.last_ping
-# end
+puts "Result is ", res
