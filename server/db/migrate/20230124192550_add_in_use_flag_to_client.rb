239ddb057e7cc9b2c8964722410523b067c67d65
Rafael Mendes <rafael@exactlylabs.com>
2023-02-01 15:35:43 -0300

    in_service field added to Client model

--- server/db/migrate/20230124192550_add_in_use_flag_to_client.rb
+++ server/db/migrate/20230124192550_add_in_use_flag_to_client.rb
@@ -1,5 +1,5 @@
 class AddInUseFlagToClient < ActiveRecord::Migration[6.1]
   def change
-    add_column :clients, :in_use, :boolean, default: false
+    add_column :clients, :in_service, :boolean, default: false
   end
 end
