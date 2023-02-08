fdd49748d9b2a5c246ebc1d0748316e0dd6acaf4
Rafael Mendes <rafael@exactlylabs.com>
2023-02-08 15:25:07 -0300

    Client model now enforces the new secret cost when authenticate_secret is called and it detects that the digested secret is using the old cost

--- server/app/models/client.rb
+++ server/app/models/client.rb
@@ -23,8 +23,6 @@ class Client < ApplicationRecord
   after_save :send_event
   after_create :send_created_event
   after_commit :check_ip_changed
-  has_secure_password :secret, validations: false
-  has_secure_password :new_secret, validations: false
 
   # Any client's which haven't pinged in PING_DURRATION * 1.5 and currently aren't marked offline
   scope :where_outdated_online, -> { where("online = true AND (pinged_at < ? OR pinged_at IS NULL)", (PING_DURATION * 1.5).second.ago) }
@@ -39,17 +37,28 @@ class Client < ApplicationRecord
   scope :where_in_service, -> { where(in_service: true)}
   scope :where_not_in_service, -> { where(in_service: false)}
 
-  def new_secret=(unencrypted_secret)
+  def secret=(unencrypted_secret)
     # Manually set new_secret, to use our custom cost on BCrypt hasher
     # https://github.com/rails/rails/blob/main/activemodel/lib/active_model/secure_password.rb#L146
     if unencrypted_secret.nil?
-      instance_variable_set("@new_secret", nil)
-      self.new_secret_digest = nil
+      instance_variable_set("@secret", nil)
+      self.secret_digest = nil
     else
-      instance_variable_set("@new_secret", unencrypted_secret)
+      instance_variable_set("@secret", unencrypted_secret)
       cost = BCrypt::Engine::MIN_COST
-      self.new_secret_digest = BCrypt::Password.create(unencrypted_secret, cost: cost)
+      self.secret_digest = BCrypt::Password.create(unencrypted_secret, cost: cost)
     end
+  end
+
+  def authenticate_secret(secret)
+    # same as https://github.com/rails/rails/blob/main/activemodel/lib/active_model/secure_password.rb#L168
+    if self.secret_digest && BCrypt::Password.new(self.secret_digest).is_password?(secret)
+      if BCrypt::Password.new(self.secret_digest).cost != BCrypt::Engine::MIN_COST
+        # Update the digested secret to use the new cost
+        self.secret = secret
+        self.save!
+      end
+    return self
   end
 
   def check_ip_changed
