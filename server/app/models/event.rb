0258c11b778d39c971beb5d7a42de396511cc0ea
Rafael Mendes <rafael@exactlylabs.com>
2023-02-28 21:15:23 -0300

    ClientCountAggregate now consumes from Event table

--- server/app/models/event.rb
+++ server/app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :aggregate, polymorphic: true
+  has_one :snapshot
 
   def self.last_version_from(aggregate)
     version = Event.where(aggregate: aggregate).order("version DESC").limit(1).pluck(:version)
