e95d9d7480d9d22ce75040bcb7d22c56671f411b
Rafael Mendes <rafael.mendes@birmind.com.br>
2023-03-03 13:52:02 -0300

    removed setupscripts directory

--- setupscripts/update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-sudo mv radarping.service /etc/systemd/system/
-sudo systemctl enable radarping
-sudo systemctl start radarping
-
--- setupscripts/update2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-sudo rm -f /var/spool/cron/crontabs/radar
-echo "$(($RANDOM % 60)) */1 * * * /home/radar/runtests.sh" | sudo tee /var/spool/cron/crontabs/radar
-sudo chmod 600 /var/spool/cron/crontabs/radar
-sudo chown 1000:108 /var/spool/cron/crontabs/radar
-
