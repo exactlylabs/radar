2fd11cda497e2a82a7ce517466a5f30531cea3b7
Rafael Mendes <rafael@exactlylabs.com>
2023-03-08 15:32:54 -0300

    scope.all when super user

--- server/app/policies/account_policy.rb
+++ server/app/policies/account_policy.rb
@@ -2,7 +2,7 @@ class AccountPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       if @user_account.present? && @user_account.user.super_user?
-        Account.all
+        scope.all
       elsif @user_account.present?
         User.find(@user_account.user_id).accounts
       else
