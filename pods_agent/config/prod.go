f6ae0f070df7da30e404a7568e90c3da2ae1e2d6
Rafael Mendes <rafael@exactlylabs.com>
2023-03-15 17:04:26 -0300

    new URL for prod.go

--- pods_agent/config/prod.go
+++ pods_agent/config/prod.go
@@ -2,7 +2,7 @@ package config
 
 var ProdConfig = &Config{
 	Environment: "Prod",
-	ServerURL:   "https://radar.exactlylabs.com",
+	ServerURL:   "https://pods.radartoolkit.com",
 	PingFreq:    "15",
 	SentryDsn:   "https://23c5c9069e774f3abda19c4f9993ad93@o1197382.ingest.sentry.io/6369668",
 	CRLUrl:      "https://storage.googleapis.com/radar.exactlylabs.com/rootCRL.crl.pem",
