a1f716511f9abd897eac0bf1353056e7e14124cc
iuke <edamm@itba.edu.ar>
2023-03-29 10:13:04 -0300

    quick change to index

--- speedtest/src/index.js
+++ speedtest/src/index.js
@@ -5,14 +5,16 @@ import App from './App';
 import * as Sentry from '@sentry/react';
 import {getConfigFromParams} from "./utils/params";
 
+const config = getConfigFromParams();
+
 if (REACT_APP_ENV === 'production') {
   Sentry.init({
     dsn: REACT_APP_SENTRY_DSN,
     environment: 'production',
-    tunnel: `https://pods.radartoolkit.com/client_api/v1/sentry?client_id=${init.clientId}`
+    tunnel: `https://pods.radartoolkit.com/client_api/v1/sentry?client_id=${config.clientId}`
   });
-  Sentry.setUser({ id: init.clientId });
+  Sentry.setUser({ id: config.clientId });
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App config={getConfigFromParams()} />);
+root.render(<App config={config} />);
