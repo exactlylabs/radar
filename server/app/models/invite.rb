02830150a6f3ef562e346cc5303cdc0cfbbd47a0
iuke <edamm@itba.edu.ar>
2023-03-30 10:33:12 -0300

    fixed alert regarding nil on invitee row

--- server/app/models/invite.rb
+++ server/app/models/invite.rb
@@ -9,4 +9,17 @@ class Invite < ApplicationRecord
   def get_badge_style
     "badge-light-warning"
   end
+
+  def get_letter_for_icon
+    if self.first_name == '' && self.last_name == '' && self.email == ''
+      return "-"
+    end
+    if self.first_name != ''
+      self.first_name[0].upcase
+    elsif self.last_name != ''
+      self.last_name[0].upcase
+    else
+      self.email[0].upcase
+    end
+  end
 end
