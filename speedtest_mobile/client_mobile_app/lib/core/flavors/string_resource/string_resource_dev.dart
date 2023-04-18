9caed33b1f5a560bcf4e0cb6fc1100077f4010cd
ignaciograsso <ignacio@exactlylabs.com>
2023-04-18 09:44:50 -0300

    added flags to webview url

--- speedtest_mobile/client_mobile_app/lib/core/flavors/string_resource/string_resource_dev.dart
+++ speedtest_mobile/client_mobile_app/lib/core/flavors/string_resource/string_resource_dev.dart
@@ -14,7 +14,8 @@ class StringResourceDev implements IStringResource {
   String SENTRY_FLUTTER_KEY = '';
 
   @override
-  String WEB_ENDPOINT = 'https://speedtest-staging.exactlylabs.com/?webviewMode=true&tab=2&noZoomControl=true';
+  String WEB_ENDPOINT =
+      'https://speedtest-staging.exactlylabs.com/?webviewMode=true&tab=2&noZoomControl=true&global=true&client_id=1';
 
   @override
   String ENVIRONMENT = 'development';
