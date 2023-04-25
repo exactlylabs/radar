80d6e6d251eabcc44c551155373d926b0635f326
Rafael Mendes <rafael.mendes@birmind.com.br>
2023-04-25 15:39:45 -0300

    fixed report to sentry for consumers and clock .rb

--- server/clock.rb
+++ server/clock.rb
@@ -1,17 +1,29 @@
 require 'rufus-scheduler'
 
+#Sentry.configuration.background_worker_threads = 0
+
 scheduler = Rufus::Scheduler.new
 
 scheduler.every '3s', overlap: false do
-  if Rails.application.healthy? && !Rails.application.transient?
-    Client.update_outdated_online!
+  begin
+    if Rails.application.healthy? && !Rails.application.transient?
+      Client.update_outdated_online!
+    end
+    Client.request_scheduled_tests!
+    Rails.application.heartbeat!
+  rescue => e
+    Sentry.capture_exception(e)
+    raise e
   end
-  Client.request_scheduled_tests!
-  Rails.application.heartbeat!
 end
 
 scheduler.every '1h', overlap: false do
-  Client.refresh_outdated_data_usage!
+  begin
+    Client.refresh_outdated_data_usage!
+  rescue => e
+    Sentry.capture_exception(e)
+    raise e
+  end
 end
 
 begin
--- server/consumers.rb
+++ server/consumers.rb
@@ -5,15 +5,42 @@ scheduler = Rufus::Scheduler.new
 sh = StudyLevelHandler::Handler.new
 
 scheduler.every '5m', overlap: false do
-  ClientCountAggregate.aggregate!
+  begin
+    ClientCountAggregate.aggregate!
+  rescue => e
+    Sentry.capture_exception(e)
+    raise e
+  end
 end
 
 scheduler.every '5m', overlap: false do
+  begin
     OnlineClientCountProjection.aggregate!
+  rescue => e
+    Sentry.capture_exception(e)
+    raise e
+  end
 end
 
 scheduler.every '5m', overlap: false do
-  sh.aggregate!
+  begin
+    sh.aggregate!
+  rescue => e
+    Sentry.capture_exception(e)
+    raise e
+  end
+end
+
+def scheduler.on_error(job, error)
+  Rails.logger.error(
+    "err#{error.object_id} rufus-scheduler intercepted #{error.inspect}" +
+    " in job #{job.inspect}"
+  )
+  error.backtrace.each_with_index do |line, i|
+    Rails.logger.error(
+      "err#{error.object_id} #{i}: #{line}"
+    )
+  end
 end
 
 begin
