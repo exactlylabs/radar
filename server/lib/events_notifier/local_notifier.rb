9129c12edc29207f066610e833bd65c25580a2f3
Rafael Mendes <rafael@exactlylabs.com>
2023-05-04 17:15:01 -0300

    new notifications for locations and user invitation

--- server/lib/events_notifier/local_notifier.rb
+++ server/lib/events_notifier/local_notifier.rb
@@ -11,4 +11,60 @@ class LocalNotifier < EventsNotifier::Notifier
         * Contact Email: #{contact.email}
     }
   end
+
+  def notify_user_invite_accepted(account, user)
+    puts %{
+      User Invite Accepted
+
+        * Account Name: #{account.name}
+        * User: #{user.first_name}
+    }
+  end
+
+  
+
+  def notify_new_location(location_info)
+    puts %{
+      New Location!
+
+        #{location_info_as_str(location_info)}
+    }
+  end
+
+  def notify_location_online(location_info)
+    puts %{
+      Location Online!
+
+        #{location_info_as_str(location_info)}
+    }
+  end
+
+  def notify_location_offline(location_info)
+    puts %{
+      Location Offline!
+
+        #{location_info_as_str(location_info)}
+    }
+  end
+
+  def notify_study_goal_reached(geospace, goal, as_org=nil)
+    puts %{
+      Study Goal Reached!
+        #{geospace.namespace.titleize} just reached #{goal} locations#{" for ISP#{as_org.name}" if as_org}!
+    }
+  end
+
+  private
+
+  def location_info_as_str(location_info)
+    %{
+      * Location: #{location_info.location.id}
+      * Location Name: #{location_info.location.name}
+      * Location State: #{location_info&.state&.name}
+      * Location County: #{location_info&.county&.name}
+      * Location Place: #{location_info&.place&.name}
+      * Location Study State?: #{location_info.location.study_state?}
+      * Location Extras: #{location_info&.extra}
+    }
+  end
 end
