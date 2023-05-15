85f5a14332b2029e74eb9bac49b3d26834696c87
Rafael Mendes <rafael@exactlylabs.com>
2023-05-15 18:16:50 -0300

    created_by belongs to fixed

--- server/app/models/location.rb
+++ server/app/models/location.rb
@@ -30,7 +30,7 @@ include EventSourceable
 
   validates :name, :address, presence: true
 
-  belongs_to :user, foreign_key: 'created_by_id'
+  belongs_to :created_by, class_name: "User", foreign_key: 'created_by_id', optional: true
   belongs_to :account
   belongs_to :location_group, optional: true
   belongs_to :account
