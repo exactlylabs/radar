43407740325807664d559fb8f7ff4ac442956917
Rafael Mendes <rafael@exactlylabs.com>
2023-05-22 16:32:01 -0300

    progress bar design fixes

--- server/app/javascript/controllers/update_group_controller.js
+++ server/app/javascript/controllers/update_group_controller.js
@@ -3,59 +3,37 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   static targets = ["bar", "handle", "fill", "input", "text"];
 
-  connect() {
-    console.log("hey");
-    var progressBar = $(this.barTarget);
-    var handle = $(this.handleTarget);
-    var fill = $(this.fillTarget);
-    var percent = $(this.inputTarget);
-    var text = $(this.textTarget);
+  connect() {}
 
-    // Added this timeout because the width is returning -2 when connect is called.
-    // probably related to the element not being drawn yet.
-    setTimeout(function() {
-      var progress = parseInt(percent.attr("value"));
-      var barWidth = progressBar.width();
-      var handleLeft = Math.round((progress / 100) * barWidth);
-
-      handle.css('left', handleLeft + 'px');
-      fill.css('width', handleLeft + 'px');
-    }, 200);
-    
-    handle.on('mousedown', function(e) {
-      e.preventDefault();
-      handle.addClass('dragging');
-      
-      $(document).on('mousemove', moveHandle);
-      $(document).on('mouseup', releaseHandle);
-    });
-
-    function moveHandle(e) {
-      var barLeft = progressBar.offset().left;
-      var barWidth = progressBar.width();
-      var mouseX = e.pageX;
-      var offsetX = mouseX - barLeft;
-      
-      var progress = Math.round((offsetX / barWidth) * 100);
-      progress = Math.max(0, Math.min(progress, 100));
-      
-      var handleLeft = Math.round((progress / 100) * barWidth);
-      handle.css('left', handleLeft + 'px');
-      fill.css('width', handleLeft + 'px');
-      text.text(progress + '%');
-      percent.attr("value", progress);
-    }
-
-    function releaseHandle() {
-      handle.removeClass('dragging');
-      
-      $(document).off('mousemove', moveHandle);
-      $(document).off('mouseup', releaseHandle);
-    }
+  onMouseDown (e) {
+    this.handleTarget.classList.add('dragging');
+    document.addEventListener('mouseup', this.releaseHandle.bind(this));
+    document.addEventListener('mousemove', this.moveHandle.bind(this));
   }
 
-  initProgressBar() {
-    
+  releaseHandle() {
+    this.handleTarget.classList.remove('dragging');
+    document.removeEventListener('mouseup', this.releaseHandle.bind(this));
+    document.removeEventListener('mousemove', this.moveHandle.bind(this));
   }
 
+  moveHandle(e) {
+    if (!this.handleTarget.classList.contains('dragging')) return;
+    let barLeft = $(this.barTarget).offset().left;
+    let barWidth = $(this.barTarget).width();
+    let mouseX = e.pageX;
+    let offsetX = mouseX - barLeft;
+
+    let progress = Math.round((offsetX / barWidth) * 100);
+    progress = Math.max(0, Math.min(progress, 100));
+    if (progress > 50) {
+      this.textTarget.style.color = 'white';
+    } else {
+      this.textTarget.style.color = 'black';
+    }
+    this.handleTarget.style = `left: ${progress}%;`
+    this.fillTarget.style = `width: ${progress}%;`;
+    this.textTarget.innerText = progress + '%';
+    this.inputTarget.setAttribute("value", progress);
+  }
 }
