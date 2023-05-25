6faa99911b2d7bf645fa58f6c7a2b1648bc6f715
Rafael Mendes <rafael@exactlylabs.com>
2023-05-25 19:28:46 -0300

    tests for pod_os

--- pods_os/tests/settings.py
+++ pods_os/tests/settings.py
@@ -1,6 +1,13 @@
 import os
 
-RADAR_IMAGE = os.getenv("RADAR_IMAGE", "dist/radar.img")
-DTB_FILE = os.getenv("DTB_FILE", "kernels/bcm2710-rpi-3-b.dtb")
-KERNEL_FILE = os.getenv("KERNEL_FILE", "kernels/kernel8.img")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+RADAR_IMAGE = os.path.join(
+    BASE_DIR, os.getenv("RADAR_IMAGE", "dist/radar.img"))
+DTB_FILE = os.path.join(
+    BASE_DIR, os.getenv("DTB_FILE", "kernels/bcm2710-rpi-3-b.dtb")
+)
+KERNEL_FILE = os.path.join(
+    BASE_DIR, os.getenv("KERNEL_FILE", "kernels/kernel8.img")
+)
 
--- pods_os/tests/test_pods_start.py
+++ pods_os/tests/test_pods_start.py
@@ -4,15 +4,13 @@ import shutil
 import subprocess
 import tempfile
 import time
-from functools import reduce
 
 import avocado
-from avocado.utils import ssh, vmimage
-
+from avocado.utils import ssh, datadrainer, cloudinit
 from qemu.machine import QEMUMachine
 from qemu.utils import get_info_usernet_hostfwd_port
 
-from tests import settings
+import settings
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -33,7 +31,7 @@ def run_cmd(args):
     return (stdout, stderr, res)
 
 
-class TestPods(avocado.Test):
+class BaseTest(avocado.Test):
     arch = "aarch64"
     cpu = "cortex-A72"
     machine = "raspi3b"
@@ -42,16 +40,34 @@ class TestPods(avocado.Test):
         "console=tty1 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 "
         "rootdelay=1"
     )
+    username = "root"
+    password = "password"
     ssh_user = "radar"
 
+    def tearDown(self):
+        self.vm.shutdown(hard=True)
+
     def setUp(self) -> None:
-        self._vm = None
         self.binary = f"/usr/local/bin/qemu-system-{self.arch}"
         self.qemu_img_binary = f"/usr/local/bin/qemu-img"
         self.dtb = settings.DTB_FILE
         self.kernel = settings.KERNEL_FILE
         self.setup_image()
         self.ssh_pub_key, self.ssh_key = self.setup_ssh_keys()
+        self.vm = self._get_vm()
+        self.vm.add_args(
+            "-machine", "raspi3b",
+            "-cpu", "cortex-a72",
+            "-dtb", settings.DTB_FILE,
+            "-m", "1G",
+            "-smp", "4",
+            "-kernel", settings.KERNEL_FILE,
+            "-sd", settings.RADAR_IMAGE,
+            "-serial", "stdio",
+            "-netdev", "user,id=net1,hostfwd=tcp::2222-:22",
+            "-device", "usb-net,netdev=net1",
+            "-append", self.kernel_params
+        )
 
     def setup_image(self):
         self.image = settings.RADAR_IMAGE
@@ -71,7 +87,6 @@ class TestPods(avocado.Test):
                     f"failed to resize the image {self.image} to {new_size}G"
                 )
 
-
     def setup_ssh_keys(self):
         # Load the private and public keys, then send the private key to the
         # workdir and return the set
@@ -86,8 +101,34 @@ class TestPods(avocado.Test):
         os.chmod(target_private_key, 0o600)
         return (ssh_pub_key, target_private_key)
 
+    def set_up_cloudinit(self, ssh_pubkey=None):
+        self.phone_server = cloudinit.PhoneHomeServer(('0.0.0.0', 0),
+                                                      self.name)
+        cloudinit_iso = self.prepare_cloudinit(ssh_pubkey)
+        self.vm.add_args('-drive', 'file=%s,format=raw' % cloudinit_iso)
+
+    def prepare_cloudinit(self, ssh_pubkey=None):
+        self.log.info('Preparing cloudinit image')
+        try:
+            cloudinit_iso = os.path.join(self.workdir, 'cloudinit.iso')
+            pubkey_content = None
+            if ssh_pubkey:
+                with open(ssh_pubkey) as pubkey:
+                    pubkey_content = pubkey.read()
+            cloudinit.iso(cloudinit_iso, self.name,
+                          username=self.username,
+                          password=self.password,
+                          # QEMU's hard coded usermode router address
+                          phone_home_host='10.0.2.2',
+                          phone_home_port=self.phone_server.server_port,
+                          authorized_key=pubkey_content)
+        except Exception:
+            self.cancel('Failed to prepare the cloudinit image')
+        return cloudinit_iso
+
     def _get_vm(self):
         self.socket_dir = tempfile.mkdtemp(prefix="pods_os")
+        self.log.info("Instantiating new machine from binary: %s", self.binary)
         vm = QEMUMachine(
             self.binary,
             base_temp_dir=self.workdir,
@@ -98,32 +139,14 @@ class TestPods(avocado.Test):
         self.log.debug("Temp. Dir: %s\nLog Dir: %s", vm.temp_dir, vm.log_dir)
         return vm
 
-    def prepare_vm(self, **options):
-        vm = self._get_vm()
-        for k, v in options.items():
-            vm.add_args(f"-{k}", v)
-        return vm
-
-    @property
-    def vm(self):
-        if not self._vm:
-            self._vm = self.prepare_vm()
-        return self._vm
-
-    def ssh(self, pubkey, user):
-        res = self.vm.command(
-            'human-monitor-command',
-            command_line='info usernet'
-        )
-        port = get_info_usernet_hostfwd_port(res)
-        self.assertIsNotNone(port)
+    def ssh(self, key, user):
         self.ssh_session = ssh.Session(
-            '127.0.0.1', port=port, user=user, key=pubkey
+            '127.0.0.1', port=2222, user=user, key=key
         )
         for i in range(5):
             try:
                 self.ssh_session.connect()
-                return
+                return self.ssh_session
             except:
                 self.log.debug(
                     "Failed to connect through ssh -- Attempt %d of %d", 1, 5
@@ -131,23 +154,32 @@ class TestPods(avocado.Test):
                 time.sleep(i)
         self.fail(
             "Unable to connect through ssh to host at port %d and user %s",
-            port, user
+            2222, user
         )
 
-    def test_pod_started(self):
-        vm = self.prepare_vm(
-            machine="raspi3b",
-            cpu="cortex-a72",
-            dtb=settings.DTB_FILE,
-            m="1G",
-            smp="4",
-            kernel=settings.KERNEL_FILE,
-            sd=settings.RADAR_IMAGE,
-            serial="stdio",
-            append=self.kernel_params
+    def launch_and_wait(self, set_up_ssh_connection=True):
+        self.vm.set_console()
+        self.vm.launch()
+        console_drainer = datadrainer.LineLogger(
+            self.vm.console_socket.fileno(),
+            logger=self.log
         )
+        console_drainer.start()
+        self.log.info('VM launched, waiting for boot confirmation from guest')
 
-        with vm as vm:
-            vm.set_console()
-            vm.launch()
+        if set_up_ssh_connection:
+            self.log.info('Setting up the SSH connection')
+            self.ssh(self.ssh_key, self.ssh_user)
+
+
+class TestPods(BaseTest):
+    timeout = 120
+
+    def test_binaries_are_present(self):
+        self.launch_and_wait(set_up_ssh_connection=False)
+        session = self.ssh(self.ssh_key, self.ssh_user)
+        res = session.cmd("ls /opt/radar")
+        self.assertEqual(res.exit_status, 0)
+        self.assertTrue("radar_agent" in res.stdout.decode())
+        self.assertTrue("watchdog" in res.stdout.decode())
 
