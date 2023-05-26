0b3360097ba9b63ff7b9142a9ddb114a2b9b7d95
Rafael Mendes <rafael@exactlylabs.com>
2023-05-26 19:16:38 -0300

    install python and other dependencies

--- pods_agent/pods_os/scripts/test_setup.sh
+++ pods_agent/pods_os/scripts/test_setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-$ROOT_DIR=$SCRIPT_DIR/../
+ROOT_DIR=$SCRIPT_DIR/../
 
 set -e
+
+apt update && apt install -y curl python3 python3-pip git tcpdump netcat iproute2 arping
 
 curl -o qemu_python.tar.gz https://gitlab.com/qemu-project/qemu/-/archive/stable-7.2/qemu-stable-7.2.tar.gz?path=python
 tar -xvf qemu_python.tar.gz
