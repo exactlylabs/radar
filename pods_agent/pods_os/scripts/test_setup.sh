8d6552828c48a00291a0e3b8a700b71ffa813da0
Rafael Mendes <rafael@exactlylabs.com>
2023-05-26 19:04:58 -0300

    avocado run tests step added

--- pods_agent/pods_os/scripts/test_setup.sh
+++ pods_agent/pods_os/scripts/test_setup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+$ROOT_DIR=$SCRIPT_DIR/../
+
+set -e
+
 curl -o qemu_python.tar.gz https://gitlab.com/qemu-project/qemu/-/archive/stable-7.2/qemu-stable-7.2.tar.gz?path=python
 tar -xvf qemu_python.tar.gz
-pip3 install -r ./requirements.txt
+pip3 install -r $ROOT_DIR/requirements.txt
 avocado vt-bootstrap --yes-to-all
+
+ssh-keygen -q -t rsa -N '' -f $ROOT_DIR/tests/keys/id_rsa <<<y > /dev/null 2>&1
