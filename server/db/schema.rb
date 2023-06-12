8a71329123b61dcf38e21287327fde3618ca22de
Ignacio Grasso <ignaciograsso@Ignacios-MacBook-Pro.local>
2023-06-12 13:23:27 -0300

    alter model on backend to support before and after position info

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -13,10 +13,8 @@
 ActiveRecord::Schema.define(version: 2023_06_12_164917) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pageinspect"
   enable_extension "plpgsql"
   enable_extension "postgis"
-  enable_extension "tablefunc"
 
   create_table "accounts", force: :cascade do |t|
     t.integer "account_type", default: 0, null: false
@@ -297,8 +295,6 @@ ActiveRecord::Schema.define(version: 2023_06_12_164917) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["aggregate_type", "aggregate_id"], name: "index_events_on_aggregate"
-    t.index ["aggregate_type", "timestamp"], name: "test_events_aggregate_type_timestamp", order: { timestamp: :desc }
-    t.index ["timestamp"], name: "test_events_timestamp", order: :desc
     t.index ["version", "aggregate_id", "aggregate_type"], name: "index_events_on_version_and_aggregate_id_and_aggregate_type", unique: true
   end
 
@@ -356,9 +352,9 @@ ActiveRecord::Schema.define(version: 2023_06_12_164917) do
     t.boolean "test_requested", default: false
     t.string "state"
     t.string "county"
+    t.boolean "manual_lat_long", default: false
     t.string "state_fips"
     t.string "county_fips"
-    t.boolean "manual_lat_long", default: false
     t.boolean "automatic_location", default: false
     t.integer "account_id"
     t.float "download_avg"
@@ -370,7 +366,6 @@ ActiveRecord::Schema.define(version: 2023_06_12_164917) do
     t.boolean "online", default: false
     t.boolean "notified_when_online", default: false
     t.index ["account_id"], name: "index_locations_on_account_id"
-    t.index ["account_id"], name: "test_locations_on_account_id"
     t.index ["created_by_id"], name: "index_locations_on_created_by_id"
     t.index ["location_group_id"], name: "index_locations_on_location_group_id"
   end
@@ -400,22 +395,14 @@ ActiveRecord::Schema.define(version: 2023_06_12_164917) do
     t.float "loss_rate"
     t.geography "lonlat", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}
     t.index ["account_id", "processed_at"], name: "index_measurements_on_account_id_and_processed_at", order: { processed_at: :desc }
-    t.index ["account_id", "processed_at"], name: "test_measurements_account_processed_at_btree", order: { processed_at: :desc }
     t.index ["account_id"], name: "index_measurements_on_account_id"
-    t.index ["account_id"], name: "test_measurements_account_btree"
     t.index ["autonomous_system_id"], name: "index_measurements_on_autonomous_system_id"
     t.index ["client_id"], name: "index_measurements_on_client_id"
-    t.index ["created_at"], name: "test_measurements_brin_created_at", using: :brin
-    t.index ["location_id", "processed_at"], name: "test_measurements_location_id_processed_at", order: { processed_at: :desc }
     t.index ["location_id"], name: "index_measurements_on_location_id"
     t.index ["lonlat"], name: "index_measurements_on_lonlat"
     t.index ["measured_by_id"], name: "index_measurements_on_measured_by_id"
     t.index ["processed_at", "location_id", "autonomous_system_id"], name: "idx_meas_filter_by_loc_and_isp", order: { processed_at: :desc }
-    t.index ["processed_at", "location_id", "autonomous_system_id"], name: "test_measurements_processed_at_location_id_autonomous_sys_id", order: { processed_at: :desc }
-    t.index ["processed_at", "location_id"], name: "test_measurements_location_id_processed_at_2", order: { processed_at: :desc }
     t.index ["processed_at"], name: "index_measurements_on_processed_at", order: :desc
-    t.index ["processed_at"], name: "measurements_brin_processed_at", using: :brin
-    t.index ["processed_at"], name: "test_measurements_processed_at", order: :desc
   end
 
   create_table "ndt7_diagnose_reports", force: :cascade do |t|
@@ -497,14 +484,6 @@ ActiveRecord::Schema.define(version: 2023_06_12_164917) do
     t.index ["parent_aggregate_id"], name: "index_study_aggregates_on_parent_aggregate_id"
   end
 
-  create_table "study_counties", id: false, force: :cascade do |t|
-    t.string "state"
-    t.string "state_code"
-    t.string "county"
-    t.string "fips"
-    t.integer "pop_2021"
-  end
-
   create_table "study_level_projections", force: :cascade do |t|
     t.datetime "timestamp"
     t.bigint "parent_aggregate_id"
@@ -534,10 +513,10 @@ ActiveRecord::Schema.define(version: 2023_06_12_164917) do
     t.index ["event_id"], name: "index_study_level_projections_on_event_id"
     t.index ["location_id"], name: "index_study_level_projections_on_location_id"
     t.index ["measurement_id"], name: "index_study_level_projections_on_measurement_id"
+    t.index ["metric_type", "timestamp"], name: "index_study_level_projections_on_metric_type_and_timestamp", order: { timestamp: :desc }
     t.index ["metric_type"], name: "index_study_level_projections_on_metric_type"
     t.index ["parent_aggregate_id"], name: "index_study_level_projections_on_parent_aggregate_id"
     t.index ["study_aggregate_id"], name: "index_study_level_projections_on_study_aggregate_id"
-    t.index ["timestamp"], name: "test_study_level_projs_timestamp", order: :desc
   end
 
   create_table "system_outages", force: :cascade do |t|
@@ -563,18 +542,6 @@ ActiveRecord::Schema.define(version: 2023_06_12_164917) do
     t.index ["old_client_version_id"], name: "index_update_groups_on_old_client_version_id"
     t.index ["old_watchdog_version_id"], name: "index_update_groups_on_old_watchdog_version_id"
     t.index ["watchdog_version_id"], name: "index_update_groups_on_watchdog_version_id"
-  end
-
-  create_table "us_counties", primary_key: "fips", id: :string, force: :cascade do |t|
-    t.string "name"
-    t.string "state_fips"
-    t.string "state"
-    t.string "state_code"
-  end
-
-  create_table "us_states", primary_key: "state_fips", id: :string, force: :cascade do |t|
-    t.string "state_code"
-    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
@@ -663,7 +630,7 @@ ActiveRecord::Schema.define(version: 2023_06_12_164917) do
   add_foreign_key "packages", "client_versions"
   add_foreign_key "shared_accounts", "accounts", column: "original_account_id"
   add_foreign_key "shared_accounts", "accounts", column: "shared_to_account_id"
-  add_foreign_key "snapshots", "events", on_delete: :cascade
+  add_foreign_key "snapshots", "events"
   add_foreign_key "study_aggregates", "autonomous_system_orgs"
   add_foreign_key "study_aggregates", "geospaces"
   add_foreign_key "study_aggregates", "study_aggregates", column: "parent_aggregate_id"
