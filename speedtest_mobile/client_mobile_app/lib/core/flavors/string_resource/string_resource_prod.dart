8ea94f99c7e9e43cf2da9dea13e06454072c4914
Ignacio Grasso <ignaciograsso@Ignacios-MacBook-Pro.local>
2023-06-14 09:10:44 -0300

    update prod web endpoint

--- speedtest_mobile/client_mobile_app/lib/core/flavors/string_resource/string_resource_prod.dart
+++ speedtest_mobile/client_mobile_app/lib/core/flavors/string_resource/string_resource_prod.dart
@@ -15,7 +15,7 @@ class StringResourceProd implements IStringResource {
 
   @override
   String WEB_ENDPOINT =
-      'https://speedtest.exactlylabs.com/?webviewMode=true&tab=2&noZoomControl=true&global=true&client_id=1';
+      'https://speed.radartoolkit.com/?webviewMode=true&tab=2&noZoomControl=true&global=true&client_id=1';
 
   @override
   String ENVIRONMENT = 'production';
