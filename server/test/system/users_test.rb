f45bf4362928e34a76e06f3c8363636bb24b85b8
Rafael Mendes <rafael@exactlylabs.com>
2023-06-16 18:51:15 -0300

    user login/logout flow tests

---
+++ server/test/system/users_test.rb
@@ -0,0 +1,53 @@
+require "application_system_test_case"
+
+class UsersTest < ApplicationSystemTestCase
+  # Usefull links:
+  # https://www.rubydoc.info/gems/capybara/Capybara/Minitest/Assertions < assertions on the elements in the browser
+  # https://guides.rubyonrails.org/v6.1/testing.html#system-testing
+
+  setup do
+    now = Time.now
+    UsersAccount.create!(user: users(:user1), account: accounts(:root), joined_at: now, invited_at: now)
+  end
+
+  test "anonymous user redirected to log in" do
+    visit root_path
+
+    assert_selector "h1", text: "Log in"
+  end
+
+  test "user log in" do
+    visit new_user_session_path
+
+    fill_in "user[email]", with: users(:user1).email
+    fill_in "user[password]", with: "user1test1234"
+
+    click_on "Log in"
+
+    assert_current_path dashboard_path
+  end
+
+  test "wrong user credentials" do
+    visit new_user_session_path
+
+    fill_in "user[email]", with: "notexistingemail@e.com"
+    fill_in "user[password]", with: "something"
+
+    click_on "Log in"
+
+    assert_selector "div", text: "Invalid Email or password."
+  end
+
+  test "user log out" do
+    sign_in users(:user1)
+
+    visit dashboard_path
+
+    assert_current_path dashboard_path
+
+    page.find(id: "sidebar-profile-toggle").click
+    click_link("sign-out-link")
+
+    assert_current_path new_user_session_path
+  end
+end
