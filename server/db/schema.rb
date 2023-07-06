4d1d66ecf094ddc272d2bfe347c8bde060070bd3
iuke <edamm@itba.edu.ar>
2023-07-06 12:20:48 -0300

    added migration for user data cap persistent config

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_12_164917) do
+ActiveRecord::Schema.define(version: 2023_07_06_121902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -571,6 +571,7 @@ ActiveRecord::Schema.define(version: 2023_06_12_164917) do
     t.datetime "locked_at"
     t.boolean "super_user", default: false
     t.string "pending_downloads", array: true
+    t.string "data_cap_unit", default: "GB"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
