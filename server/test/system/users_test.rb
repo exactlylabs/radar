362ed2463b3c3f70340133c745cce53b8903d76f
Rafael Mendes <rafael@exactlylabs.com>
2023-07-12 18:30:52 -0300

    fix for user tests

--- server/test/system/users_test.rb
+++ server/test/system/users_test.rb
@@ -1,9 +1,12 @@
 require "application_system_test_case"
+require 'selenium-webdriver'
 
 class UsersTest < ApplicationSystemTestCase
   # Useful links:
   # https://www.rubydoc.info/gems/capybara/Capybara/Minitest/Assertions < assertions on the elements in the browser
   # https://guides.rubyonrails.org/v6.1/testing.html#system-testing
+
+  wait = Selenium::WebDriver::Wait.new(timeout: 10)
 
   setup do
   end
@@ -20,7 +23,11 @@ class UsersTest < ApplicationSystemTestCase
     fill_in "user[email]", with: users(:user1).email
     fill_in "user[password]", with: "user1test1234"
 
+    previous_path = page.current_path
+
     click_on "Log in"
+
+    wait.until { page.current_path != previous_path }
 
     assert_current_path dashboard_path
   end
@@ -43,8 +50,10 @@ class UsersTest < ApplicationSystemTestCase
 
     assert_current_path dashboard_path
 
+    previous_path = page.current_path
     page.find(id: "sidebar--profile-popover-regular").click
     click_link("sign-out-link")
+    wait.until { page.current_path != previous_path }
 
     assert_current_path new_user_session_path
   end
