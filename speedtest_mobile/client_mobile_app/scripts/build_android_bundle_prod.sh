47158bc544de8d4954941f15cd1eecf98bcf5a70
Ignacio Grasso <ignaciograsso@Ignacios-MacBook-Pro.local>
2023-07-17 13:55:05 -0300

    added config for stg and prod

---
+++ speedtest_mobile/client_mobile_app/scripts/build_android_bundle_prod.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Build and move to the workspace directory
+#
+#############################################
+set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BASE_URL=${BASE_URL:-https://pods.radartoolkit.com}
+WORKSPACE=${SCRIPT_DIR}/../workspace
+APPDIR=${SCRIPT_DIR}/..
+BUILD=${BUILD:-$(date +%s)}
+( cd ${APPDIR} && flutter build appbundle --build-number ${BUILD} --release --flavor prod -t lib/main_prod.dart --dart-define=BASE_URL=$BASE_URL )
+
+mkdir -p ${WORKSPACE}/android/prod
+
+export VERSION_NAME=$(cat ${APPDIR}/pubspec.yaml | sed -nre 's/version: ([0-9]+\.[0-9]+\.[0-9]+)\+[0-9]+/\1/p')
+cp ${SCRIPT_DIR}/../build/app/outputs/bundle/prodRelease/app-prod-release.aab ${WORKSPACE}/android/prod/app_${VERSION_NAME}+${BUILD}-release.aab
