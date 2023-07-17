02971dc7e511bcd1fc40eb1a93aff69b26107f39
iuke <edamm@itba.edu.ar>
2023-07-17 17:22:16 -0300

    wip: networks

--- server/app/helpers/application_helper.rb
+++ server/app/helpers/application_helper.rb
@@ -18,6 +18,10 @@ module ApplicationHelper
     policy_scope(Client).where(location: nil).count > 0
   end
 
+  def are_there_unassigned_pods?
+    policy_scope(Client).where(location: nil).count > 0
+  end
+
   def is_super_user_disabled?
     possible_cookie = cookies[:radar_super_user_disabled]
     possible_cookie.present? && possible_cookie == "true"
