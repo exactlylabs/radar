d9b0fec3d5023a48bd8bb68a03e956b2f784b0de
Ignacio Grasso <ignaciograsso@Ignacios-MacBook-Pro.local>
2023-07-17 17:20:59 -0300

    added missing changes on stg and prod .sh files

--- speedtest_mobile/client_mobile_app/scripts/build_android_apk_staging.sh
+++ speedtest_mobile/client_mobile_app/scripts/build_android_apk_staging.sh
@@ -9,7 +9,7 @@ BASE_URL=${BASE_URL:-https://radar-staging.exactlylabs.com}
 WORKSPACE=${SCRIPT_DIR}/../workspace
 APPDIR=${SCRIPT_DIR}/..
 BUILD=${BUILD:-$(date +%s)}
-( cd ${APPDIR} && flutter build appbundle --build-number ${BUILD} --release --flavor staging -t lib/main_staging.dart --dart-define=BASE_URL=$BASE_URL )
+( cd ${APPDIR} && flutter build apk --build-number ${BUILD} --release --flavor staging -t lib/main_staging.dart --dart-define=BASE_URL=$BASE_URL )
 
 export VERSION_NAME=$(cat ${APPDIR}/pubspec.yaml | sed -nre 's/version: ([0-9]+\.[0-9]+\.[0-9]+)\+[0-9]+/\1/p')
 mkdir -p ${WORKSPACE}/android/staging/${VERSION_NAME}+${BUILD}/
