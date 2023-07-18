7dcda76e8c1a1b6d3d91811a301b6587827f94da
iuke <edamm@itba.edu.ar>
2023-07-18 16:45:35 -0300

    PR changes

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -14,8 +14,8 @@ class User < ApplicationRecord
   has_many :measurements
   has_many :users_accounts
   has_many :accounts, through: :users_accounts
-  has_many :feature_flags_users
-  has_many :feature_flags, through: :feature_flags_users
+  has_and_belongs_to_many :feature_flags_users
+  has_and_belongs_to_many :feature_flags, through: :feature_flags_users
 
   after_save :check_pending_downloads
 
