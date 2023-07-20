a31f80dba05e13c1e81ec681d9c174f9712f497d
Ignacio Grasso <ignaciograsso@Ignacios-MacBook-Pro.local>
2023-07-20 11:04:20 -0300

    completed ttac-1648 and ttac-1765

--- speedtest/src/components/Frame/Footer.jsx
+++ speedtest/src/components/Frame/Footer.jsx
@@ -9,13 +9,23 @@ import radarLogoDark from '../../assets/radar-logo-dark.png';
 import {useViewportSizes} from "../../hooks/useViewportSizes";
 
 const footerStyle = {
-  height: 173,
   backgroundColor: DEFAULT_FOOTER_BACKGROUND_COLOR,
   boxShadow: `0 0 10px 0 ${FOOTER_BOX_SHADOW}`
 };
 
 const contentWrapperStyle = {
   width: '90%',
+  maxWidth: '1200px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  alignItems: 'stretch',
+  // padding: '30px 0',
+  margin: '0 auto',
+};
+
+const topContainerWrapperStyle = {
+  width: '100%',
   maxWidth: '1200px',
   display: 'flex',
   flexDirection: 'row',
@@ -57,6 +67,23 @@ const leftSideBottomRowStyle = {
   width: '100%',
   fontSize: 14,
   color: DEFAULT_FOOTER_FONT_COLOR,
+};
+
+const bottomParagraphStyle = {
+  width: '100%',
+  fontSize: 14,
+  lineHeight: '21px',
+  color: DEFAULT_FOOTER_FONT_COLOR,
+  paddingBottom: 36,
+};
+
+const mobileBottomParagraphStyle = {
+  width: '100%',
+  fontSize: 14,
+  color: DEFAULT_FOOTER_FONT_COLOR,
+  lineHeight: '21px',
+  textAlign: 'center',
+  paddingBottom: 40,
 };
 
 const mobileFooterStyle = {
@@ -145,34 +172,38 @@ const Footer = () => {
       </a>
       <FooterHorizontalDivider />
       <div style={copyrightStyle}>{`Copyright © ${new Date().getFullYear()} Radar. All Rights Reserved`}</div>
+      <div style={mobileBottomParagraphStyle}>This project was developed as part of the Telehealth Broadband Pilot Program.<br/>The Telehealth Broadband Pilot Program is made possible by grant #GA540183 from the Office for the Advancement of Telehealth, Health Resources and Services Administration, DHHS.</div>
     </div>
     :
     <div style={footerStyle}>
       <div style={contentWrapperStyle}>
-        <div style={leftSideContainerStyle}>
-          <div className={'speedtest--bold'} style={leftSideTopRowStyle}>
-            <img src={radarLogoDark} alt={'Radar-logo-dark'} width={100} height={24} style={footerLogoStyle} />
-            <a className={'speedtest--bold speedtest--regular-link--hoverable'}
-               style={footerLinkStyle(isSmallSizeScreen, isMediumSizeScreen)}
-               href={'https://radartoolkit.com/privacy-policy'}
-               target={'_blank'}
-            >Privacy Policy</a>
+        <div style={topContainerWrapperStyle}>
+          <div style={leftSideContainerStyle}>
+            <div className={'speedtest--bold'} style={leftSideTopRowStyle}>
+              <img src={radarLogoDark} alt={'Radar-logo-dark'} width={100} height={24} style={footerLogoStyle} />
+              <a className={'speedtest--bold speedtest--regular-link--hoverable'}
+                style={footerLinkStyle(isSmallSizeScreen, isMediumSizeScreen)}
+                href={'https://radartoolkit.com/privacy-policy'}
+                target={'_blank'}
+              >Privacy Policy</a>
+            </div>
+            <div style={leftSideBottomRowStyle}>
+              <div>{`Copyright © ${new Date().getFullYear()} Radar. All Rights Reserved`}</div>
+            </div>
           </div>
-          <div style={leftSideBottomRowStyle}>
-            <div>{`Copyright © ${new Date().getFullYear()} Radar. All Rights Reserved`}</div>
+          <div style={rightSideContainerStyle}>
+            <a style={linkWithRightMargin} href={'https://anthc.org'} target={'_blank'}>
+              <img src={anthcLogo} alt={'ANTHC-logo'} width={110} height={30} />
+            </a>
+            <a style={linkWithRightMargin} href={'https://exactlylabs.com'} target={'_blank'}>
+              <img src={exactlyLogo} alt={'exactly labs logo'} width={110} height={24}/>
+            </a>
+            <a style={stylessLink} href={'https://anthc.org'} target={'_blank'}>
+              <img src={xlabLogo} alt={'XLab logo'} width={32} height={38}/>
+            </a>
           </div>
         </div>
-        <div style={rightSideContainerStyle}>
-          <a style={linkWithRightMargin} href={'https://anthc.org'} target={'_blank'}>
-            <img src={anthcLogo} alt={'ANTHC-logo'} width={110} height={30} />
-          </a>
-          <a style={linkWithRightMargin} href={'https://exactlylabs.com'} target={'_blank'}>
-            <img src={exactlyLogo} alt={'exactly labs logo'} width={110} height={24}/>
-          </a>
-          <a style={stylessLink} href={'https://anthc.org'} target={'_blank'}>
-            <img src={xlabLogo} alt={'XLab logo'} width={32} height={38}/>
-          </a>
-        </div>
+        <p style={bottomParagraphStyle}>This project was developed as part of the Telehealth Broadband Pilot Program.<br/>The Telehealth Broadband Pilot Program is made possible by grant #GA540183 from the Office for the Advancement of Telehealth, Health Resources and Services Administration, DHHS.</p>
       </div>
     </div>
 };
