1b13bd061da48f31d0bc6b9884965a667349ab21
Ignacio Grasso <ignaciograsso@Ignacios-MacBook-Pro.local>
2023-07-20 11:28:31 -0300

    completed ttac-1649

--- broadband_mapping/frontend/src/components/Frame/styles/SecondLevelFooter.style.ts
+++ broadband_mapping/frontend/src/components/Frame/styles/SecondLevelFooter.style.ts
@@ -3,20 +3,29 @@ import {DEFAULT_CONTEXT_DIVIDER, FOOTER_TEXT} from "../../../styles/colors";
 
 const secondLevelFooterStyle: CSSProperties = {
   width: '100%',
-  height: '105px',
+  height: '176px',
   borderTop: `1px solid ${DEFAULT_CONTEXT_DIVIDER}`,
   display: 'flex',
   flexDirection: 'row',
-  alignItems: 'center',
+  alignItems: 'start',
+  padding: '32px 0px 32px 0px',
 }
 
 const leftSideContainerStyle: CSSProperties = {
   width: 'max-content',
   marginLeft: '60px',
-}
+  paddingTop: '6px',
+  }
 
 const copyrightTextStyle: CSSProperties = {
   fontSize: '15px',
+  color: FOOTER_TEXT,
+}
+
+const devInfoTextStyle: CSSProperties = {
+  fontSize: '15px',
+  maxWidth: '629px',
+  paddingTop: 24,
   color: FOOTER_TEXT,
 }
 
@@ -51,6 +60,7 @@ export const styles = {
   SecondLevelFooter: secondLevelFooterStyle,
   LeftSideContainer: leftSideContainerStyle,
   CopyrightText: copyrightTextStyle,
+  DevInfoText: devInfoTextStyle,
   RightSideContainer: rightSideContainerStyle,
   FooterIcon: footerIconStyle,
   ExactlyIcon: exactlyIconStyle,
