25d9455b081de6340f4d505fb1c311c173105346
iuke <edamm@itba.edu.ar>
2023-07-25 13:05:28 -0300

    fix progress on export downloads

--- server/app/mailers/exports_mailer.rb
+++ server/app/mailers/exports_mailer.rb
@@ -6,4 +6,10 @@ class ExportsMailer < ApplicationMailer
     attachments.inline["download-ready-icon.png"] = File.read("#{Rails.root}/app/assets/images/download-ready-icon.png")
     mail(to: @user.email, subject: 'Your download is ready')
   end
+
+  def export_error_email
+    @user = params[:user]
+    attachments.inline["new-logo.png"] = File.read("#{Rails.root}/app/assets/images/new-logo.png")
+    mail(to: @user.email, subject: 'There has been an error processing your download')
+  end
 end
