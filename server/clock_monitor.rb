8e4f338725b59533c57120e0fa26f109e9e4efd8
Rafael Mendes <rafael@exactlylabs.com>
2023-07-27 20:03:34 -0300

    clock_monitor.rb

---
+++ server/clock_monitor.rb
@@ -0,0 +1,61 @@
+$stdout.sync = true
+require 'sentry-ruby'
+require 'redis'
+require 'rufus-scheduler'
+require 'time'
+require './lib/events_notifier/discord_notifier.rb'
+
+HEARTBEAT_TOLERANCE = 300 # 5 minutes
+
+
+scheduler = Rufus::Scheduler.new
+$notifier = DiscordNotifier.new ENV["DISCORD_WEBHOOK_URL"], nil, nil
+
+$last_alerted = nil
+$state = :healthy
+
+
+def verify_heartbeat
+  redis = Redis.new url: ENV['REDIS_URL'] || 'redis://localhost:6379/0'
+  last_beat = redis.get("radar::application_healthmonitor")
+  last_beat = DateTime.strptime(last_beat, "%Y-%m-%d %H:%M:%S %z") if last_beat
+  if last_beat.nil? || Time.now - last_beat.to_time > HEARTBEAT_TOLERANCE
+    puts "Application heartbeat is missing"
+    $state = :unhealthy
+    if $last_alerted.nil? || Time.now - $last_alerted > 3600 # 1 hour
+      $notifier.notify_heartbeat_missing(last_beat)
+      $last_alerted = Time.now
+    end
+  elsif $state != :healthy
+    $state = :healthy
+    $last_alerted = nil
+    puts "Application heartbeat recovered"
+    $notifier.notify_heartbeat_recovered
+  end
+end
+
+scheduler.every '5s', overlap: false do
+  begin
+    puts "Verifying heartbeat..."
+    verify_heartbeat
+  rescue => e
+    Sentry.capture_exception(e)
+    raise e
+  end
+end
+
+Sentry.init do |config|
+  config.dsn = 'https://824cb73d4b5149459eb889296687f94f@o1197382.ingest.sentry.io/6320151'
+  config.breadcrumbs_logger = [:sentry_logger, :http_logger]
+
+  config.release = 'clock-monitor@1.0.0'
+end
+
+begin
+  scheduler.join
+rescue Interrupt
+
+rescue SignalException
+
+end
+
