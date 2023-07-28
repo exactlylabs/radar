9d88d449ca3cb35941013e48f0d01c8ff9666316
Ignacio Grasso <ignaciograsso@Ignacios-MacBook-Pro.local>
2023-07-28 18:43:45 -0300

    add autonomous system info to client speed json

--- server/app/models/client_speed_test.rb
+++ server/app/models/client_speed_test.rb
@@ -2,4 +2,17 @@ class ClientSpeedTest < ApplicationRecord
   has_one_attached :result
   belongs_to :widget_client, foreign_key: 'tested_by'
   belongs_to :autonomous_system, optional: true
+
+  def as_json(options = nil)
+    super(include: {
+      autonomous_system: {
+        except: %i[id autonomous_system_org_id created_at updated_at],
+        include: {
+          autonomous_system_org: {
+            except: %i[id created_at updated_at]
+          }
+        }
+      }
+    })
+  end
 end
