5ddfdb2e3e2327e9cf148400d754d4eedd003fe9
iuke <edamm@itba.edu.ar>
2023-08-08 18:16:15 -0300

    multiple bugfixes

--- server/app/controllers/users/registrations_controller.rb
+++ server/app/controllers/users/registrations_controller.rb
@@ -4,13 +4,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   prepend_before_action :authenticate_scope!, only: [:edit, :edit_email, :update, :update_email, :destroy]
   prepend_before_action :set_minimum_password_length, only: [:new, :edit, :edit_email]
   before_action :set_invite, only: [:render_invite_sign_up, :render_invite_sign_in, :sign_from_invite, :create_from_invite]
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
-
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
 
   # POST /register
   def create
@@ -272,40 +265,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
       raise ActiveRecord::RecordNotFound.new("Couldn't find Invite", Invite.name)
     end
   end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
