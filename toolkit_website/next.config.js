7141a88074246e73f87bfca22e5dc7c65aadd813
Ignacio Grasso <ignaciograsso@Ignacios-MacBook-Pro.local>
2023-10-23 20:13:38 -0300

    handle server url based on the environment

--- toolkit_website/next.config.js
+++ toolkit_website/next.config.js
@@ -1,16 +1,17 @@
+const destination_base_url = process.env.NEXT_PUBLIC_APP_ENV === 'production' ? 'https://pods.radartoolkit.com' : 'https://pods.staging.radartoolkit.com'
 module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/TTUHSC',
-          destination: 'https://pods.radartoolkit.com/TTUHSC',
-          permanent: false,
-        },
-        {
-            source: '/TTUHSC/:slug',
-            destination: 'https://pods.radartoolkit.com/TTUHSC/:slug',
-            permanent: false,
-          },
-      ]
-    },
-  }
+  async redirects() {
+    return [
+      {
+        source: '/TTUHSC',
+        destination:  destination_base_url + '/TTUHSC',
+        permanent: false,
+      },
+      {
+        source: '/TTUHSC/:slug',
+        destination: destination_base_url  + '/TTUHSC/:slug',
+        permanent: false,
+      },
+    ]
+  },
+}
