0f5458579ed75a7e5520e98423164d3254159cc3
Rafael Mendes <rafael@exactlylabs.com>
2024-01-17 11:32:27 -0300

    fix

--- pods_agent/services/sysinfo/sysinfomanager.go
+++ pods_agent/services/sysinfo/sysinfomanager.go
@@ -243,8 +243,8 @@ func (si SysInfoManager) EnsureTailscale() error {
 		log.Println("sysinfo.SysInfoManager#EnsureTailscale: Tailscale not installed, installing it")
 
 		// There's been an error where apt update fails. Removing old sources.list.d file might fix it.
-		si.runCommand(exec.Command("cp", "/var/backups/dpkg.status.0", "/var/lib/dpkg/status"))
-		si.runCommand(exec.Command("apt", "clean", "&&", "apt", "update"))
+		// si.runCommand(exec.Command("cp", "/var/backups/dpkg.status.0", "/var/lib/dpkg/status"))
+		// si.runCommand(exec.Command("apt", "clean", "&&", "apt", "update"))
 		_, err := si.runCommand(
 			exec.Command("bash", "-c", "curl -fsSL https://tailscale.com/install.sh | sh"))
 
