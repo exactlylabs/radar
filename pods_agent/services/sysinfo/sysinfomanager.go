2bd672860fb2a69212156ac0ad482f1516f92b5e
Rafael Mendes <rafael@exactlylabs.com>
2024-01-17 11:12:17 -0300

    EnsureTailscale clearing apt mergeList

--- pods_agent/services/sysinfo/sysinfomanager.go
+++ pods_agent/services/sysinfo/sysinfomanager.go
@@ -245,6 +245,8 @@ func (si SysInfoManager) EnsureTailscale() error {
 		// There's been an error where apt update fails. Removing old sources.list.d file might fix it.
 		os.Remove("/usr/share/keyrings/tailscale-archive-keyring.gpg")
 		os.Remove("/etc/apt/sources.list.d/tailscale.list")
+		si.runCommand(exec.Command("rm", "-r", "/var/lib/dpkg/status"))
+		si.runCommand(exec.Command("apt", "clean", "&&", "apt", "update"))
 		_, err := si.runCommand(
 			exec.Command("bash", "-c", "curl -fsSL https://tailscale.com/install.sh | sh"))
 
