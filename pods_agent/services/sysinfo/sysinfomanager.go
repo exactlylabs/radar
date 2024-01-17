4b053c5a134978180a80594eb84a51e0b7d0bc7d
Rafael Mendes <rafael@exactlylabs.com>
2024-01-17 12:07:04 -0300

    attempt #4

--- pods_agent/services/sysinfo/sysinfomanager.go
+++ pods_agent/services/sysinfo/sysinfomanager.go
@@ -245,6 +245,8 @@ func (si SysInfoManager) EnsureTailscale() error {
 		// There's been an error where apt update fails. Removing old sources.list.d file might fix it.
 		// si.runCommand(exec.Command("cp", "/var/backups/dpkg.status.0", "/var/lib/dpkg/status"))
 		// si.runCommand(exec.Command("apt", "clean", "&&", "apt", "update"))
+		si.runCommand(exec.Command("dpkg", "-P", "tailscale"))
+		si.runCommand(exec.Command("dpkg", "-P", "tailscale-archive-keyring"))
 		_, err := si.runCommand(
 			exec.Command("bash", "-c", "curl -fsSL https://tailscale.com/install.sh | sh"))
 
@@ -253,8 +255,7 @@ func (si SysInfoManager) EnsureTailscale() error {
 			if metadata != nil && strings.Contains((*metadata)["stderr"].(string), "Unable to acquire the dpkg frontend lock") {
 				return nil
 			}
-			// return errors.W(err)
-			return nil
+			return errors.W(err)
 		}
 		log.Println("sysinfo.SysInfoManager#EnsureTailscale: Tailscale installed, allowing auto-update")
 		_, err = si.runCommand(
