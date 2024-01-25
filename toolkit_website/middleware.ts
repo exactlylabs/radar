53c9bd561339176221d27db177242be939911194
iuke <edamm@itba.edu.ar>
2024-01-25 17:11:07 +0100

    added case insensitive middleware + updated Node/Next

--- toolkit_website/middleware.ts
+++ toolkit_website/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 const DESTINATION_BASE_URL = process.env.NEXT_PUBLIC_APP_ENV === 'production' ? 'https://pods.radartoolkit.com' : 'https://pods.staging.radartoolkit.com'
 
 export function middleware(request: NextRequest) {
-  const tbpMatcher = /(T|t){1}(B|b){1}(P|p){1}/;
+  const tbpMatcher = /tbp/i;
   if (tbpMatcher.test(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL('/TBP?origin=toolkit', DESTINATION_BASE_URL));
   }
