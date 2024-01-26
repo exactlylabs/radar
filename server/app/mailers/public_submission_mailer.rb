a15fb2dab783557187a666b5a527d1b9376e6ced
iuke <edamm@itba.edu.ar>
2024-01-26 15:33:46 +0100

    Firefox specific fixes

--- server/app/mailers/public_submission_mailer.rb
+++ server/app/mailers/public_submission_mailer.rb
@@ -1,6 +1,6 @@
 class PublicSubmissionMailer < ApplicationMailer
   def new_submission
     @submission = params[:submission]
-    mail(to: "matt@exactlylabs.com", subject: 'New public page submission')
+    mail(to: ["matt@exactlylabs.com", "amanda@exactlylabs.com"], subject: 'New public page submission')
   end
 end
