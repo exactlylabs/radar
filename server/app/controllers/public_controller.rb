2a1e843ec3297bcbd7b52c801b8c15301338efd9
Rafael Mendes <rafael@exactlylabs.com>
2024-02-02 18:34:39 -0300

    new redirections from public page and marketing assets tracking

--- server/app/controllers/public_controller.rb
+++ server/app/controllers/public_controller.rb
@@ -86,7 +86,7 @@ class PublicController < PublicApplicationController
   end
 
   def send_event_to_amplitude
-    return unless Rails.env.production?
+    # return unless Rails.env.production?
     comes_from_toolkit = params[:origin] == 'toolkit'
     url = URI("https://api2.amplitude.com/2/httpapi")
     headers = {
@@ -101,6 +101,7 @@ class PublicController < PublicApplicationController
                    "ip": request.remote_ip,
                    "event_properties": {
                      "comes_from_toolkit": comes_from_toolkit,
+                     "marketing_asset": params[:r],
                    }
                  }],
     }.to_json
