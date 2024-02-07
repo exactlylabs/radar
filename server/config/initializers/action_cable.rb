4fc4d4d62f066cbd8427d8220526e9dfd6cfc386
Rafael Mendes <rafael@exactlylabs.com>
2024-02-07 15:36:04 -0300

    configured new ActionCable server called mobile_server, using the MobileConnection class

--- server/config/initializers/action_cable.rb
+++ server/config/initializers/action_cable.rb
@@ -4,3 +4,12 @@ CHANNELS = {
   notifications: 'notifications',
   watchdog_pub: 'watchdog_pub',
 }
+
+module ActionCable
+  # Declare a new ActionCable server for the mobile app to send data to.
+  module_function def mobile_server
+    @mobile_server ||= ActionCable::Server::Base.new
+    @mobile_server.config.connection_class = -> { ApplicationCable::MobileConnection }
+    @mobile_server
+  end
+end
