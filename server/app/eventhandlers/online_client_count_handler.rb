d77e2313b52378ec27c815edf635bfafb853bc20
Rafael Mendes <rafael@exactlylabs.com>
2024-02-09 18:03:38 -0300

    online_client_count_handler processing performance fixes

--- server/app/eventhandlers/online_client_count_handler.rb
+++ server/app/eventhandlers/online_client_count_handler.rb
@@ -1,48 +1,58 @@
 class OnlineClientCountHandler
+  MAX_QUEUE_SIZE = 50000
+  DEFAULT_BATCH_SIZE = 10000
+
   def initialize()
-    @last_events = {}
     @consumer_offset = ConsumerOffset.find_or_create_by!(consumer_id: "OnlineClientCountProjection")
+    @insertion_queue = []
+
+    # Cached data to avoid repeated DB queries
+    @last_projections = {}
+    @locations = {}
+    @last_client_event = {}
+    @ongoing_system_outage = nil
   end
 
   def aggregate!()
-    events = self.to_be_processed_events
-    if events.empty?
-      return
+    conn = ActiveRecord::Base.connection_pool.checkout
+    begin
+      @raw = conn.raw_connection
+      self._aggregate!
+    ensure
+      ActiveRecord::Base.connection_pool.checkin(conn)
     end
+  end
+
+  def _aggregate!()
+    events = self.to_be_processed_events
     events.each do |event|
       next if self.is_duplicated_event(event)
-      OnlineClientCountProjection.transaction do
-        begin
-          if event.aggregate_type == Client.name
-            self.handle_client_event! event
-          elsif event.aggregate_type == SystemOutage.name
-            self.handle_system_outage_event! event
-          elsif event.aggregate_type == Location.name
-            self.handle_location_event! event
-          end
-        rescue ActiveRecord::InvalidForeignKey
+      @ongoing_system_outage = SystemOutage.at(event.timestamp).exists? if @ongoing_system_outage.nil?
+      begin
+        if event.aggregate_type == Client.name
+          self.handle_client_event! event
+        elsif event.aggregate_type == SystemOutage.name
+          self.handle_system_outage_event! event
+        elsif event.aggregate_type == Location.name
+          self.handle_location_event! event
         end
+      rescue ActiveRecord::InvalidForeignKey
       end
       @consumer_offset.offset = event.id
-      @consumer_offset.save!
+      if @insertion_queue.length > MAX_QUEUE_SIZE
+        self.flush_insertion_queue
+      end
     end
+    self.flush_insertion_queue
     return nil
   end
 
   def handle_client_event!(event)
-    if event.snapshot.nil? || SystemOutage.at(event.timestamp).exists?
+    if event.snapshot.nil? || @ongoing_system_outage
       return
     end
     state = event.snapshot.state
-
-    last_count = OnlineClientCountProjection.latest_for state["account_id"], state["autonomous_system_id"], state["location_id"]
-    if last_count.nil?
-      last_count = OnlineClientCountProjection.new(
-        account_id: state["account_id"],
-        autonomous_system_id: state["autonomous_system_id"],
-        location_id: state["location_id"]
-      )
-    end
+    last_count = last_projection(state["account_id"], state["autonomous_system_id"], state["location_id"])
     case event.name
     when Client::Events::CREATED
       if state["online"]
@@ -51,7 +61,7 @@ class OnlineClientCountHandler
 
     when Client::Events::ACCOUNT_CHANGED
       if state["online"]
-        old_account_count = OnlineClientCountProjection.latest_for event.data["from"], state["autonomous_system_id"], state["location_id"]
+        old_account_count = self.last_projection(event.data["from"], state["autonomous_system_id"], state["location_id"])
         self.new_record!(last_count, event, increment=1)
         if old_account_count.present?
           self.new_record!(old_account_count, event, increment=-1)
@@ -60,7 +70,7 @@ class OnlineClientCountHandler
 
     when Client::Events::LOCATION_CHANGED
       if state["online"]
-        old_location_count = OnlineClientCountProjection.latest_for state["account_id"], state["autonomous_system_id"], event.data["from"]
+        old_location_count = self.last_projection(state["account_id"], state["autonomous_system_id"], event.data["from"])
         self.new_record!(last_count, event, increment=1)
         if old_location_count.present?
           self.new_record!(old_location_count, event, increment=-1)
@@ -69,7 +79,7 @@ class OnlineClientCountHandler
 
     when Client::Events::AS_CHANGED
       if state["online"]
-        old_as_count = OnlineClientCountProjection.latest_for state["account_id"], event.data["from"], state["location_id"]
+        old_as_count = self.last_projection(state["account_id"], event.data["from"], state["location_id"])
         self.new_record!(last_count, event, increment=1)
         if old_as_count.present?
           self.new_record!(old_as_count, event, increment=-1)
@@ -85,7 +95,10 @@ class OnlineClientCountHandler
   end
 
   def handle_system_outage_event!(event)
-    if event.name == SystemOutage::Events::FINISHED
+    if event.name == SystemOutage::Events::CREATED
+      @ongoing_system_outage = true
+    elsif event.name == SystemOutage::Events::FINISHED
+      @ongoing_system_outage = false
       outage = event.snapshot.state
       # Once a system outage is finished, we search for
       # pods whose state differs from the state before the outage.
@@ -162,29 +175,44 @@ class OnlineClientCountHandler
 
   private
 
-  def preload(current_timestamp)
-    Client.all.each do |client|
-      @last_events[client.id] = Event.from_aggregate(client).prior_to(current_timestamp).last
-    end
-  end
-
-  def last_event_for(client, timestamp)
-    @last_events[client.id] ||= Event.from_aggregate(client).prior_to(timestamp).last
-    @last_events[client.id]
-  end
-
-  def to_be_processed_events()
-    events = Event.where(
-        "id > ? AND (aggregate_type = ? OR aggregate_type = ? OR aggregate_type = ?)",
-        @consumer_offset.offset, Client.name, SystemOutage.name, Location.name
-    ).order('timestamp ASC, version ASC')
-  end
-
-  def is_duplicated_event(event)
-    if event.aggregate_type != Client.name || ![Client::Events::WENT_ONLINE, Client::Events::WENT_OFFLINE].include?(event.name)
+  def location_valid?(location_id)
+    return true if location_id.nil?
+    begin
+      @locations[location_id] ||= Location.unscoped.find(location_id)
+    rescue ActiveRecord::RecordNotFound
       return false
     end
-    return last_event_for(event.aggregate, event.timestamp)&.name == event.name
+    return @locations[location_id].present?
+  end
+
+  def push_to_queue(event, account_id, autonomous_system_id, location_id, online, total, total_in_service, incr)
+    return unless self.location_valid?(location_id)
+    @insertion_queue.push({
+      "timestamp" => event.timestamp,
+      "account_id" => account_id,
+      "autonomous_system_id" => autonomous_system_id,
+      "location_id" => location_id,
+      "online" => online,
+      "total" => total,
+      "total_in_service" => total_in_service,
+      "event_id" => event.id,
+      "incr" => incr
+    })
+  end
+
+  def last_projection(account_id, autonomous_system_id, location_id)
+    # Return the last projection for given parameters set.
+    #  It first tries to localize an already loaded object in memory otherwise it queries the DB and caches the result.
+    key = "#{account_id}-#{autonomous_system_id}-#{location_id}"
+    @last_projections[key] ||= OnlineClientCountProjection.latest_for account_id, autonomous_system_id, location_id
+    if @last_projections[key].nil?
+      @last_projections[key] = OnlineClientCountProjection.new(
+        account_id: account_id,
+        autonomous_system_id: autonomous_system_id,
+        location_id: location_id
+      )
+    end
+    return @last_projections[key]
   end
 
   def new_record!(previous_count, event, increment=0)
@@ -194,6 +222,60 @@ class OnlineClientCountHandler
     count.event_id = event.id
     count.online += increment
     count.incr = increment
-    count.save!
+    self.push_to_queue(event, count.account_id, count.autonomous_system_id, count.location_id, count.online, count.total, count.total_in_service, increment)
+    @last_projections["#{count.account_id}-#{count.autonomous_system_id}-#{count.location_id}"] = count
   end
+
+  def last_event_for(client, timestamp)
+    @last_client_event[client.id] ||= Event.from_aggregate(client).prior_to(timestamp).last
+    @last_client_event[client.id]
+  end
+
+  def to_be_processed_events(**opts)
+    batch_size = opts[:batch_size] || DEFAULT_BATCH_SIZE
+    Enumerator.new { |g|
+      events = Event.where(
+          "id > ? AND (aggregate_type = ? OR aggregate_type = ? OR aggregate_type = ?)",
+          @consumer_offset.offset, Client.name, SystemOutage.name, Location.name
+      ).order('timestamp ASC, version ASC').preload(:snapshot, :aggregate).find_each(batch_size: batch_size) do |event|
+        g.yield event
+        @last_client_event[event.aggregate_id] = event if event.aggregate_type == Client.name
+      end
+    }
+  end
+
+  def is_duplicated_event(event)
+    if event.aggregate_type != Client.name || ![Client::Events::WENT_ONLINE, Client::Events::WENT_OFFLINE].include?(event.name)
+      return false
+    end
+    return last_event_for(event.aggregate, event.timestamp)&.name == event.name
+  end
+
+  def flush_insertion_queue
+    if @insertion_queue.empty?
+      return
+    end
+    @raw.transaction do
+      Rails.logger.info "Flushing insertion queue with #{@insertion_queue.length} records"
+      t = Time.now
+      @consumer_offset.save!
+      @raw.exec(%{
+        COPY online_client_count_projections (timestamp, account_id, autonomous_system_id, location_id, online, total, total_in_service, event_id, incr)
+        FROM STDIN CSV
+      })
+      @insertion_queue.each do |row|
+        @raw.put_copy_data(
+          "#{row['timestamp']},#{row['account_id']},#{row['autonomous_system_id']},#{row['location_id']},#{row['online']},#{row['total']},#{row['total_in_service']},#{row['event_id']},#{row['incr']}\n")
+      end
+      @raw.put_copy_end
+      while res = @raw.get_result do
+        if res.result_status != 1
+          raise res.error_message
+        end
+      end
+      Rails.logger.info "Insertion took #{Time.now - t} seconds"
+    end
+    @insertion_queue = []
+  end
+
 end
