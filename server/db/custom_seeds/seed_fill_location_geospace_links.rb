f6105d7d0c36c381d0d91519725a9f28b53b714d
Rafael Mendes <rafael@exactlylabs.com>
2024-02-15 17:45:32 -0300

    fix seed

--- server/db/custom_seeds/seed_fill_location_geospace_links.rb
+++ server/db/custom_seeds/seed_fill_location_geospace_links.rb
@@ -1,6 +1,6 @@
 # Remove geospaces that don't match the location's lonlat and search for all that do match and link them to it.
 
-Location.each.do |location|
+Location.all.each do |location|
   location.geospaces.excluding_lonlat(location.lonlat).each do |geospace|
     location.geospaces.delete geospace
   end
