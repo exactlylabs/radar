028c0f513bc73b9cb51a433443fb4e3402bf18ec
Rafael Mendes <rafael@exactlylabs.com>
2024-02-15 15:43:14 -0300

    missing seed file

---
+++ server/db/custom_seeds/seed_fill_location_geospace_links.rb
@@ -0,0 +1,10 @@
+# Remove geospaces that don't match the location's lonlat and search for all that do match and link them to it.
+
+Location.each.do |location|
+  location.geospaces.excluding_lonlat(location.lonlat).each do |geospace|
+    location.geospaces.delete geospace
+  end
+  Geospace.containing_lonlat(location.lonlat).each do |geospace|
+    location.geospaces << geospace unless location.geospaces.include? geospace
+  end
+end
