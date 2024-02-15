2f44bc651694eedfc9d698aef771c255c0b073f8
Rafael Mendes <rafael@exactlylabs.com>
2024-02-15 17:47:53 -0300

    fill location geospace links also make sure to fill any missing lonlat fields

--- server/db/custom_seeds/seed_fill_location_geospace_links.rb
+++ server/db/custom_seeds/seed_fill_location_geospace_links.rb
@@ -1,6 +1,13 @@
 # Remove geospaces that don't match the location's lonlat and search for all that do match and link them to it.
 
 Location.all.each do |location|
+  if location.lonlat.nil? && location.longitude.present? && location.latitude.present?
+    location.lonlat = "POINT(#{location.longitude} #{location.latitude})"
+    location.save
+  elsif location.lonlat.nil?
+    next
+  end
+
   location.geospaces.excluding_lonlat(location.lonlat).each do |geospace|
     location.geospaces.delete geospace
   end
