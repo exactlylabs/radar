c5c889747af3372c7c0324ee7e8755deedf3ea70
Rafael Mendes <rafael@exactlylabs.com>
2024-02-15 22:51:09 -0300

    fix mobile actioncable server conflicting with the default server

--- server/config/initializers/action_cable.rb
+++ server/config/initializers/action_cable.rb
@@ -7,9 +7,14 @@ CHANNELS = {
 
 module ActionCable
   # Declare a new ActionCable server for the mobile app to send data to.
+  # This server works separate from the main one, so it will have its own worker pools
   module_function def mobile_server
-    @mobile_server ||= ActionCable::Server::Base.new
-    @mobile_server.config.connection_class = -> { ApplicationCable::MobileConnection }
-    @mobile_server
+    # We have to instantiate a new config, otherwise we'll share the same instance with the default server
+    # This new config points to the MobileConnection class, and we also have to set the logger,
+    #  otherwise it would be nil as it's set somewhere in Rails initialization phase.
+    config = ActionCable::Server::Configuration.new
+    config.connection_class = -> { ApplicationCable::MobileConnection }
+    config.logger = ActionCable.server.config.logger
+    @mobile_server ||= ActionCable::Server::Base.new(config: config)
   end
 end
