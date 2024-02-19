2e223bbc6914d5ea7070f5c245427f7c09c180a5
Rafael Mendes <rafael@exactlylabs.com>
2024-02-19 17:55:38 -0300

    refactored dashboard helpers to dynamically mount the query depending on the existing params, and also included SQL sanitization. Modified controllers to use the new methods

--- server/app/controllers/dashboard_controller.rb
+++ server/app/controllers/dashboard_controller.rb
@@ -37,6 +37,8 @@ class DashboardController < ApplicationController
         @show_ftue = cookie_is_turned_on && has_no_accounts
       end
     end
+    params = as_orgs_filters_params()
+    @filter_as_orgs = ActiveRecord::Base.connection.execute(DashboardHelper.get_as_orgs_sql(params[:account_ids], params[:from], params[:to], location_ids: params[:location_ids]))
   end
 
   def onboarding_step_1
@@ -69,38 +71,79 @@ class DashboardController < ApplicationController
   end
 
   def online_pods
-    sql = DashboardHelper.get_online_pods_sql
-    sql = replace_shared_params(sql)
-    sql = replace_online_pods_time_params(sql)
+    params = online_pods_params()
+    sql = DashboardHelper.get_online_pods_sql(params[:interval_type], params[:from], params[:to], params[:account_ids], as_org_ids: params[:as_org_ids], location_ids: params[:location_ids])
     @online_pods = ActiveRecord::Base.connection.execute(sql)
   end
 
   def download_speeds
-    sql = DashboardHelper.get_download_speed_sql
-    sql = replace_speed_time_params(sql)
+    params = download_speeds_params()
+    sql = DashboardHelper.get_download_speed_sql(params[:account_ids], params[:from], params[:to], as_org_ids: params[:as_org_ids], location_ids: params[:location_ids])
     @download_speeds = ActiveRecord::Base.connection.execute(replace_shared_params(sql))
   end
 
   def upload_speeds
-    sql = DashboardHelper.get_upload_speed_sql
-    sql = replace_speed_time_params(sql)
+    params = upload_speeds_params()
+    sql = DashboardHelper.get_upload_speed_sql(params[:account_ids], params[:from], params[:to], as_org_ids: params[:as_org_ids], location_ids: params[:location_ids])
     @upload_speeds = ActiveRecord::Base.connection.execute(replace_shared_params(sql))
   end
 
   def latency
-    sql = DashboardHelper.get_latency_sql
-    sql = replace_speed_time_params(sql)
+    params = latency_params()
+    sql = DashboardHelper.get_latency_sql(params[:account_ids], params[:from], params[:to], as_org_ids: params[:as_org_ids], location_ids: params[:location_ids])
     @latencies = ActiveRecord::Base.connection.execute(replace_shared_params(sql))
   end
 
   def data_usage
-    sql = DashboardHelper.get_usage_sql
-    sql = replace_shared_params(sql)
-    sql = replace_data_usage_time_params(sql)
+    params = data_usage_params()
+    sql = DashboardHelper.get_usage_sql(params[:interval_type], params[:from], params[:to], params[:account_ids], as_org_ids: params[:as_org_ids], location_ids: params[:location_ids])
     @usage = ActiveRecord::Base.connection.execute(sql)
   end
 
   private
+
+  def as_orgs_filters_params()
+    common_filter_params.merge(time_filter_params)
+  end
+
+  def online_pods_params()
+    common_filter_params.merge(time_filter_params).merge(interval_type: 'd')
+  end
+
+  def download_speeds_params()
+    common_filter_params.merge(time_filter_params)
+  end
+
+  def upload_speeds_params()
+    common_filter_params.merge(time_filter_params)
+  end
+
+  def latency_params()
+    common_filter_params.merge(time_filter_params)
+  end
+
+  def data_usage_params()
+    common_filter_params.merge(time_filter_params).merge(interval_type: 'd')
+  end
+
+  def time_filter_params()
+    days = params[:days] || 30
+    start_time = params[:start].present? ? Time.at(params[:start].to_i / 1000) : (Time.now - days.to_i.days)
+    end_time = params[:end].present? ? Time.at(params[:end].to_i / 1000) : Time.now
+    {from: start_time, to: end_time}
+  end
+
+  def common_filter_params()
+    account_ids = params[:account_id].present? ? policy_filter_ids(Account, params[:account_id]) : current_account.is_all_accounts? ? policy_scope(Account).pluck(:id).join(',') : [current_account.id]
+    as_org_ids = params[:isp_id].present? ? policy_filter_ids(AutonomousSystemOrg, params[:isp_id]) : nil
+    location_ids = params[:network_id].present? ? policy_filter_ids(Location, params[:network_id]) : nil
+    {account_ids: account_ids, as_org_ids: as_org_ids, location_ids: location_ids}
+  end
+
+  def policy_filter_ids(model, ids)
+    policy_scope(model).where(id: ids).pluck(:id).join(',')
+  end
+
   def replace_online_pods_time_params(sql)
     days = params[:days].present? ? params[:days] : 30
 
--- server/app/helpers/dashboard_helper.rb
+++ server/app/helpers/dashboard_helper.rb
@@ -17,12 +17,12 @@ module DashboardHelper
     "#{start_date.strftime('%b %d, %Y')} - #{end_date.strftime('%b %d, %Y')}"
   end
 
-  def self.get_online_pods_sql
-    %{
+  def self.get_online_pods_sql(interval_type, from, to, account_ids, as_org_ids: nil, location_ids: nil)
+    sql = %{
       with base_series AS (
             SELECT
-              date_trunc('$interval_type', dd) as "time"
-            FROM generate_series($from, $to, '1 $interval_type'::interval) dd
+              date_trunc(:interval_type, dd) as "time"
+            FROM generate_series(:from, :to, CONCAT('1 ', :interval_type)::interval) dd
       ),
           filtered_dimensions AS (
             SELECT
@@ -34,27 +34,21 @@ module DashboardHelper
             LEFT JOIN autonomous_systems ON autonomous_systems.id = autonomous_system_id
             LEFT JOIN autonomous_system_orgs ON autonomous_system_orgs.id = autonomous_systems.autonomous_system_org_id
             WHERE
-              account_id IN ($account_ids)
-              AND CASE WHEN $param_asn_org_ids = '-1' THEN
-                true
-              ELSE
-                autonomous_system_orgs.id IN ($as_orgs)
-              END
-              AND CASE WHEN $param_location_ids = '-1' THEN
-                true
-              ELSE
-                location_id IN ($location_ids)
-              END
+              account_id IN (:account_ids)
+    }
+    sql += " AND autonomous_system_orgs.id IN (:as_org_ids)" if as_org_ids.present?
+    sql += " AND location_id IN (:location_ids)" if location_ids.present?
+    sql += %{
       ),
         time_filtered AS (
           SELECT * FROM filtered_dimensions
-          WHERE "time" BETWEEN $from AND $to
+          WHERE "time" BETWEEN :from AND :to
           ORDER BY "time" ASC
       ),
         initial AS (
-          SELECT $from::timestamp as "time", total_online
+          SELECT :from::timestamp as "time", total_online
             FROM filtered_dimensions
-            WHERE "time" < $from
+            WHERE "time" < :from
             ORDER BY "time", id DESC LIMIT 1
       ),
         table_with_initial AS (
@@ -68,7 +62,6 @@ module DashboardHelper
 
           SELECT "time", total_online
           FROM time_filtered
-
       ),
         completed_table AS (
           SELECT
@@ -80,130 +73,107 @@ module DashboardHelper
           ) t
       )
 
-
       SELECT EXTRACT(EPOCH FROM "time") * 1000 as x, COALESCE(total_online, 0) as y FROM completed_table ORDER BY time ASC
     }
+    ActiveRecord::Base.sanitize_sql([sql, {interval_type: interval_type, account_ids: account_ids, as_org_ids: as_org_ids, location_ids: location_ids, from: from, to: to}])
   end
 
-  def self.get_download_speed_sql
-    %{
+  def self.get_download_speed_sql(account_ids, from, to, as_org_ids: nil, location_ids: nil)
+    sql = %{
       SELECT
           EXTRACT(EPOCH FROM time) * 1000 as x,
           MAX(download_max) as "#9138e5",
           percentile_disc(0.5) WITHIN GROUP (ORDER BY download_median) as "#4b7be5",
           MIN(download_min) AS "#ff695d"
-      FROM aggregated_measurements($from::timestamp, $to::timestamp)
+      FROM aggregated_measurements(:from::timestamp, :to::timestamp)
       WHERE
-          CASE WHEN $param_account_ids = '-1' THEN
-            true
-          ELSE
-            account_id IN ($account_ids)
-          END
-        AND CASE WHEN $param_asn_org_ids = '-1' THEN
-            true
-          ELSE
-            autonomous_system_org_id IN ($as_orgs)
-          END
-        AND CASE WHEN $param_location_ids = '-1' THEN
-            true
-          ELSE
-            location_id IN ($location_ids)
-          END
+          account_id IN (:account_ids)
+    }
+    sql += " AND autonomous_system_org_id IN (:as_org_ids)" if as_org_ids.present?
+    sql += " AND location_id IN (:location_ids)" if location_ids.present?
+
+    sql += %{
       GROUP BY 1
       ORDER BY EXTRACT(EPOCH FROM "time") * 1000 ASC;
     }
+    ActiveRecord::Base.sanitize_sql([sql, {account_ids: account_ids, as_org_ids: as_org_ids, location_ids: location_ids, from: from, to: to}])
   end
 
-  def self.get_upload_speed_sql
-    %{
+  def self.get_upload_speed_sql(account_ids, from, to, as_org_ids: nil, location_ids: nil)
+    sql = %{
       SELECT
           EXTRACT(EPOCH FROM time) * 1000 as x,
           MAX(upload_max) as "#9138e5",
           percentile_disc(0.5) WITHIN GROUP (ORDER BY upload_median) as "#4b7be5",
           MIN(upload_min) AS "#ff695d"
-      FROM aggregated_measurements($from::timestamp, $to::timestamp)
+      FROM aggregated_measurements(:from::timestamp, :to::timestamp)
       WHERE
-          CASE WHEN $param_account_ids = '-1' THEN
-            true
-          ELSE
-            account_id IN ($account_ids)
-          END
-        AND CASE WHEN $param_asn_org_ids = '-1' THEN
-            true
-          ELSE
-            autonomous_system_org_id IN ($as_orgs)
-          END
-        AND CASE WHEN $param_location_ids = '-1' THEN
-            true
-          ELSE
-            location_id IN ($location_ids)
-          END
+        account_id IN (:account_ids)
+    }
+    sql += " AND autonomous_system_org_id IN (:as_org_ids)" if as_org_ids.present?
+    sql += " AND location_id IN (:location_ids)" if location_ids.present?
+    sql += %{
       GROUP BY 1
       ORDER BY EXTRACT(EPOCH FROM "time") * 1000 ASC;
     }
+    ActiveRecord::Base.sanitize_sql([sql, {account_ids: account_ids, as_org_ids: as_org_ids, location_ids: location_ids, from: from, to: to}])
   end
 
-  def self.get_latency_sql
-    %{
+  def self.get_latency_sql(account_ids, from, to, as_org_ids: nil, location_ids: nil)
+    sql = %{
       SELECT
           EXTRACT(EPOCH FROM time) * 1000 as x,
           MAX(latency_max) as "#9138e5",
           percentile_disc(0.5) WITHIN GROUP (ORDER BY latency_median) as "#4b7be5",
           MIN(latency_min) AS "#ff695d"
-      FROM aggregated_measurements($from::timestamp, $to::timestamp)
+      FROM aggregated_measurements(:from::timestamp, :to::timestamp)
       WHERE
-          CASE WHEN $param_account_ids = '-1' THEN
-            true
-          ELSE
-            account_id IN ($account_ids)
-          END
-        AND CASE WHEN $param_asn_org_ids = '-1' THEN
-            true
-          ELSE
-            autonomous_system_org_id IN ($as_orgs)
-          END
-        AND CASE WHEN $param_location_ids = '-1' THEN
-            true
-          ELSE
-            location_id IN ($location_ids)
-          END
+        account_id IN (:account_ids)
+    }
+    sql += " AND autonomous_system_org_id IN (:as_org_ids)" if as_org_ids.present?
+    sql += " AND location_id IN (:location_ids)" if location_ids.present?
+    sql += %{
       GROUP BY 1
       ORDER BY EXTRACT(EPOCH FROM "time") * 1000 ASC;
     }
+    ActiveRecord::Base.sanitize_sql([sql, {account_ids: account_ids, as_org_ids: as_org_ids, location_ids: location_ids, from: from, to: to}])
   end
 
-  def self.get_usage_sql
-    %{
+  def self.get_usage_sql(interval_type, from, to, account_ids, as_org_ids: nil, location_ids: nil)
+    sql = %{
       WITH data_used_per_day AS (
         SELECT
-          date_trunc('$interval_type', processed_at) as "time",
+          date_trunc(:interval_type, processed_at) as "time",
           SUM(measurements.download_total_bytes) + SUM(measurements.upload_total_bytes) AS "total"
         FROM measurements
         JOIN autonomous_systems ON autonomous_systems.id = autonomous_system_id
         JOIN autonomous_system_orgs ON autonomous_system_orgs.id = autonomous_systems.autonomous_system_org_id
         WHERE
-          processed_at BETWEEN $from AND $to
-          AND CASE WHEN $param_account_ids = '-1' THEN
-              true
-            ELSE
-              account_id IN ($account_ids)
-            END
-          AND CASE WHEN $param_asn_org_ids = '-1' THEN
-              true
-            ELSE
-              autonomous_system_orgs.id IN ($as_orgs)
-            END
-            AND CASE WHEN $param_location_ids = '-1' THEN
-              true
-            ELSE
-              location_id IN ($location_ids)
-            END
+          processed_at BETWEEN :from AND :to
+          AND account_id IN (:account_ids)
+      }
+    sql += " AND autonomous_system_orgs.id IN (:as_org_ids)" if as_org_ids.present?
+    sql += " AND location_id IN (:location_ids)" if location_ids.present?
+
+    sql += %{
         GROUP BY 1
         ORDER BY 1 ASC
       )
-
       SELECT total as y, EXTRACT(EPOCH FROM time) * 1000 as x FROM data_used_per_day ORDER BY time ASC;
     }
+    ActiveRecord::Base.sanitize_sql([sql, {interval_type: interval_type, account_ids: account_ids, as_org_ids: as_org_ids, location_ids: location_ids, from: from, to: to}])
+  end
+
+  def self.get_as_orgs_sql(account_ids, from, to, location_ids: nil)
+    sql = %{
+      SELECT DISTINCT as_org.name as name, as_org.id as id
+      FROM aggregated_measurements(:from::timestamp, :to::timestamp) t
+      JOIN autonomous_system_orgs as_org ON autonomous_system_org_id = as_org.id
+      WHERE
+      t.account_id IN (:account_ids)
+    }
+    sql += " AND location_id IN (:location_ids)" if location_ids.present?
+    ActiveRecord::Base.sanitize_sql([sql, {account_ids: account_ids, from: from, to: to, location_ids: location_ids}])
   end
 
 end
