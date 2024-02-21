bca461702367b2bc39f738732bd31620c522e51d
Rafael Mendes <rafael@exactlylabs.com>
2024-02-21 08:57:24 -0300

    sentry exceptions deduplication within a 5 minutes window

--- server/config/initializers/sentry.rb
+++ server/config/initializers/sentry.rb
@@ -2,14 +2,23 @@ Sentry.init do |config|
   config.dsn = 'https://824cb73d4b5149459eb889296687f94f@o1197382.ingest.sentry.io/6320151'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  # Set tracesSampleRate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  # We recommend adjusting this value in production
   config.traces_sample_rate = 0.0
-  # or
-  #config.traces_sampler = lambda do |context|
-  #  true
-  #end
+
+  config.before_send = lambda do |event, hint|
+    e = hint[:exception]
+    begin
+      if REDIS.get("sentry:#{e.class.name}:#{e.message}").nil?
+        REDIS.set("sentry:#{e.class.name}:#{e.message}", 1)
+        REDIS.expire("sentry:#{e.class.name}:#{e.message}", 300) # 5 minutes
+        event
+      else
+        nil
+      end
+    rescue
+      event
+    end
+  end
+
 
   config.enabled_environments = %w[production]
 end
