352739f09c87be8d2aa0e39323a69eda60c56190
Rafael Mendes <rafael@exactlylabs.com>
2024-03-01 13:31:47 -0300

    changes to allow system outage insertions to backfill past projection inserions

--- server/app/eventhandlers/fetchers.rb
+++ server/app/eventhandlers/fetchers.rb
@@ -1,18 +1,17 @@
 module Fetchers
   DEFAULT_BATCH_SIZE = 10000
 
-  def fetch_from_events(offset, **opts)
-    batch_size = opts[:batch_size] || DEFAULT_BATCH_SIZE
+  def events_iterator(model, offset, batch_size: DEFAULT_BATCH_SIZE)
     Enumerator.new { |g|
       events = Event.where(
-        "id > ? AND (aggregate_type = ? OR aggregate_type = ?)", offset, Client.name, SystemOutage.name
-      ).order('timestamp ASC, version ASC').preload(:snapshot).find_each(batch_size: batch_size) do |event|
-        g.yield event
+        "id > ? AND aggregate_type = ?", offset, model.name
+      ).order('timestamp ASC, version ASC').preload(:snapshot, :aggregate).find_each(batch_size: batch_size) do |event|
+        g.yield ({name: model.name, data: event, timestamp: event.timestamp.to_i})
       end
     }
   end
 
-  def fetch_from_measurements(offset, **opts)
+  def measurements_iterator(offset, batch_size: DEFAULT_BATCH_SIZE)
     attrs = %w(
       measurements.id
       measurements.location_id
@@ -29,7 +28,7 @@ module Fetchers
       autonomous_system_org_id
       autonomous_system_org_name
     )
-    batch_size = opts[:batch_size] || DEFAULT_BATCH_SIZE
+
     Enumerator.new { |g|
       measurements = Measurement.joins(:autonomous_system => [:autonomous_system_org]).where(
         "processed = true AND measurements.id > ? AND lonlat IS NOT NULL AND location_id IS NOT NULL",
@@ -38,13 +37,14 @@ module Fetchers
         "processed_at ASC, measurements.id ASC"
       ).in_batches(of: batch_size) do |measurements|
         measurements.pluck(*attrs).each do |measurement|
-          g.yield attrs_name.zip(measurement).to_h
+          data = attrs_name.zip(measurement).to_h
+          g.yield ({name: "Measurements", data: data, timestamp: data["processed_at"].to_i})
         end
       end
     }
   end
 
-  def fetch_from_speed_tests(offset, **opts)
+  def client_speed_tests_iterator(offset, batch_size: DEFAULT_BATCH_SIZE)
     attrs = %w(
       client_speed_tests.id
       client_speed_tests.lonlat
@@ -60,7 +60,7 @@ module Fetchers
       autonomous_system_org_id
       autonomous_system_org_name
     )
-    batch_size = opts[:batch_size] || DEFAULT_BATCH_SIZE
+
     speed_tests = ClientSpeedTest.joins(:autonomous_system => [:autonomous_system_org]).where(
       "processed_at IS NOT NULL AND client_speed_tests.id > ? AND lonlat IS NOT NULL",
       offset
@@ -68,50 +68,40 @@ module Fetchers
     Enumerator.new { |g|
       speed_tests.in_batches(of: batch_size) do |speed_tests|
         speed_tests.pluck(*attrs).each do |speed_test|
-          g.yield attrs_name.zip(speed_test).to_h
+          data = attrs_name.zip(speed_test).to_h
+          g.yield ({name: "ClientSpeedTest", data: data, timestamp: data["processed_at"].to_i})
         end
       end
     }
   end
 
-  def fetch_from_daily_trigger(offset)
+  def days_iterator(offset)
     # We use this to trigger actions that check conditions daily
     offset = Event.first.timestamp.to_date.to_time.to_i if offset == 0
 
     Enumerator.new { |g|
       Time.at(offset).to_date.next_day.upto(Date.today).each do |date|
-        g.yield date.at_beginning_of_day
+        g.yield ({name: "DailyTrigger", data: date.at_beginning_of_day, timestamp: date.at_beginning_of_day.to_i})
       end
     }
-
   end
 
-  def sources_iterator(**opts)
-    events_offset = opts[:events_offset]
-    measurements_offset = opts[:measurements_offset]
-    speed_tests_offset = opts[:speed_tests_offset]
-    daily_trigger_offset = opts[:daily_trigger_offset]
-
-    # Returns a single iterator that selects the next element from all sources
-    events = fetch_from_events events_offset if events_offset.present?
-    measurements = fetch_from_measurements measurements_offset if measurements_offset.present?
-    speed_tests = fetch_from_speed_tests speed_tests_offset if speed_tests_offset.present?
-    daily_triggers = fetch_from_daily_trigger daily_trigger_offset if daily_trigger_offset.present?
-
+  def sorted_iteration(iterators)
     Enumerator.new do |g|
       loop do
-        next_values = []
-        next_values << ["event", events.peek.timestamp.to_i, events] if (events.peek rescue nil)
-        next_values << ["measurement", measurements.peek["processed_at"].to_i, measurements] if (measurements.peek rescue nil)
-        next_values << ["speed_test", speed_tests.peek["processed_at"].to_i, speed_tests] if (speed_tests.peek rescue nil)
-        next_values << ["daily_trigger", daily_triggers.peek.to_time.to_i, daily_triggers] if (daily_triggers.peek rescue nil)
+        next_values = iterators.map do |it|
+          content = it.peek rescue nil
+          [content[:timestamp], it] if content
+        end.compact # drop all nils
+
         if next_values.blank?
           break
         end
-        next_values.sort_by! { |name, at, it| at }
-        name, at, it = next_values.first
 
-        g.yield [name, it.next]
+        next_values.sort_by! { |at, it| at }
+        at, it = next_values.first
+
+        g.yield it.next
       end
     end
   end
