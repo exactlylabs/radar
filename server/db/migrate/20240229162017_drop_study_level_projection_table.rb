444bf93c331ec3b3ce5c065f385c189faefdfbca
Rafael Mendes <rafael@exactlylabs.com>
2024-03-10 19:49:14 -0300

    force drop cascade

--- server/db/migrate/20240229162017_drop_study_level_projection_table.rb
+++ server/db/migrate/20240229162017_drop_study_level_projection_table.rb
@@ -1,5 +1,5 @@
 class DropStudyLevelProjectionTable < ActiveRecord::Migration[6.1]
   def change
-    drop_table :study_level_projections
+    drop_table :study_level_projections, force: :cascade
   end
 end
