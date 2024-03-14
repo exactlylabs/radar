4bcbf5f422efaba7c87ee52676f7afd5783843e0
ignaciograsso <ignacio@exactlylabs.com>
2024-03-14 13:27:15 -0300

    send missing fields

--- speedtest_mobile/client_mobile_app/lib/core/wifi_tracking/wifi_tracker.dart
+++ speedtest_mobile/client_mobile_app/lib/core/wifi_tracking/wifi_tracker.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:async';
+import 'package:client_mobile_app/core/ws_mobile_messages/google/protobuf/struct.pb.dart';
+import 'package:configuration_monitoring/configuration_monitoring.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:network_connection_info/network_connection_info.dart';
 import 'package:client_mobile_app/core/local_storage/local_storage.dart';
@@ -13,23 +17,32 @@ class WifiTracker {
     required LocalStorage localStorage,
     required NetworkConnectionInfo networkConnectionInfo,
     required WebSocketClient webSocketClient,
+    required ConfigurationMonitoring configurationMonitoring,
   })  : _localStorage = localStorage,
         _webSocketClient = webSocketClient,
+        _configurationMonitoring = configurationMonitoring,
         _networkConnectionInfo = networkConnectionInfo;
 
   final WebSocketClient _webSocketClient;
   final LocalStorage _localStorage;
   final NetworkConnectionInfo _networkConnectionInfo;
+  final ConfigurationMonitoring _configurationMonitoring;
 
   Position? _position;
   StreamSubscription<Position>? _positionStreamSubscription;
   List<Map<String, dynamic>> _responses = [];
+  PackageInfo? _packageInfo;
+  String? _sessionId;
+  Map<String, dynamic>? _deviceAndPermissionsState;
 
   Future<void> startWifiTracking() async {
     setInitialValues();
     final scannedWifiResults = await _networkConnectionInfo.getWifiNetworkList();
 
     _responses.addAll(scannedWifiResults);
+    _sessionId = _localStorage.getSessionId();
+    _packageInfo = await PackageInfo.fromPlatform();
+    _deviceAndPermissionsState = await _configurationMonitoring.getDeviceAndPermissionsState();
     _sendScannedWifiResults();
   }
 
@@ -44,11 +57,6 @@ class WifiTracker {
     if (Platform.isIOS) return;
     _webSocketClient.open();
 
-    final wsMessage = WSMessage(
-      event: Events.SCAN_START,
-      timestamp: Timestamp.fromDateTime(DateTime.now()),
-    );
-    _webSocketClient.send(wsMessage.writeToBuffer());
     final androidSettings = AndroidSettings(
       accuracy: LocationAccuracy.bestForNavigation,
       forceLocationManager: true,
@@ -87,8 +95,8 @@ class WifiTracker {
     print('Scanned wifi results: $_responses');
     try {
       final wsMessage = WSMessage(
-          event: Events.SCAN_RESULT,
-          scanResult: ScanResult(
+        event: Events.SCAN_RESULT,
+        scanResult: ScanResult(
             scannedAps: _responses
                 .map((response) => ScannedAP(
                       bssid: response['bssid'],
@@ -108,7 +116,16 @@ class WifiTracker {
                 .toList(),
             latitude: _position?.latitude,
             longitude: _position?.longitude,
-          ));
+            metadata: Struct(
+              fields: {
+                'version_number': Value(stringValue: _packageInfo?.version),
+                'build_number': Value(stringValue: _packageInfo?.buildNumber),
+                'device_and_permissions':
+                    getDeviceAndPermissionsState(_deviceAndPermissionsState ?? {}),
+              },
+            )),
+        sessionId: _sessionId,
+      );
       final wsMessageToByteArray = wsMessage.writeToBuffer();
       return wsMessageToByteArray;
     } catch (e) {
@@ -118,7 +135,28 @@ class WifiTracker {
     }
   }
 
+  Value getDeviceAndPermissionsState(Map<String, dynamic> data) {
+    Map<String, Value> fields = {};
+    data.forEach((key, value) {
+      if (value is String) {
+        fields[key] = Value(stringValue: value);
+      } else if (value is bool) {
+        fields[key] = Value(boolValue: value);
+      } else if (value is int) {
+        fields[key] = Value(numberValue: value.toDouble());
+      } else if (value is double) {
+        fields[key] = Value(numberValue: value);
+      } else if (value is List) {
+        fields[key] =
+            Value(listValue: ListValue(values: value.map((e) => Value(stringValue: e)).toList()));
+      }
+    });
+    return Value(structValue: Struct(fields: fields));
+  }
+
   void setInitialValues() {
     _responses = [];
+    _sessionId = null;
+    _deviceAndPermissionsState = null;
   }
 }
--- speedtest_mobile/client_mobile_app/lib/core/ws_mobile_messages/ws_mobile_messages.pbjson.dart
+++ speedtest_mobile/client_mobile_app/lib/core/ws_mobile_messages/ws_mobile_messages.pbjson.dart
@@ -18,8 +18,6 @@ const Events$json = {
   '1': 'Events',
   '2': [
     {'1': 'SCAN_RESULT', '2': 0},
-    {'1': 'SCAN_START', '2': 1},
-    {'1': 'SCAN_STOP', '2': 2},
     {'1': 'WELCOME', '2': 3},
     {'1': 'PING', '2': 4},
     {'1': 'Other', '2': 99},
@@ -27,9 +25,9 @@ const Events$json = {
 };
 
 /// Descriptor for `Events`. Decode as a `google.protobuf.EnumDescriptorProto`.
-final $typed_data.Uint8List eventsDescriptor = $convert.base64Decode(
-    'CgZFdmVudHMSDwoLU0NBTl9SRVNVTFQQABIOCgpTQ0FOX1NUQVJUEAESDQoJU0NBTl9TVE9QEA'
-    'ISCwoHV0VMQ09NRRADEggKBFBJTkcQBBIJCgVPdGhlchBj');
+final $typed_data.Uint8List eventsDescriptor = $convert
+    .base64Decode('CgZFdmVudHMSDwoLU0NBTl9SRVNVTFQQABILCgdXRUxDT01FEAMSCAoEUElORxAEEgkKBU90aG'
+        'VyEGM=');
 
 @$core.Deprecated('Use scannedAPDescriptor instead')
 const ScannedAP$json = {
@@ -45,48 +43,81 @@ const ScannedAP$json = {
     {'1': 'is80211mc_responder', '3': 8, '4': 1, '5': 8, '10': 'is80211mcResponder'},
     {'1': 'channel_width', '3': 9, '4': 1, '5': 5, '10': 'channelWidth'},
     {'1': 'is_passpoint_network', '3': 10, '4': 1, '5': 8, '10': 'isPasspointNetwork'},
-    {'1': 'information_elements', '3': 11, '4': 1, '5': 12, '10': 'informationElements'},
+    {'1': 'information_elements', '3': 11, '4': 3, '5': 12, '10': 'informationElements'},
     {'1': 'wifi_standard', '3': 12, '4': 1, '5': 5, '10': 'wifiStandard'},
-    {'1': 'timestamp', '3': 13, '4': 1, '5': 11, '6': '.google.protobuf.Timestamp', '10': 'timestamp'},
+    {
+      '1': 'timestamp',
+      '3': 13,
+      '4': 1,
+      '5': 11,
+      '6': '.google.protobuf.Timestamp',
+      '10': 'timestamp'
+    },
   ],
 };
 
 /// Descriptor for `ScannedAP`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List scannedAPDescriptor = $convert.base64Decode(
-    'CglTY2FubmVkQVASFAoFYnNzaWQYASABKAlSBWJzc2lkEhIKBHNzaWQYAiABKAlSBHNzaWQSIg'
-    'oMY2FwYWJpbGl0aWVzGAMgASgJUgxjYXBhYmlsaXRpZXMSFAoFbGV2ZWwYBCABKAVSBWxldmVs'
-    'EhwKCWZyZXF1ZW5jeRgFIAEoBVIJZnJlcXVlbmN5EiEKDGNlbnRlcl9mcmVxMBgGIAEoBVILY2'
-    'VudGVyRnJlcTASIQoMY2VudGVyX2ZyZXExGAcgASgFUgtjZW50ZXJGcmVxMRIvChNpczgwMjEx'
-    'bWNfcmVzcG9uZGVyGAggASgIUhJpczgwMjExbWNSZXNwb25kZXISIwoNY2hhbm5lbF93aWR0aB'
-    'gJIAEoBVIMY2hhbm5lbFdpZHRoEjAKFGlzX3Bhc3Nwb2ludF9uZXR3b3JrGAogASgIUhJpc1Bh'
-    'c3Nwb2ludE5ldHdvcmsSMQoUaW5mb3JtYXRpb25fZWxlbWVudHMYCyABKAxSE2luZm9ybWF0aW'
-    '9uRWxlbWVudHMSIwoNd2lmaV9zdGFuZGFyZBgMIAEoBVIMd2lmaVN0YW5kYXJkEjgKCXRpbWVz'
-    'dGFtcBgNIAEoCzIaLmdvb2dsZS5wcm90b2J1Zi5UaW1lc3RhbXBSCXRpbWVzdGFtcA==');
+final $typed_data.Uint8List scannedAPDescriptor = $convert
+    .base64Decode('CglTY2FubmVkQVASFAoFYnNzaWQYASABKAlSBWJzc2lkEhIKBHNzaWQYAiABKAlSBHNzaWQSIg'
+        'oMY2FwYWJpbGl0aWVzGAMgASgJUgxjYXBhYmlsaXRpZXMSFAoFbGV2ZWwYBCABKAVSBWxldmVs'
+        'EhwKCWZyZXF1ZW5jeRgFIAEoBVIJZnJlcXVlbmN5EiEKDGNlbnRlcl9mcmVxMBgGIAEoBVILY2'
+        'VudGVyRnJlcTASIQoMY2VudGVyX2ZyZXExGAcgASgFUgtjZW50ZXJGcmVxMRIvChNpczgwMjEx'
+        'bWNfcmVzcG9uZGVyGAggASgIUhJpczgwMjExbWNSZXNwb25kZXISIwoNY2hhbm5lbF93aWR0aB'
+        'gJIAEoBVIMY2hhbm5lbFdpZHRoEjAKFGlzX3Bhc3Nwb2ludF9uZXR3b3JrGAogASgIUhJpc1Bh'
+        'c3Nwb2ludE5ldHdvcmsSMQoUaW5mb3JtYXRpb25fZWxlbWVudHMYCyADKAxSE2luZm9ybWF0aW'
+        '9uRWxlbWVudHMSIwoNd2lmaV9zdGFuZGFyZBgMIAEoBVIMd2lmaVN0YW5kYXJkEjgKCXRpbWVz'
+        'dGFtcBgNIAEoCzIaLmdvb2dsZS5wcm90b2J1Zi5UaW1lc3RhbXBSCXRpbWVzdGFtcA==');
 
 @$core.Deprecated('Use scanResultDescriptor instead')
 const ScanResult$json = {
   '1': 'ScanResult',
   '2': [
-    {'1': 'scanned_aps', '3': 1, '4': 3, '5': 11, '6': '.ws_mobile_messages_pb.ScannedAP', '10': 'scannedAps'},
+    {
+      '1': 'scanned_aps',
+      '3': 1,
+      '4': 3,
+      '5': 11,
+      '6': '.ws_mobile_messages_pb.ScannedAP',
+      '10': 'scannedAps'
+    },
     {'1': 'latitude', '3': 2, '4': 1, '5': 2, '10': 'latitude'},
     {'1': 'longitude', '3': 3, '4': 1, '5': 2, '10': 'longitude'},
+    {'1': 'metadata', '3': 99, '4': 1, '5': 11, '6': '.google.protobuf.Struct', '10': 'metadata'},
   ],
 };
 
 /// Descriptor for `ScanResult`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List scanResultDescriptor = $convert.base64Decode(
-    'CgpTY2FuUmVzdWx0EkEKC3NjYW5uZWRfYXBzGAEgAygLMiAud3NfbW9iaWxlX21lc3NhZ2VzX3'
-    'BiLlNjYW5uZWRBUFIKc2Nhbm5lZEFwcxIaCghsYXRpdHVkZRgCIAEoAlIIbGF0aXR1ZGUSHAoJ'
-    'bG9uZ2l0dWRlGAMgASgCUglsb25naXR1ZGU=');
+final $typed_data.Uint8List scanResultDescriptor = $convert
+    .base64Decode('CgpTY2FuUmVzdWx0EkEKC3NjYW5uZWRfYXBzGAEgAygLMiAud3NfbW9iaWxlX21lc3NhZ2VzX3'
+        'BiLlNjYW5uZWRBUFIKc2Nhbm5lZEFwcxIaCghsYXRpdHVkZRgCIAEoAlIIbGF0aXR1ZGUSHAoJ'
+        'bG9uZ2l0dWRlGAMgASgCUglsb25naXR1ZGUSMwoIbWV0YWRhdGEYYyABKAsyFy5nb29nbGUucH'
+        'JvdG9idWYuU3RydWN0UghtZXRhZGF0YQ==');
 
 @$core.Deprecated('Use wSMessageDescriptor instead')
 const WSMessage$json = {
   '1': 'WSMessage',
   '2': [
     {'1': 'event', '3': 1, '4': 1, '5': 14, '6': '.ws_mobile_messages_pb.Events', '10': 'event'},
-    {'1': 'scan_result', '3': 2, '4': 1, '5': 11, '6': '.ws_mobile_messages_pb.ScanResult', '9': 0, '10': 'scanResult'},
-    {'1': 'timestamp', '3': 3, '4': 1, '5': 11, '6': '.google.protobuf.Timestamp', '9': 0, '10': 'timestamp'},
-    {'1': 'json', '3': 4, '4': 1, '5': 9, '9': 0, '10': 'json'},
+    {'1': 'session_id', '3': 5, '4': 1, '5': 9, '10': 'sessionId'},
+    {
+      '1': 'scan_result',
+      '3': 2,
+      '4': 1,
+      '5': 11,
+      '6': '.ws_mobile_messages_pb.ScanResult',
+      '9': 0,
+      '10': 'scanResult'
+    },
+    {
+      '1': 'timestamp',
+      '3': 3,
+      '4': 1,
+      '5': 11,
+      '6': '.google.protobuf.Timestamp',
+      '9': 0,
+      '10': 'timestamp'
+    },
+    {'1': 'json', '3': 4, '4': 1, '5': 11, '6': '.google.protobuf.Struct', '9': 0, '10': 'json'},
   ],
   '8': [
     {'1': 'message'},
@@ -94,10 +125,10 @@ const WSMessage$json = {
 };
 
 /// Descriptor for `WSMessage`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List wSMessageDescriptor = $convert.base64Decode(
-    'CglXU01lc3NhZ2USMwoFZXZlbnQYASABKA4yHS53c19tb2JpbGVfbWVzc2FnZXNfcGIuRXZlbn'
-    'RzUgVldmVudBJECgtzY2FuX3Jlc3VsdBgCIAEoCzIhLndzX21vYmlsZV9tZXNzYWdlc19wYi5T'
-    'Y2FuUmVzdWx0SABSCnNjYW5SZXN1bHQSOgoJdGltZXN0YW1wGAMgASgLMhouZ29vZ2xlLnByb3'
-    'RvYnVmLlRpbWVzdGFtcEgAUgl0aW1lc3RhbXASFAoEanNvbhgEIAEoCUgAUgRqc29uQgkKB21l'
-    'c3NhZ2U=');
-
+final $typed_data.Uint8List wSMessageDescriptor = $convert
+    .base64Decode('CglXU01lc3NhZ2USMwoFZXZlbnQYASABKA4yHS53c19tb2JpbGVfbWVzc2FnZXNfcGIuRXZlbn'
+        'RzUgVldmVudBIdCgpzZXNzaW9uX2lkGAUgASgJUglzZXNzaW9uSWQSRAoLc2Nhbl9yZXN1bHQY'
+        'AiABKAsyIS53c19tb2JpbGVfbWVzc2FnZXNfcGIuU2NhblJlc3VsdEgAUgpzY2FuUmVzdWx0Ej'
+        'oKCXRpbWVzdGFtcBgDIAEoCzIaLmdvb2dsZS5wcm90b2J1Zi5UaW1lc3RhbXBIAFIJdGltZXN0'
+        'YW1wEi0KBGpzb24YBCABKAsyFy5nb29nbGUucHJvdG9idWYuU3RydWN0SABSBGpzb25CCQoHbW'
+        'Vzc2FnZQ==');
