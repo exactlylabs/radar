2f972497965008382e6ceafbd0db181caaf08f6b
Rafael Mendes <rafael@exactlylabs.com>
2024-04-23 09:18:00 -0300

    New Outage tables

---
+++ server/db/migrate/20240422162200_add_outage_models.rb
@@ -0,0 +1,27 @@
+class AddOutageModels < ActiveRecord::Migration[6.1]
+  def change
+    create_table :outage_events do |t|
+      t.integer :status, null: false, default: 0
+      t.integer :outage_type, null: false, default: 0
+      t.references :autonomous_system_org, foreign_key: true
+
+      t.datetime :started_at
+      t.datetime :resolved_at
+    end
+
+    create_table :client_outages do |t|
+      t.integer :status, null: false, default: 0
+      t.boolean :has_service_started_event, null: false, default: false
+      t.references :client, foreign_key: true
+      t.references :outage_event, foreign_key: true
+      t.references :autonomous_system_org, foreign_key: true
+
+      # To avoid having permanent ISP outages, we only verify the back online count from the pods that went offline in a specific time-window
+      t.boolean :accounted_in_isp_window, null: false, default: false
+
+      t.datetime :started_at
+      t.datetime :resolved_at
+    end
+
+  end
+end
