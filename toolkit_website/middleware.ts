157960934495306913fc7e9635a688d006206de9
ignaciograsso <ignacio@exactlylabs.com>
2024-04-30 10:13:35 -0300

    Cr changes

--- toolkit_website/middleware.ts
+++ toolkit_website/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const DESTINATION_BASE_URL = process.env.NEXT_PUBLIC_APP_ENV === 'production' ? 'https://pods.radartoolkit.com' : 'http://0.0.0.0:3000'
+const DESTINATION_BASE_URL = process.env.NEXT_PUBLIC_APP_ENV === 'production' ? 'https://pods.radartoolkit.com' : 'https://pods.staging.radartoolkit.com'
 
 const MATCHERS = {
   TBP: /tbp/i,
@@ -41,32 +41,31 @@ export function middleware(request: NextRequest) {
   } else if (MATCHERS.GO_ALASKA.URL.test(pathName)) {
     nextUrl = '/GO';
     queryParams.append('state',MATCHERS.GO_ALASKA.NAME);
-    setSourceAndDateForCampaing(queryParams, MATCHERS.GO_ALASKA.SOURCE, MATCHERS.GO_ALASKA.DATE);
+    setSourceAndDateForCampaign(queryParams, MATCHERS.GO_ALASKA.SOURCE, MATCHERS.GO_ALASKA.DATE);
   } else if (MATCHERS.GO_MICHIGAN.URL.test(pathName)) {
     nextUrl = '/GO';
     queryParams.append('state',MATCHERS.GO_MICHIGAN.NAME);
-    setSourceAndDateForCampaing(queryParams, MATCHERS.GO_MICHIGAN.SOURCE, MATCHERS.GO_MICHIGAN.DATE);
+    setSourceAndDateForCampaign(queryParams, MATCHERS.GO_MICHIGAN.SOURCE, MATCHERS.GO_MICHIGAN.DATE);
   } else if (MATCHERS.GO_TEXAS.URL.test(pathName)) {
     nextUrl = '/GO';
     queryParams.append('state',MATCHERS.GO_TEXAS.NAME);
-    setSourceAndDateForCampaing(queryParams, MATCHERS.GO_TEXAS.SOURCE, MATCHERS.GO_TEXAS.DATE);
+    setSourceAndDateForCampaign(queryParams, MATCHERS.GO_TEXAS.SOURCE, MATCHERS.GO_TEXAS.DATE);
   } else if (MATCHERS.GO_WEST_VIRGINIA.URL.test(pathName)) {
     nextUrl = '/GO';
     queryParams.append('state',MATCHERS.GO_WEST_VIRGINIA.NAME);
-    setSourceAndDateForCampaing(queryParams, MATCHERS.GO_WEST_VIRGINIA.SOURCE, MATCHERS.GO_WEST_VIRGINIA.DATE);
+    setSourceAndDateForCampaign(queryParams, MATCHERS.GO_WEST_VIRGINIA.SOURCE, MATCHERS.GO_WEST_VIRGINIA.DATE);
   }
 
-  if (nextUrl != null && nextUrl != undefined) {
+  if (nextUrl !== null && nextUrl !== undefined) {
     const url = new URL(DESTINATION_BASE_URL + nextUrl);
     const newUrl = url.origin + url.pathname + '?' + queryParams.toString();
-    console.log(newUrl);
     return NextResponse.redirect(newUrl);
   } else {
     return NextResponse.next();
   }
 }
 
-function setSourceAndDateForCampaing(queryParams: URLSearchParams, source: string, date: string) {
+function setSourceAndDateForCampaign(queryParams: URLSearchParams, source: string, date: string) {
   if(!queryParams.has('source')) {
     queryParams.append('source',source);
   }
