7a5906297e8f7b7ed92dd7aaadaebad6d388bb62
ignaciograsso <ignacio@exactlylabs.com>
2024-04-30 09:20:28 -0300

    pod_assignment_type to helper

---
+++ server/app/helpers/pods_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module PodsHelper
+  module PodAssignmentType
+    NoNetwork = 'no_network'
+    ExistingNetwork = 'existing_network'
+    NewNetwork = 'new_network'
+  end
+end
