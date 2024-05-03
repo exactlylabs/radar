400535e09a5cce20359ca9c528b437fd900beee9
Rafael Mendes <rafael@exactlylabs.com>
2024-05-03 11:31:44 -0300

    new client API for creating, assigning to account/location, and showing self info

--- server/app/controllers/api/v1/api_controller.rb
+++ server/app/controllers/api/v1/api_controller.rb
@@ -8,30 +8,69 @@ module Api
       skip_before_action :verify_authenticity_token
 
       def render_error_for(model)
-        render json: { 
+        render json: {
           errors: model.errors,
         }, status: 400
       end
 
+      def pundit_user
+        AuthenticationHolder.new(@current_user, false, false, false)
+      end
+
       private
+
+      ##
+      #
+      # Authenticates either an Account or UsersAccount in a Token type Auth.
+      #
+      ##
       def check_basic_auth
-        unless request.authorization.present?
+        token_value = get_token("Token")
+        unless token_value.present?
           head :unauthorized
           return
         end
-        token = request.authorization
-        token_key, token_value = token.split(" ")
-        if token_key != "Token"
-          head :unauthorized
-        end
+
         @account = Account.find_by_token(token_value)
-        head :unauthorized unless @account
+        if @account.nil?
+          user_account = UsersAccount.find_by_token(token_value)
+          if user_account.present?
+            @account = user_account.account
+            @current_user = user_account.user
+          end
+        end
+
+        head :unauthorized unless @account.present?
+      end
+
+      def authenticate_client
+        token_value = get_token("ClientToken")
+        unless token_value.present?
+          head :unauthorized
+          return
+        end
+
+        # unix_user:secret base64 encoded
+        unix_user, secret = Base64.decode64(token_value)&.split(":")
+        @client = Client.find_by_unix_user(unix_user)&.authenticate_secret(secret)
+
+        head :unauthorized unless @client.present?
       end
 
       def ensure_superaccount!
         if !@account.superaccount?
           render json: {error: "you must be a superaccount to use this endpoint"}, status: :forbidden
         end
+      end
+
+      def get_token(expected_type)
+        return unless request.authorization.present?
+
+        token = request.authorization
+        token_key, token_value = token.split(" ")
+        return unless token_key == expected_type
+
+        token_value
       end
     end
   end
--- server/app/controllers/api/v1/clients_controller.rb
+++ server/app/controllers/api/v1/clients_controller.rb
@@ -1,19 +1,78 @@
 module Api
-    module V1
-      class ClientsController < ApiController
-        before_action :authenticate_client!
+  module V1
+    class ClientsController < ApiController
+      skip_before_action :check_basic_auth
+      before_action :authenticate_client, except: [:create]
 
-        private
+      # GET /api/v1/clients/me return the ClientToken's owner information
+      def me
+        return :unauthorized unless @client.present?
 
-        def authenticate_client!
-          client_id = params[:unix_user]
-          client_secret = params[:secret]
-          @client = Client.find_by_unix_user(client_id)&.authenticate_secret(client_secret)
-          if !@client
-            head(403)
-          end
+        render json: @client.as_json.except("secret_digest"), status: :ok
+      end
+
+      # POST /api/v1/clients
+      def create
+        @client = Client.new(create_client_params)
+        @client.user = current_user
+        @secret = SecretsHelper.create_human_readable_secret(11)
+        @client.secret = @secret
+        ug = UpdateGroup.default_group
+        if !ug.nil?
+          @client.update_group = ug
         end
 
+        # If it's registered with a superaccount token
+        # set the pod as having a watchdog (physical pod)
+        if params[:registration_token] && Account.find_by_token(params[:registration_token])&.superaccount?
+          @client.has_watchdog = true
+        end
+
+        if @client.save
+          render json: @client.as_json.except("secret_digest").merge("secret" => @secret), status: :created
+        else
+          render_error_for @client
+        end
+      end
+
+      # POST /api/v1/clients/assign
+      def assign
+        user_account = UsersAccount.find_by_token(params[:token])
+        unless user_account.present?
+          render json: { error: "Invalid User Account Token" }, status: :bad_request
+        end
+
+        @client.account = user_account.account
+        @client.user = user_account.user
+
+        if network_params.present? && network_params[:id].nil?
+          @client.location = create_network_for_client(@client)
+
+        elsif network_params.present?
+          location = policy_scope(Location).find(network_params[:id])
+          @client.update!(location: location)
+        end
+
+        if @client.save
+          render json: @client.as_json.except("secret_digest"), status: :ok
+        else
+          render_error_for @client
+        end
+      end
+
+      private
+
+      def create_network_for_client(client)
+        Location.create!(network_params.merge(account: client.account, created_by_id: client.user.id))
+      end
+
+      def network_params
+        params.permit(:network => [:id, :name, :latitude, :longitude, :address, :expected_mbps_down, :expected_mbps_up])
+      end
+
+      def create_client_params
+        params.permit(:name, :register_label)
       end
     end
+  end
 end
