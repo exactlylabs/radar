3265ff963182cdb1f3758cb2a1ab21ffbf6c22af
Michael M. Wasser <untoldone@users.noreply.github.com>
2024-05-03 21:46:18 -0700

    Fixes server paginator to use activerecord-isms to use offset and limit instead of Very Large Selects

--- server/app/controllers/concerns/paginator.rb
+++ server/app/controllers/concerns/paginator.rb
@@ -2,6 +2,6 @@ module Paginator extend ActiveSupport::Concern
   def paginate(elements, page, page_size)
     page = page.nil? ? 0 : page.to_i > 0 ? page.to_i - 1 : 0
     page_size = page_size.nil? ? 10 : page_size.to_i
-    elements.drop(page * page_size).first(page_size)
+    elements.offset(page * page_size).limit(page_size)
   end
 end
