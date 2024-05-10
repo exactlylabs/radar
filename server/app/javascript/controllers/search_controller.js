ceb10b510295740033e2662a3087cf035c1d45f2
ignaciograsso <ignacio@exactlylabs.com>
2024-05-09 21:57:50 -0300

    pods search done

--- server/app/javascript/controllers/search_controller.js
+++ server/app/javascript/controllers/search_controller.js
@@ -1,5 +1,5 @@
-import { Controller } from "@hotwired/stimulus";
-import { emitCustomEvent } from "../eventsEmitter";
+import {Controller} from "@hotwired/stimulus";
+import {emitCustomEvent} from "../eventsEmitter";
 
 /**
  * The idea for this specific controller is to have an application-wide
@@ -9,68 +9,86 @@ import { emitCustomEvent } from "../eventsEmitter";
  * the form's submit action. Paired with turbo frames, this makes
  * for a very simplistic, inline, seamless update of the elements
  * of any list.
- * 
+ *
  * By default, the request will be sent expecting HTML processing
  * on the controller's end. If that is not the expected behaviour
  * and you prefer to have TURBO_STREAM processing, add the following
  * attributes to the <form> element:
- * 
+ *
  * - data-fetch-url => expected url for the GET request
  * - data-turbo => can be set to "true" to make it explicit
  */
 export default class extends Controller {
 
-  connect() {
-    this.token = document.getElementsByName("csrf-token")[0].content;
-  }
-
-  doStreamRequest(formElement) {
-    const query = document.querySelector("[name=query]").value;
-    let fetchUrl = formElement.getAttribute('data-fetch-url');
-    const url = new URL(window.location.origin + fetchUrl)
-    const queryParams = new URLSearchParams(url.search)
-    queryParams.set('query', query)
-    const newUrl = url.origin + url.pathname + '?' + queryParams.toString()
-
-    const eventToEmitKey = formElement.getAttribute('data-event-key');
-    fetch(newUrl, {
-      method: 'GET',
-      headers: { "X-CSRF-Token": this.token },
-    })
-    .then (response => response.text())
-    .then(html => {
-      Turbo.renderStreamMessage(html);
-      // The use of a custom event here allows multi-controller
-      // communication to handle some functionality when the response
-      // is ready to process.
-      if(eventToEmitKey) emitCustomEvent(eventToEmitKey);
-    })
-    .catch((err) => {
-      handleError(err, this.identifier);
-    });
-  }
-
-  search(e) {
-    if(e.target.value) {
-      document.querySelector('#category--clear-icon-ref').classList.remove('invisible');
-    } else {
-      document.querySelector('#category--clear-icon-ref').classList.add('invisible');
+    connect() {
+        this.token = document.getElementsByName("csrf-token")[0].content;
     }
-    if(!this.element.getAttribute('data-turbo')) {
-      this.element.requestSubmit();
-    } else {
-      this.doStreamRequest(this.element);
-    }
-  }
 
-  clearSearch() {
-    this.element.classList.add('invisible');
-    document.querySelector("[name=query]").value = null;
-    const formElement = document.querySelector('#search-form');
-    if(!formElement.getAttribute('data-turbo')) {
-      formElement.requestSubmit();
-    } else {
-      this.doStreamRequest(formElement);
+    doStreamRequest(formElement) {
+        const query = document.querySelector("[name=query]").value;
+
+        const currentUrl = new URL(window.location.href);
+        const currentSearchParams = new URLSearchParams(currentUrl.search);
+
+        let fetchUrl = formElement.getAttribute('data-fetch-url');
+
+        const newUrl = new URL(currentUrl.origin + fetchUrl);
+        const newSearchParams = new URLSearchParams(newUrl.search);
+
+        newSearchParams.set('query', query);
+        let updateUrl = formElement.getAttribute('data-update-url');
+
+
+        if (updateUrl !== null && updateUrl !== undefined && updateUrl) {
+            for (let paramName of currentSearchParams.keys()) {
+                if (!newSearchParams.has(paramName)) {
+                    newSearchParams.set(paramName, currentSearchParams.get(paramName));
+                }
+            }
+            url = newUrl + "?" + newSearchParams.toString();
+            window.history.replaceState(null, null, url);
+        }
+        let url = newUrl.origin + newUrl.pathname + '?' + newSearchParams.toString()
+
+        const eventToEmitKey = formElement.getAttribute('data-event-key');
+        fetch(url, {
+            method: 'GET',
+            headers: {"X-CSRF-Token": this.token},
+        })
+            .then(response => response.text())
+            .then(html => {
+                Turbo.renderStreamMessage(html);
+                // The use of a custom event here allows multi-controller
+                // communication to handle some functionality when the response
+                // is ready to process.
+                if (eventToEmitKey) emitCustomEvent(eventToEmitKey);
+            })
+            .catch((err) => {
+                handleError(err, this.identifier);
+            });
     }
-  }
+
+    search(e) {
+        if (e.target.value) {
+            document.querySelector('#category--clear-icon-ref').classList.remove('invisible');
+        } else {
+            document.querySelector('#category--clear-icon-ref').classList.add('invisible');
+        }
+        if (!this.element.getAttribute('data-turbo')) {
+            this.element.requestSubmit();
+        } else {
+            this.doStreamRequest(this.element);
+        }
+    }
+
+    clearSearch() {
+        this.element.classList.add('invisible');
+        document.querySelector("[name=query]").value = null;
+        const formElement = document.querySelector('#search-form');
+        if (!formElement.getAttribute('data-turbo')) {
+            formElement.requestSubmit();
+        } else {
+            this.doStreamRequest(formElement);
+        }
+    }
 }
