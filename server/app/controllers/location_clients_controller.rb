0ca5be1c41cba9ccdde8eb4b30cf5890e332fd42
ignaciograsso <ignacio@exactlylabs.com>
2024-05-09 23:16:17 -0300

    adjust container height

--- server/app/controllers/location_clients_controller.rb
+++ server/app/controllers/location_clients_controller.rb
@@ -9,10 +9,10 @@ class LocationClientsController < ApplicationController
     @clients = @location.clients
     if FeatureFlagHelper.is_available('networks', current_user)
       status = params[:status]
-      @query = params[:query]
+      query = params[:query]
       @clients = @clients.where(online: status == 'online') if status.present?
       @total = @clients.count
-      @clients = @clients.where("unix_user ILIKE ?", "%#{@query}%") if @query.present?
+      @clients = @clients.where("unix_user ILIKE ?", "%#{query}%") if query.present?
 
       @clients = paginate(@clients, params[:page], params[:page_size])
     end
