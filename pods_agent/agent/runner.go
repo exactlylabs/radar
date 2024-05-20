4d991831e074cd8c332264ae5e4cba2ed7cb197d
Rafael Mendes <rafael@exactlylabs.com>
2024-05-20 19:06:53 -0300

    moved runners executor logic to runnersExecutor struct. Removed the call to infer the default interface if non is given. Now we use the old method which uses whatever default the system selects

--- pods_agent/agent/runner.go
+++ pods_agent/agent/runner.go
@@ -10,116 +10,50 @@ import (
 	"github.com/exactlylabs/go-errors/pkg/errors"
 	"github.com/exactlylabs/go-monitor/pkg/sentry"
 	"github.com/exactlylabs/radar/pods_agent/config"
-	"github.com/exactlylabs/radar/pods_agent/services/sysinfo/network/netroute"
 	"github.com/exactlylabs/radar/pods_agent/services/sysinfo/network/wifi"
 	"github.com/exactlylabs/radar/pods_agent/services/ws"
 )
 
-func startSpeedTestRunner(ctx context.Context, c *config.Config, runTestCh <-chan RunTestServerMessage, runners []Runner, reporter RadarClient) {
+type runnersExecutor struct {
+	runners   []Runner
+	reporter  RadarClient
+	c         *config.Config
+	wlanNames []string
+}
+
+func newExecutor(c *config.Config, runners []Runner, reporter RadarClient) *runnersExecutor {
+	wlanNames, err := wifi.WlanInterfaceNames()
+	if err != nil && !errors.Is(err, wifi.ErrNotSupported) {
+		sentry.NotifyErrorOnce(errors.W(err), map[string]sentry.Context{})
+	}
+	return &runnersExecutor{
+		runners:   runners,
+		reporter:  reporter,
+		c:         c,
+		wlanNames: wlanNames,
+	}
+}
+
+func (r *runnersExecutor) listen(ctx context.Context, runTestCh <-chan RunTestServerMessage) {
 	for msg := range runTestCh {
 		log.Println("Starting Speed Tests")
-		runUntilSuccessfull(ctx, runners, reporter, c, msg.Interfaces)
+		r.runUntilSuccessfull(ctx, msg.Interfaces)
 		log.Println("Finished running Speed Tests")
 	}
 }
 
-func runUntilSuccessfull(ctx context.Context, runners []Runner, reporter RadarClient, c *config.Config, interfaces []string) {
-
-	successfull := false
-	backoff := ws.NewExponentialBackOff(2*time.Second, time.Minute, 2.0)
-	if len(interfaces) == 0 {
-		// Fallback to default interface in case of nothing given
-		interfaces = []string{"default"}
-	}
-	wlanNames, err := wifi.WlanInterfaceNames()
-	if err != nil && !errors.Is(err, wifi.ErrNotSupported) {
-		panic(errors.W(err))
-	}
-	for !successfull {
-		for _, runner := range runners {
-			for _, iface := range interfaces {
-
-				if iface == "default" {
-					r, err := netroute.DefaultRoute()
-					if err == nil {
-						iface = r.Interface.Name
-					} else {
-						iface = "" // Runner internally should default to the default route
-					}
-				}
-				report := MeasurementReport{
-					Interface: iface,
-				}
-
-				// Gather ConnectionInfo of the Wifi signal and skip the test if it fails to retrieve it
-				if slices.Contains(wlanNames, iface) {
-					report.Wlan = true
-					status, err := getWlanConnInfo(iface)
-					if errors.Is(err, wifi.ErrNotConnected) || errors.Is(err, wifi.ErrNotSupported) {
-						// Send failed measurement
-						continue
-					} else if err != nil {
-						err = errors.W(err)
-						log.Println(err)
-						sentry.NotifyErrorOnce(err, map[string]sentry.Context{
-							"interface": {"name": iface},
-						})
-						continue
-					}
-					report.ConnectionInfo = status
-				}
-				runnerBackoff := ws.NewExponentialBackOff(5*time.Second, time.Minute, 2.0)
-				result, err := runRunner(runner, runnerBackoff, 3, iface)
-				if err != nil {
-					log.Println(errors.Wrap(err, "failed all attempts to run speed test"))
-					continue
-				}
-				report.Result = result.Raw
-
-				senderBackoff := ws.NewExponentialBackOff(15*time.Second, time.Minute, 2.0)
-				err = sendMeasurement(ctx, reporter, report, runner.Type(), senderBackoff, 5)
-				if err != nil {
-					log.Println(errors.Wrap(err, "failed all attempts to send speed test result to server"))
-					continue
-				}
-
-				c.LastTested = fmt.Sprintf("%d", time.Now().Unix())
-				c.LastDownloadSpeed = fmt.Sprintf("%.2f", result.DownloadMbps)
-				c.LastUploadSpeed = fmt.Sprintf("%.2f", result.UploadMbps)
-				if err := config.Save(c); err != nil {
-					log.Println(errors.Wrap(err, "failed to save config"))
-					sentry.NotifyErrorOnce(err, map[string]sentry.Context{})
-				}
-				successfull = true
-			}
-		}
-
-		if !successfull {
-			log.Println("agent.startSpeedTestRunner: all runners have failed, retrying after backoff period")
-			<-backoff.Next()
-		}
-	}
+func (r *runnersExecutor) fillWlanInfo(report *MeasurementReport, iface string) error {
+	report.Wlan = true
+	status, err := getWlanConnInfo(iface)
+	report.ConnectionInfo = status
+	return err
 }
 
-func runRunner(runner Runner, backoff *ws.ExponentialBackOff, retries int, ifaceName string) (res *Measurement, err error) {
+// sendMeasurement with a retry mechanism, to avoid causing another run of the runner interface
+func (r *runnersExecutor) sendMeasurement(ctx context.Context, report MeasurementReport, runner Runner, retries int) (err error) {
+	backoff := ws.NewExponentialBackOff(15*time.Second, time.Minute, 2.0)
 	for i := 0; i < retries; i++ {
-		res, err = runner.RunForInterface(context.Background(), ifaceName)
-		if err != nil {
-			log.Println(errors.Wrap(err, "failed to run speed test"))
-			if !errors.Is(err, ErrRunnerConnectionError) {
-				sentry.NotifyErrorOnce(err, map[string]sentry.Context{})
-			}
-			<-backoff.Next()
-			continue
-		}
-		return res, nil
-	}
-	return nil, err
-}
-
-func sendMeasurement(ctx context.Context, reporter RadarClient, report MeasurementReport, runnerName string, backoff *ws.ExponentialBackOff, retries int) (err error) {
-	for i := 0; i < retries; i++ {
-		err = reporter.SendMeasurement(ctx, runnerName, report)
+		err = r.reporter.SendMeasurement(ctx, runner.Type(), report)
 		if err != nil {
 			err = errors.Wrap(err, "failed to send speed test result to server")
 			log.Println(err)
@@ -132,6 +66,79 @@ func sendMeasurement(ctx context.Context, reporter RadarClient, report Measureme
 		return nil
 	}
 	return err
+}
+
+func (r *runnersExecutor) execute(ctx context.Context, runner Runner, iface string) (report *MeasurementReport, err error) {
+	report = &MeasurementReport{}
+	report.Interface = iface
+
+	// Gather ConnectionInfo of the Wifi signal and skip the test if it fails to retrieve it
+	if slices.Contains(r.wlanNames, iface) {
+		if err := r.fillWlanInfo(report, iface); err != nil {
+			return nil, errors.W(err).WithMetadata(errors.Metadata{"interface": iface})
+		}
+	}
+
+	// Execute the Runner using either the selected interface or the default in case none given.
+	var result *Measurement
+	if iface != "" {
+		result, err = runner.RunForInterface(ctx, iface)
+	} else {
+		result, err = runner.Run(ctx)
+	}
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to run speed test").WithMetadata(errors.Metadata{"runner": runner.Type(), "interface": iface})
+	}
+	report.Result = result.Raw
+
+	// Report back to the server
+	err = r.sendMeasurement(ctx, *report, runner, 5)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed all attempts to send speed test result to server")
+	}
+
+	// Update local configuration file with the latest test results (watchdog uses this information from the config file)
+	r.c.LastTested = fmt.Sprintf("%d", time.Now().Unix())
+	r.c.LastDownloadSpeed = fmt.Sprintf("%.2f", result.DownloadMbps)
+	r.c.LastUploadSpeed = fmt.Sprintf("%.2f", result.UploadMbps)
+	if err := config.Save(r.c); err != nil {
+		log.Println(errors.Wrap(err, "failed to save config"))
+		sentry.NotifyErrorOnce(err, map[string]sentry.Context{})
+	}
+
+	return report, nil
+}
+
+// runUntilSuccessfull will block until at least one measurement report was sent successfully to the server
+func (r *runnersExecutor) runUntilSuccessfull(ctx context.Context, interfaces []string) {
+	successfull := false
+	backoff := ws.NewExponentialBackOff(2*time.Second, time.Minute, 2.0)
+	if len(interfaces) == 0 {
+		interfaces = append(interfaces, "")
+	}
+
+	for !successfull {
+		for _, runner := range r.runners {
+			for _, iface := range interfaces {
+				_, err := r.execute(ctx, runner, iface)
+				if err != nil {
+					err = errors.W(err)
+					log.Println(err)
+					if !errorIsAny(err, ErrRunnerConnectionError, wifi.ErrNotConnected, wifi.ErrNotSupported) {
+						// Nothing to do about these errors
+						sentry.NotifyErrorOnce(err, map[string]sentry.Context{})
+					}
+					continue
+				}
+				successfull = true
+			}
+		}
+
+		if !successfull {
+			log.Println("agent.startSpeedTestRunner: all runners have failed, retrying after backoff period")
+			<-backoff.Next()
+		}
+	}
 }
 
 func getWlanConnInfo(name string) (*wifi.WifiStatus, error) {
