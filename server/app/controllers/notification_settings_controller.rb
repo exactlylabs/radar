d14b7de22f705a22d8a30a5a268227fd625d2e2f
ignaciograsso <ignacio@exactlylabs.com>
2024-05-24 16:21:30 -0300

    reduce responsibility from policy scope

--- server/app/controllers/notification_settings_controller.rb
+++ server/app/controllers/notification_settings_controller.rb
@@ -22,6 +22,17 @@ class NotificationSettingsController < ApplicationController
 
   def set_notification_settings
     @notification_settings = policy_scope(NotificationSettings)
+    if @notification_settings.nil?
+      user_notifications_settings = NotificationSettings.where(user: @auth_holder.user)
+      if user_notifications_settings.count > 0
+        @notification_settings = user_notifications_settings.first.dup
+        @notification_settings.account = @auth_holder.account
+        @notification_settings.save
+      else
+        @notification_settings = NotificationSettings.create(user: @auth_holder.user, account: @auth_holder.account)
+      end
+    end
+    @notification_settings
   end
 
   def update_notification_settings_from_user_all_accounts(setting_name, setting_value)
