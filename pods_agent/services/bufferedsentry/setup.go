a95e59eafe6f4085bd610d6fa13d3417e1c6bd70
Rafael Mendes <rafael@exactlylabs.com>
2024-05-27 19:20:33 -0300

    deactivated buffered sentry due to spike of alerts

--- pods_agent/services/bufferedsentry/setup.go
+++ pods_agent/services/bufferedsentry/setup.go
@@ -8,8 +8,8 @@ func Setup(dsn, clientId, version string, environment string, application string
 	if dsn == "" {
 		return
 	}
-	sentry.Setup(dsn, version, environment, application, sentry.WithTransport(newCustomSentryTranport(bufferDir, dsn)))
-
+	// sentry.Setup(dsn, version, environment, application, sentry.WithTransport(newCustomSentryTranport(bufferDir, dsn)))
+	sentry.Setup(dsn, version, environment, application)
 	sentry.ConfigureScope(func(scope *sentry.Scope) {
 		scope.SetUser(sentry.User{ID: clientId})
 	})
