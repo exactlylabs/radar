48885a20d0daebf97db302ad908bf7e5ebb2407b
Rafael Mendes <rafael@exactlylabs.com>
2024-05-30 17:51:06 -0300

    pub/sub machanism to the events generated by our models

--- server/app/subscribers/events_subscriber.rb
+++ server/app/subscribers/events_subscriber.rb
@@ -5,15 +5,19 @@ class EventsSubscriber
   # Register a subscription to an event from our EventSourceable module
   #
   # Example:
-  #  subscribe(Client, ["WENT_ONLINE"]) do |event|
-  #      # Do something with Event model class.
+  # class Subscriber
+  #  def self.call
+  #    subscribe(Client, ["WENT_ONLINE"]) do |event|
+  #        # Do something with Event model class.
+  #    end
   #  end
+  # end
   def self.subscribe(aggregate_class, to, &block)
     to = [to] unless to.is_a?(Array)
     to.each do |event_name|
       event_name = "#{aggregate_class.name}::#{event_name}"
       ActiveSupport::Notifications.subscribe(event_name) do |name, started, finished, unique_id, data|
-        block.call(data)
+        block.call(data[:payload])
       end
     end
   end
--- server/app/subscribers/pod_connectivity_config_subscriber.rb
+++
@@ -1,7 +0,0 @@
-class PodConnectivityConfigSubscriber < EventsSubscriber
-
-  subscribe(PodConnectivityConfig, [PodConnectivityConfig::Events::WLAN_CONNECTED, PodConnectivityConfig::Events::WLAN_DISCONNECTED]) do |event|
-    # Call job to notify users that own this pod and are signed for receiving this notification
-    NotifyClientWlanStatusChangeJob.perform_later(event.aggregate, event.data["to"])
-  end
-end
