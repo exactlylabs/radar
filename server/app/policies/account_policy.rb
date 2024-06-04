2f7d2151cb3a5208ca3b34eadd38f7a493437029
iuke <edamm@itba.edu.ar>
2024-06-04 16:04:30 +0200

    fixed broken listener on categories dropdown

--- server/app/policies/account_policy.rb
+++ server/app/policies/account_policy.rb
@@ -7,7 +7,7 @@ class AccountPolicy < ApplicationPolicy
           scope.all.not_deleted
         else
           own_accounts = user.accounts.not_deleted
-          shared = user.shared_accounts
+          shared = user.shared_accounts.not_deleted
           # Bring back array to actual ORM model to be able to use it as usual
           scope.where(id: (own_accounts + shared).map {|a| a.id})
         end
