aeee84445ac53fa7767b152c8049320eeae426db
Rafael Mendes <rafael@exactlylabs.com>
2024-06-05 11:48:12 -0300

    fix non super user access to pods details

--- server/app/policies/client_policy.rb
+++ server/app/policies/client_policy.rb
@@ -1,20 +1,18 @@
 class ClientPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      if @auth_holder&.is_all_accounts?
+      if @auth_holder&.user.super_user? && !@auth_holder.is_super_user_disabled?
+        scope.all
+      elsif @auth_holder&.is_all_accounts?
         user = @auth_holder.user
-        if user.super_user && !@auth_holder.is_super_user_disabled?
-          scope.all
-        else
-          all_pods = []
-          user.accounts.not_deleted.each do |account|
-            all_pods.append(*account.clients.pluck(:id))
-          end
-          user.shared_accounts.each do |account|
-            all_pods.append(*account.clients.pluck(:id))
-          end
-          scope.where(id: all_pods)
+        all_pods = []
+        user.accounts.not_deleted.each do |account|
+          all_pods.append(*account.clients.pluck(:id))
         end
+        user.shared_accounts.each do |account|
+          all_pods.append(*account.clients.pluck(:id))
+        end
+        scope.where(id: all_pods)
       else
         super
       end
