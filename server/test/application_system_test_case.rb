e0f9ef6ff4684ec52398a520ee8961233cef8b20
iuke <edamm@itba.edu.ar>
2024-06-05 18:28:38 +0200

    test chromedriver version fix

--- server/test/application_system_test_case.rb
+++ server/test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  Webdrivers::Chromedriver.required_version = "114.0.5735.90"
+  Webdrivers::Chromedriver.required_version = "125.0.6422.141"
   DRIVER = ENV["CI"] ? :headless_chrome : :chrome
   driven_by :selenium, using: DRIVER, screen_size: [1400, 1400]
 end
