a5e87249ecd69258177dc0ed9c425d143846f749
iuke <edamm@itba.edu.ar>
2024-06-14 13:16:27 +0200

    remove debug message

--- server/app/controllers/dashboard_controller.rb
+++ server/app/controllers/dashboard_controller.rb
@@ -40,7 +40,6 @@ class DashboardController < ApplicationController
     end
     if current_account.present?
       params = as_orgs_filters_params(current_account)
-      puts "PARAMS: #{params}"
       @filter_as_orgs = ActiveRecord::Base.connection.execute(DashboardHelper.get_as_orgs_sql(params[:account_ids], params[:from], params[:to], location_ids: params[:location_ids]))
     end
   end
