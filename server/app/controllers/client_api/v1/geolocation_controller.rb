538caacab0c98f8b976444e98b634ea548b53806
iuke <edamm@itba.edu.ar>
2024-06-27 12:52:32 +0200

    some dev exp improvements for speedtest BE;

--- server/app/controllers/client_api/v1/geolocation_controller.rb
+++ server/app/controllers/client_api/v1/geolocation_controller.rb
@@ -51,6 +51,9 @@ module ClientApi
         # By using remote_ip we would get the actual client ip even
         # after going through proxies/nginx.
         # https://stackoverflow.com/questions/10997005/whats-the-difference-between-request-remote-ip-and-request-ip-in-rails#:~:text=request.remote_ip%20is%20smarter%20and%20gets%20the%20actual%20client%20ip.%20This%20can%20only%20be%20done%20if%20the%20all%20the%20proxies%20along%20the%20way%20set%20the%20X%2DForwarded%2DFor%20header.
+
+        # Coordinates for middle of USA for development
+        return render json: [39.50, -98.35] if ENV['RAILS_ENV'] == 'development'
         results = Geocoder.search(request.remote_ip)
         if results.first
           return render json: results.first.coordinates
