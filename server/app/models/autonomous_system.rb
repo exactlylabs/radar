823827565b2bd1196c37e9adfb79465608b736f8
Rafael Mendes <rafael@exactlylabs.com>
2024-07-02 10:40:22 -0300

    AsnIpLookup model and relationships

--- server/app/models/autonomous_system.rb
+++ server/app/models/autonomous_system.rb
@@ -3,5 +3,11 @@ class AutonomousSystem < ApplicationRecord
     has_many :clients
     has_many :measurements
     has_many :client_speed_tests
+    has_many :asn_ip_lookups
     validates :asn, uniqueness: true
+
+
+    def self.find_by_ip(ip)
+      AsnIpLookup.eager_load(:autonomous_system).where("? << ip", ip).order("masklen(ip) DESC").first&.autonomous_system
+    end
 end
