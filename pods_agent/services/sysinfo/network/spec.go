fdc5d846978dca704823fa841d4dd053ee682781
Rafael Mendes <rafael@exactlylabs.com>
2024-07-02 21:15:54 -0300

    sysinfo/network package with more data on the network type and connection status - WIP

--- pods_agent/services/sysinfo/network/spec.go
+++ pods_agent/services/sysinfo/network/spec.go
@@ -4,12 +4,40 @@ import (
 	"net"
 )
 
+type NetType string
+
+const (
+	Virtual  NetType = "virtual"
+	Ethernet NetType = "ethernet"
+	Wlan     NetType = "wlan"
+)
+
+type NetStatus string
+
+const (
+	ConnectedWithInternet NetStatus = "connected_with_internet"
+	ConnectedNoInternet   NetStatus = "connected_no_internet"
+	Disconnected          NetStatus = "disconnected"
+)
+
+type ConnectionError string
+
+const (
+	NoError       ConnectionError = ""
+	WrongPassword ConnectionError = "wrong_password"
+	SSIDNotFound  ConnectionError = "ssid_not_found"
+	UnknownError  ConnectionError = "unknown_error"
+)
+
 type NetInterface struct {
 	Name         string     `json:"name"`
 	MAC          string     `json:"mac"`
 	IPs          []net.Addr `json:"ips"`
 	DefaultRoute bool       `json:"default"`
-	IsWlan       bool       `json:"is_wlan"`
+	Type         NetType    `json:"type"`
+	IsWlan       bool       `json:"is_wlan"` // To deprecate. Use Type instead
+	Status       NetStatus  `json:"status"`
+	Enabled      bool       `json:"enabled"`
 }
 
 func (n NetInterface) UnicastAddress() *net.IPNet {
