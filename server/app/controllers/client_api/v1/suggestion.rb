452a26bec3603f5e3b09a8e7b4c0b13943f9de0c
iuke <edamm@itba.edu.ar>
2024-07-04 17:17:39 +0200

    new BE fixes for zip code geocoding

--- server/app/controllers/client_api/v1/suggestion.rb
+++ server/app/controllers/client_api/v1/suggestion.rb
@@ -5,7 +5,25 @@ module ClientApi
 
       def initialize(city, street, state, postal_code, house_number, coordinates)
         @city, @street, @state, @postal_code, @house_number, @coordinates = city, street, state, postal_code, house_number, coordinates
-        @address = "#{house_number} #{street}, #{city}, #{state} #{postal_code}"
+        # We now want these fields to be optional, so the address string has to be created progressively
+        @address = ""
+        if house_number.present? && street.present?
+          @address += "#{house_number} #{street},"
+        elsif street.present?
+          @address += "#{street},"
+        end
+
+        if city.present?
+          @address += " #{city},"
+        end
+
+        if state.present?
+          @address += " #{state}"
+        end
+
+        if postal_code.present?
+          @address += " #{postal_code}"
+        end
       end
 
       def ==(other)
