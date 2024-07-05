d627f748ca3bad72668fb841f0deabc153e756b1
Rafael Mendes <rafael@exactlylabs.com>
2024-07-05 11:03:16 -0300

    setup.sh to include the files version

--- server/scripts/setup.sh
+++ server/scripts/setup.sh
@@ -17,8 +17,10 @@ mkdir -p ${TARGET_DIR}
 # ASN code to Organization Map
 URL="https://publicdata.caida.org/datasets/as-organizations"
 FILE=$(curl -s -L $URL | grep -o '".*.jsonl.gz"' | sed 's/"//g' | sort | tail -n 1)
+FILE_DATE=$(echo ${FILE} | sed 's/\..*//') # Drop anything after the first dot
 curl -s -f "${URL}/${FILE}" -o "${TARGET_DIR}/as-org2info.jsonl.gz"
 gzip -fd ${TARGET_DIR}/as-org2info.jsonl.gz
+echo -n ${FILE_DATE} > ${TARGET_DIR}/.as-org2info_version
 
 # IPVx to ASN Map
 rm -rf ${TARGET_DIR}/GeoLite2-ASN-CSV_*
@@ -27,6 +29,9 @@ curl -L -s -f "${URL}" -o "${TARGET_DIR}/GeoLite2-ASN-CSV.zip"
 unzip -o -d ${TARGET_DIR} ${TARGET_DIR}/GeoLite2-ASN-CSV.zip
 cp ${TARGET_DIR}/GeoLite2-ASN-CSV_*/*IPv4.csv ${TARGET_DIR}/
 cp ${TARGET_DIR}/GeoLite2-ASN-CSV_*/*IPv6.csv ${TARGET_DIR}/
+
+FILE_DATE=$(ls -d ${TARGET_DIR}/GeoLite2-ASN-CSV_* | sed 's/.*_//')
+echo -n $FILE_DATE > ${TARGET_DIR}/.geo_lite2_version
 
 rm -r $(ls -d ${TARGET_DIR}/GeoLite2-ASN-CSV_*)
 rm $(ls ${TARGET_DIR}/*.zip)
