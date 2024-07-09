60abefba7ad6123c2277475a132082fa1b02139e
Rafael Mendes <rafael@exactlylabs.com>
2024-07-09 14:59:05 -0300

    moved betterstack keys to an initializer, and getting it from envvars

--- server/clock.rb
+++ server/clock.rb
@@ -8,7 +8,7 @@ scheduler.every '5s', overlap: false do
       Client.update_outdated_online!
       Location.update_online_status!
       Client.resend_missed_test_requests!
-      Rails.application.send_cronjob_heartbeat("WeBSSynbt69rbruQXhEyWWVd") if Rails.env.production?
+      Rails.application.send_cronjob_heartbeat(BETTERSTACK_KEYS[:online_status_update_cron])
     end
   rescue => e
     Sentry.capture_exception(e)
@@ -23,7 +23,7 @@ end
 scheduler.every '15s', overlap: false do
   begin
     Client.request_scheduled_tests!
-    Rails.application.send_cronjob_heartbeat("LMZAerCdkZiuZrcjPDVbVpMh") if Rails.env.production?
+    Rails.application.send_cronjob_heartbeat(BETTERSTACK_KEYS[:request_tests_cron])
   rescue => e
     Sentry.capture_exception(e)
     raise e
