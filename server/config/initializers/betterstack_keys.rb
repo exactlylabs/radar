d572780c598ee85668c543d9bd53c5104099c43a
Rafael Mendes <rafael@exactlylabs.com>
2024-07-09 14:59:05 -0300

    moved betterstack keys to an initializer, and getting it from envvars

---
+++ server/config/initializers/betterstack_keys.rb
@@ -0,0 +1,4 @@
+BETTERSTACK_KEYS = {
+    request_tests_cron: ENV["BETTERSTACK_TESTS_CRON_KEY"],
+    online_status_update_cron: ENV["BETTERSTACK_STATUS_CRON_KEY"]
+}
--- server/lib/monitor/monitor.rb
+++ server/lib/monitor/monitor.rb
@@ -71,6 +71,7 @@ module HealthMonitor
     # Send a GET request to Betterstack's Heartbeat monitor, to notify the cron job is active
     #
     def send_cronjob_heartbeat(key)
+      return if key.empty?
       uri = URI("https://uptime.betterstack.com/api/v1/heartbeat/#{key}")
       Net::HTTP.get_response(uri)
     end
