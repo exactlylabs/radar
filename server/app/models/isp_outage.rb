25e94d55fb239b5db730c862bf0a1b80cc7febed
Rafael Mendes <rafael@exactlylabs.com>
2024-08-06 08:33:54 -0700

    Refactor in Outages models to better reflect the needs of the system

--- server/app/models/isp_outage.rb
+++ server/app/models/isp_outage.rb
@@ -1,24 +1,7 @@
 class IspOutage < ApplicationRecord
-  include EventSourceable
-
-  module Events
-    CREATED = "CREATED"
-    FINISHED = "FINISHED"
-  end
-
-  notify_change :end_time, Events::FINISHED
-  
+  has_many :network_outages
   belongs_to :autonomous_system
-  
-  scope :where_active, -> { where("end_time IS NULL") }
-  scope :where_intersects, -> (start_time, end_time) { where("start_time >= ? AND start_time <= ?", start_time, end_time) }
 
-  def send_created_event
-    super
-    # Cover the case when an outage is created with an end_time already set (retroactive outage)
-    if self.end_time.present?
-      send_field_changed_event(:end_time, nil, self.end_time, self.end_time)
-    end
-  end
+  default_scope -> { where(cancelled_at: nil) }
 
 end
--- server/app/models/network_outage.rb
+++ server/app/models/network_outage.rb
@@ -1,9 +1,10 @@
 class NetworkOutage < ApplicationRecord
   belongs_to :location
-  belongs_to :outage_event, optional: true
+  belongs_to :isp_outage, optional: true
   belongs_to :autonomous_system, optional: true
 
   enum status: { active: 0, cancelled: 1, resolved: 2 }
+  enum outage_type: { unknown_reason: 0, network_failure: 1, isp_outage: 2, power_outage: 3 }
 
   scope :where_active, -> { where(status: :active) }
   scope :where_resolved, -> { where(status: :resolved) }
