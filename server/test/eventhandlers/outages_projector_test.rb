40e4107663a5d9b27fcb5ae42775fb2cdb79169b
Rafael Mendes <rafael@exactlylabs.com>
2024-08-06 22:58:18 -0700

    test for isp outage detection and fixes

--- server/test/eventhandlers/outages_projector_test.rb
+++ server/test/eventhandlers/outages_projector_test.rb
@@ -190,4 +190,146 @@ class OutagesProjectorTest < ActiveSupport::TestCase
     assert no.power_outage?
     assert no.resolved?
   end
+
+  test "when network outage within existing isp outage, expect it gets assigned" do
+    network_outage = NetworkOutage.create!(
+      outage_type: :network_failure,
+      location: locations(:outage_location1),
+      autonomous_system: autonomous_systems(:outage_as1),
+      status: :resolved,
+      started_at: Time.new(2024, 7, 1),
+      resolved_at: Time.new(2024, 8, 1),
+    )
+    isp_outage = IspOutage.create!(
+      autonomous_system: autonomous_systems(:outage_as1),
+      offline_window_start: network_outage.started_at - 1.minute,
+      offline_window_end: network_outage.started_at + 2.minute,
+      online_window_start: network_outage.resolved_at - 2.minute,
+      online_window_end: network_outage.resolved_at + 1.minute,
+    )
+
+    OutagesProjector.new.detect_isp_outage(network_outage)
+    network_outage.reload
+
+    assert network_outage.isp_outage?
+    assert_equal network_outage.isp_outage, isp_outage
+  end
+
+  test "when network outage not within existing isp outage, expect nothing changes" do
+    network_outage = NetworkOutage.create!(
+      outage_type: :network_failure,
+      location: locations(:outage_location1),
+      autonomous_system: autonomous_systems(:outage_as1),
+      status: :resolved,
+      started_at: Time.new(2024, 7, 1),
+      resolved_at: Time.new(2024, 8, 1),
+    )
+    isp_outage = IspOutage.create!(
+      autonomous_system: autonomous_systems(:outage_as1),
+      offline_window_start: network_outage.started_at + 4.minute,
+      offline_window_end: network_outage.started_at + 6.minute, # Start is out of the range
+      online_window_start: network_outage.resolved_at - 2.minute,
+      online_window_end: network_outage.resolved_at + 1.minute,
+    )
+
+    OutagesProjector.new.detect_isp_outage(network_outage)
+    network_outage.reload
+
+    assert network_outage.network_failure?
+    assert_nil network_outage.isp_outage
+  end
+
+  test "when 4 network outages within online and offline window, expect new isp outage" do
+    network_outage1 = NetworkOutage.create!(
+      outage_type: :network_failure,
+      location: locations(:outage_location1),
+      autonomous_system: autonomous_systems(:outage_as1),
+      status: :resolved,
+      started_at: Time.new(2024, 7, 1),
+      resolved_at: Time.new(2024, 8, 1),
+    )
+
+    network_outage2 = NetworkOutage.create!(
+      outage_type: :network_failure,
+      location: locations(:outage_location2),
+      autonomous_system: autonomous_systems(:outage_as1),
+      status: :resolved,
+      started_at: Time.new(2024, 7, 1) + 0.3.minute,
+      resolved_at: Time.new(2024, 8, 1) - 0.5.minute,
+    )
+
+    network_outage3 = NetworkOutage.create!(
+      outage_type: :network_failure,
+      location: locations(:outage_location3),
+      autonomous_system: autonomous_systems(:outage_as1),
+      status: :resolved,
+      started_at: Time.new(2024, 7, 1) + 0.5.minute,
+      resolved_at: Time.new(2024, 8, 1) + 0.6.minute,
+    )
+
+    network_outage4 = NetworkOutage.create!(
+      outage_type: :network_failure,
+      location: locations(:outage_location4),
+      autonomous_system: autonomous_systems(:outage_as1),
+      status: :resolved,
+      started_at: Time.new(2024, 7, 1) + 0.9.minute,
+      resolved_at: Time.new(2024, 8, 1) + 0.9.minute,
+    )
+    
+    assert_changes 'IspOutage.count' do
+      OutagesProjector.new.detect_isp_outage(network_outage3)
+    end
+
+    network_outage1.reload
+    network_outage2.reload
+    network_outage3.reload
+    network_outage4.reload
+    assert network_outage1.isp_outage?
+    assert network_outage2.isp_outage?
+    assert network_outage3.isp_outage?
+    assert network_outage4.isp_outage?
+
+  end
+
+  test "when some outages not within online and offline window, expect no new isp outage" do
+    network_outage1 = NetworkOutage.create!(
+      outage_type: :network_failure,
+      location: locations(:outage_location1),
+      autonomous_system: autonomous_systems(:outage_as1),
+      status: :resolved,
+      started_at: Time.new(2024, 7, 1),
+      resolved_at: Time.new(2024, 8, 1),
+    )
+
+    network_outage2 = NetworkOutage.create!(
+      outage_type: :network_failure,
+      location: locations(:outage_location2),
+      autonomous_system: autonomous_systems(:outage_as1),
+      status: :resolved,
+      started_at: Time.new(2024, 7, 1) + 0.3.minute,
+      resolved_at: Time.new(2024, 8, 1) - 0.5.minute,
+    )
+
+    network_outage3 = NetworkOutage.create!(
+      outage_type: :network_failure,
+      location: locations(:outage_location3),
+      autonomous_system: autonomous_systems(:outage_as1),
+      status: :resolved,
+      started_at: Time.new(2024, 7, 1) + 0.5.minute,
+      resolved_at: Time.new(2024, 8, 1) + 0.6.minute,
+    )
+
+    network_outage3 = NetworkOutage.create!(
+      outage_type: :network_failure,
+      location: locations(:outage_location4),
+      autonomous_system: autonomous_systems(:outage_as1),
+      status: :resolved,
+      started_at: Time.new(2024, 7, 1) + 2.minute,
+      resolved_at: Time.new(2024, 8, 1) + 0.9.minute,
+    )
+    
+    assert_no_changes 'IspOutage.count' do
+      OutagesProjector.new.detect_isp_outage(network_outage3)
+    end
+  end
 end
