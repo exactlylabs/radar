a73a2d97e5e81f9b2f2b9158a16ab37d2097f583
Rafael Mendes <rafael@exactlylabs.com>
2024-08-14 14:12:37 -0300

    fix to group outages by parent (isp outage)

--- server/app/controllers/outages_controller.rb
+++ server/app/controllers/outages_controller.rb
@@ -22,17 +22,23 @@ class OutagesController < ApplicationController
 
       @outages = NetworkOutage.where(id: @outage_ids.map { |outage| outage['id'] })
                             .order(started_at: :asc)
-                            .joins(:location)
-                            .joins(:autonomous_system)
+                            .joins("LEFT JOIN isp_outages ON isp_outages.id = network_outages.isp_outage_id")
+                            .joins("JOIN locations ON locations.id = network_outages.location_id")
+                            .left_joins(:autonomous_system)
                             .select("
         network_outages.id,
         network_outages.autonomous_system_id,
         network_outages.outage_type,
         network_outages.started_at,
         network_outages.location_id,
+        network_outages.isp_outage_id,
+        locations.name as location_name,
+        isp_outages.offline_window_start AS isp_outage_started_at,
+        isp_outages.online_window_end AS isp_outage_resolved_at,
         COALESCE(network_outages.resolved_at, NOW()) AS resolved_at,
         EXTRACT(EPOCH FROM COALESCE(network_outages.resolved_at, NOW()) - network_outages.started_at) * 1000 AS duration
       ")
+      @outages = OutagesHelper.join_by_parent(@outages)
       @outages = OutagesHelper.group_outages(@outages, 'desc', @grouped)
     else
       @outages = {}
