913c0a45c640b0da3890cb65cc48f00ab5d1106d
Asher Morgan <59518073+ashermorgan@users.noreply.github.com>
2024-09-05 12:46:29 -0700

    Update export ready email design

--- server/app/mailers/exports_mailer.rb
+++ server/app/mailers/exports_mailer.rb
@@ -2,8 +2,9 @@ class ExportsMailer < ApplicationMailer
   def export_ready_email
     @user = params[:user]
     @url = params[:url]
-    attachments.inline["new-logo.png"] = File.read("#{Rails.root}/app/assets/images/new-logo.png")
-    attachments.inline["download-ready-icon.png"] = File.read("#{Rails.root}/app/assets/images/download-ready-icon.png")
+    attachments.inline["new-radar-logo.png"] = File.read("#{Rails.root}/app/assets/images/new-radar-logo.png")
+    attachments.inline["downloads-icon.png"] = File.read("#{Rails.root}/app/assets/images/downloads-icon.png")
+    attachments.inline["download-archive-white.svg"] = File.read("#{Rails.root}/app/assets/images/download-archive-white.svg")
     mail(to: @user.email, subject: 'Your download is ready')
   end
 
