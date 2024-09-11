7faeb16dbdf1a80fa214615b2c282bc9fd12a2e7
ignaciograsso <ignacio@exactlylabs.com>
2024-09-11 13:23:12 -0300

    cr changes

--- server/app/javascript/controllers/timezone_controller.js
+++ server/app/javascript/controllers/timezone_controller.js
@@ -1,18 +1,7 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-
-    /**
-     * By using this controller, we can identify user's timezone with more precision than native Rails Time API
-     * This is intended to be used specifically in tooltips for all of our tables which include time-related columns.
-     */
     connect() {
-        /**
-         * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/resolvedOptions#:~:text=as%20default%20values.-,timeZone,-The%20value%20provided
-         * Intl.DateTimeFormat().resolvedOptions().timeZone returns users' browser timezone value.
-         * An example of possible output: America/Buenos_Aires
-         * An example of possible output: UTC
-         */
         const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
         const tzCookie = document.cookie.split(';').find((item) => item.trim().startsWith('timezone='));
         if (tzCookie) {
--- server/config/application.rb
+++ server/config/application.rb
@@ -22,8 +22,6 @@ module Radar
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    config.time_zone = 'UTC'
-    config.active_record.default_timezone = :utc
 
     # config.eager_load_paths << Rails.root.join("extras")
 
