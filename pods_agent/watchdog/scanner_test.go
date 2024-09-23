8141d4884b3204e8d65a9346f41c0a3c0f505fea
Rafael Mendes <rafael@exactlylabs.com>
2024-09-23 15:00:25 -0300

    changes in watchdog specs to not not have it depending on sysinfo structures/variables.

--- pods_agent/watchdog/scanner_test.go
+++ pods_agent/watchdog/scanner_test.go
@@ -1,26 +1,27 @@
-package watchdog
+package watchdog_test
 
 import (
 	"os"
-	"strings"
 	"testing"
 	"time"
 
 	"github.com/exactlylabs/radar/pods_agent/config"
 	"github.com/exactlylabs/radar/pods_agent/services/sysinfo/network"
+	"github.com/exactlylabs/radar/pods_agent/watchdog"
 	"github.com/exactlylabs/radar/pods_agent/watchdog/mocks"
 )
 
 //go:generate mockery
 
 func mustReadFile(filePath string) []byte {
-	val, err := OSFS.ReadFile(filePath)
+	val, err := watchdog.OSFS.ReadFile(filePath)
 	if err != nil {
 		panic(err)
 	}
 	return val
 }
 
+var utc, _ = time.LoadLocation("UTC")
 var otherTz, _ = time.LoadLocation("America/Sao_Paulo")
 
 func TestScanSystemNoChange(t *testing.T) {
@@ -39,11 +40,14 @@ func TestScanSystemNoChange(t *testing.T) {
 	m.On("EnsurePathPermissions", "/tmp/tracing_buffer", os.FileMode(0777)).Return(nil)
 	m.On("EnsureUserGroups", "radar", []string{"netdev"}).Return(false, nil)
 	m.On("EnsureWifiEnabled").Return(nil)
-	m.On("PodAgentRunning").Return(false, nil)
-	m.On("EthernetStatus").Return(network.Disconnected, nil)
+	m.On("SystemStatus").Return(watchdog.SystemStatus{
+		WlanStatus:      network.Disconnected,
+		EthernetStatus:  network.Disconnected,
+		PodAgentRunning: false,
+	}, nil)
 	c := &config.Config{}
 	c.ClientId = "1234"
-	scanner := NewScanner(m)
+	scanner := watchdog.NewScanner(m)
 	go scanner.ScanSystem(c)
 	select {
 	case evt := <-scanner.Events():
@@ -70,16 +74,19 @@ func TestScanSystemHostDiffers(t *testing.T) {
 	m.On("EnsurePathPermissions", "/tmp/tracing_buffer", os.FileMode(0777)).Return(nil)
 	m.On("EnsureUserGroups", "radar", []string{"netdev"}).Return(false, nil)
 	m.On("EnsureWifiEnabled").Return(nil)
-	m.On("PodAgentRunning").Return(false, nil)
-	m.On("EthernetStatus").Return(network.Disconnected, nil)
+	m.On("SystemStatus").Return(watchdog.SystemStatus{
+		WlanStatus:      network.Disconnected,
+		EthernetStatus:  network.Disconnected,
+		PodAgentRunning: false,
+	}, nil)
 	c := &config.Config{}
 	c.ClientId = "1234"
-	scanner := NewScanner(m)
+	scanner := watchdog.NewScanner(m)
 	go scanner.ScanSystem(c)
 	select {
 	case evt := <-scanner.Events():
-		if evt.EventType != SystemFileChanged {
-			t.Fatalf("expected a SystemFileChanged event")
+		if evt.EventType != watchdog.SystemFileChanged {
+			t.Fatalf("expected a watchdog.SystemFileChanged event")
 		}
 	case <-time.NewTimer(1 * time.Second).C:
 		t.Fatalf("expected an event")
@@ -103,16 +110,19 @@ func TestScanSystemRCLocalDiffers(t *testing.T) {
 	m.On("EnsurePathPermissions", "/tmp/tracing_buffer", os.FileMode(0777)).Return(nil)
 	m.On("EnsureUserGroups", "radar", []string{"netdev"}).Return(false, nil)
 	m.On("EnsureWifiEnabled").Return(nil)
-	m.On("PodAgentRunning").Return(false, nil)
-	m.On("EthernetStatus").Return(network.Disconnected, nil)
+	m.On("SystemStatus").Return(watchdog.SystemStatus{
+		WlanStatus:      network.Disconnected,
+		EthernetStatus:  network.Disconnected,
+		PodAgentRunning: false,
+	}, nil)
 	c := &config.Config{}
 	c.ClientId = "1234"
-	scanner := NewScanner(m)
+	scanner := watchdog.NewScanner(m)
 	go scanner.ScanSystem(c)
 	select {
 	case evt := <-scanner.Events():
-		if evt.EventType != SystemFileChanged {
-			t.Fatalf("expected a SystemFileChanged event")
+		if evt.EventType != watchdog.SystemFileChanged {
+			t.Fatalf("expected a watchdog.SystemFileChanged event")
 		}
 	case <-time.NewTimer(1 * time.Second).C:
 		t.Fatalf("expected an event")
@@ -136,16 +146,19 @@ func TestScanSystemBootConfigDiffers(t *testing.T) {
 	m.On("EnsurePathPermissions", "/tmp/tracing_buffer", os.FileMode(0777)).Return(nil)
 	m.On("EnsureUserGroups", "radar", []string{"netdev"}).Return(false, nil)
 	m.On("EnsureWifiEnabled").Return(nil)
-	m.On("PodAgentRunning").Return(false, nil)
-	m.On("EthernetStatus").Return(network.Disconnected, nil)
+	m.On("SystemStatus").Return(watchdog.SystemStatus{
+		WlanStatus:      network.Disconnected,
+		EthernetStatus:  network.Disconnected,
+		PodAgentRunning: false,
+	}, nil)
 	c := &config.Config{}
 	c.ClientId = "1234"
-	scanner := NewScanner(m)
+	scanner := watchdog.NewScanner(m)
 	go scanner.ScanSystem(c)
 	select {
 	case evt := <-scanner.Events():
-		if evt.EventType != SystemFileChanged {
-			t.Fatalf("expected a SystemFileChanged event")
+		if evt.EventType != watchdog.SystemFileChanged {
+			t.Fatalf("expected a watchdog.SystemFileChanged event")
 		}
 	case <-time.NewTimer(1 * time.Second).C:
 		t.Fatalf("expected an event")
@@ -163,22 +176,25 @@ func TestScanSystemCMDLineDiffers(t *testing.T) {
 	m.On("GetRadarAgentServiceFile").Return(mustReadFile("osfiles/etc/systemd/system/radar_agent.service"), nil)
 	m.On("GetAuthLogFile").Return([]byte(""), nil)
 	m.On("GetSysTimezone").Return(utc, nil)
-	m.On("SetCMDLine", []byte(strings.Join(cmdLineCommands, " "))).Return(nil)
+	m.On("SetCMDLine", []byte("quiet splash rootwait logo.nologo vt.global_cursor_default=0 loglevel=0")).Return(nil)
 	m.On("EnsureTailscale").Return(nil)
 	m.On("EnsureBinaryPermissions", "/opt/radar/watchdog").Return(nil)
 	m.On("EnsurePathPermissions", "/tmp/tracing_buffer", os.FileMode(0777)).Return(nil)
 	m.On("EnsureUserGroups", "radar", []string{"netdev"}).Return(false, nil)
 	m.On("EnsureWifiEnabled").Return(nil)
-	m.On("PodAgentRunning").Return(false, nil)
-	m.On("EthernetStatus").Return(network.Disconnected, nil)
+	m.On("SystemStatus").Return(watchdog.SystemStatus{
+		WlanStatus:      network.Disconnected,
+		EthernetStatus:  network.Disconnected,
+		PodAgentRunning: false,
+	}, nil)
 	c := &config.Config{}
 	c.ClientId = "1234"
-	scanner := NewScanner(m)
+	scanner := watchdog.NewScanner(m)
 	go scanner.ScanSystem(c)
 	select {
 	case evt := <-scanner.Events():
-		if evt.EventType != SystemFileChanged {
-			t.Fatalf("expected a SystemFileChanged event")
+		if evt.EventType != watchdog.SystemFileChanged {
+			t.Fatalf("expected a watchdog.SystemFileChanged event")
 		}
 	case <-time.NewTimer(1 * time.Second).C:
 		t.Fatalf("expected an event")
@@ -202,16 +218,19 @@ func TestScanSystemLogindDiffers(t *testing.T) {
 	m.On("EnsurePathPermissions", "/tmp/tracing_buffer", os.FileMode(0777)).Return(nil)
 	m.On("EnsureUserGroups", "radar", []string{"netdev"}).Return(false, nil)
 	m.On("EnsureWifiEnabled").Return(nil)
-	m.On("PodAgentRunning").Return(false, nil)
-	m.On("EthernetStatus").Return(network.Disconnected, nil)
+	m.On("SystemStatus").Return(watchdog.SystemStatus{
+		WlanStatus:      network.Disconnected,
+		EthernetStatus:  network.Disconnected,
+		PodAgentRunning: false,
+	}, nil)
 	c := &config.Config{}
 	c.ClientId = "1234"
-	scanner := NewScanner(m)
+	scanner := watchdog.NewScanner(m)
 	go scanner.ScanSystem(c)
 	select {
 	case evt := <-scanner.Events():
-		if evt.EventType != SystemFileChanged {
-			t.Fatalf("expected a SystemFileChanged event")
+		if evt.EventType != watchdog.SystemFileChanged {
+			t.Fatalf("expected a watchdog.SystemFileChanged event")
 		}
 	case <-time.NewTimer(1 * time.Second).C:
 		t.Fatalf("expected an event")
@@ -236,16 +255,19 @@ func TestScanSystemMultipleChanged(t *testing.T) {
 	m.On("EnsurePathPermissions", "/tmp/tracing_buffer", os.FileMode(0777)).Return(nil)
 	m.On("EnsureUserGroups", "radar", []string{"netdev"}).Return(false, nil)
 	m.On("EnsureWifiEnabled").Return(nil)
-	m.On("PodAgentRunning").Return(false, nil)
-	m.On("EthernetStatus").Return(network.Disconnected, nil)
+	m.On("SystemStatus").Return(watchdog.SystemStatus{
+		WlanStatus:      network.Disconnected,
+		EthernetStatus:  network.Disconnected,
+		PodAgentRunning: false,
+	}, nil)
 	c := &config.Config{}
 	c.ClientId = "1234"
-	scanner := NewScanner(m)
+	scanner := watchdog.NewScanner(m)
 	go scanner.ScanSystem(c)
 	select {
 	case evt := <-scanner.Events():
-		if evt.EventType != SystemFileChanged {
-			t.Fatalf("expected a SystemFileChanged event")
+		if evt.EventType != watchdog.SystemFileChanged {
+			t.Fatalf("expected a watchdog.SystemFileChanged event, got %v", evt.EventType)
 		}
 	case <-time.NewTimer(1 * time.Second).C:
 		t.Fatalf("expected an event")
@@ -269,16 +291,19 @@ func TestScanSystemTimeZoneChanged(t *testing.T) {
 	m.On("EnsurePathPermissions", "/tmp/tracing_buffer", os.FileMode(0777)).Return(nil)
 	m.On("EnsureUserGroups", "radar", []string{"netdev"}).Return(false, nil)
 	m.On("EnsureWifiEnabled").Return(nil)
-	m.On("PodAgentRunning").Return(false, nil)
-	m.On("EthernetStatus").Return(network.Disconnected, nil)
+	m.On("SystemStatus").Return(watchdog.SystemStatus{
+		WlanStatus:      network.Disconnected,
+		EthernetStatus:  network.Disconnected,
+		PodAgentRunning: false,
+	}, nil)
 	c := &config.Config{}
 	c.ClientId = "1234"
-	scanner := NewScanner(m)
+	scanner := watchdog.NewScanner(m)
 	go scanner.ScanSystem(c)
 	select {
 	case evt := <-scanner.Events():
-		if evt.EventType != SystemFileChanged {
-			t.Fatalf("expected a SystemFileChanged event")
+		if evt.EventType != watchdog.SystemFileChanged {
+			t.Fatalf("expected a watchdog.SystemFileChanged event")
 		}
 	case <-time.NewTimer(1 * time.Second).C:
 		t.Fatalf("expected an event")
@@ -301,15 +326,61 @@ func TestScanSystemTimeZoneReturnedNilNoChange(t *testing.T) {
 	m.On("EnsurePathPermissions", "/tmp/tracing_buffer", os.FileMode(0777)).Return(nil)
 	m.On("EnsureUserGroups", "radar", []string{"netdev"}).Return(false, nil)
 	m.On("EnsureWifiEnabled").Return(nil)
-	m.On("PodAgentRunning").Return(false, nil)
-	m.On("EthernetStatus").Return(network.Disconnected, nil)
+	m.On("SystemStatus").Return(watchdog.SystemStatus{
+		WlanStatus:      network.Disconnected,
+		EthernetStatus:  network.Disconnected,
+		PodAgentRunning: false,
+	}, nil)
 	c := &config.Config{}
 	c.ClientId = "1234"
-	scanner := NewScanner(m)
+	scanner := watchdog.NewScanner(m)
 	go scanner.ScanSystem(c)
 	select {
 	case evt := <-scanner.Events():
 		t.Fatalf("unexpected event: %v", evt)
 	case <-time.NewTimer(1 * time.Second).C:
+	}
+}
+
+func TestScanSystemStatusChanged(t *testing.T) {
+	m := mocks.NewMockSystemManager(t)
+	m.On("GetHostname").Return("1234", nil)
+	m.On("GetRCLocal").Return(mustReadFile("osfiles/etc/rc.local"), nil)
+	m.On("GetBootConfig").Return(mustReadFile("osfiles/boot/config.txt"), nil)
+	m.On("GetCMDLine").Return(mustReadFile("osfiles/boot/cmdline.txt"), nil)
+	m.On("GetLogindConf").Return(mustReadFile("osfiles/etc/systemd/logind.conf"), nil)
+	m.On("GetWatchdogServiceFile").Return(mustReadFile("osfiles/etc/systemd/system/podwatchdog@.service"), nil)
+	m.On("GetRadarAgentServiceFile").Return(mustReadFile("osfiles/etc/systemd/system/radar_agent.service"), nil)
+	m.On("GetSysTimezone").Return(nil, nil)
+	m.On("GetAuthLogFile").Return([]byte(""), nil)
+	m.On("EnsureTailscale").Return(nil)
+	m.On("EnsureBinaryPermissions", "/opt/radar/watchdog").Return(nil)
+	m.On("EnsurePathPermissions", "/tmp/tracing_buffer", os.FileMode(0777)).Return(nil)
+	m.On("EnsureUserGroups", "radar", []string{"netdev"}).Return(false, nil)
+	m.On("EnsureWifiEnabled").Return(nil)
+	m.On("SystemStatus").Return(watchdog.SystemStatus{
+		WlanStatus:      network.ConnectedNoInternet,
+		EthernetStatus:  network.ConnectedWithInternet,
+		PodAgentRunning: false,
+	}, nil)
+	c := &config.Config{}
+	c.ClientId = "1234"
+	scanner := watchdog.NewScanner(m)
+	go scanner.ScanSystem(c)
+	select {
+	case evt := <-scanner.Events():
+		if evt.EventType != watchdog.ConnectionStatusChanged {
+			t.Fatalf("expected ConnectionStatusChanged, got %v", evt.EventType)
+		}
+		if data, ok := evt.Data.(map[string]network.NetStatus); !ok {
+			t.Fatalf("expected event.Data to be of type watchdog.ConnectionsStatus, got %T", evt.Data)
+		} else if data["ethernet"] != network.ConnectedWithInternet {
+			t.Fatalf("expected Ethernet to be %v, got: %v", network.ConnectedWithInternet, data["ethernet"])
+		} else if data["wlan"] != network.ConnectedNoInternet {
+			t.Fatalf("expected Wlan.Status to be %v, got: %v", network.ConnectedNoInternet, data["wlan"])
+		}
+
+	case <-time.NewTimer(1 * time.Second).C:
+		t.Fatalf("expected an event")
 	}
 }
