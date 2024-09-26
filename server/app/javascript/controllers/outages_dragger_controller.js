6a9c16e3f1fe8568f30e1d70a6c548e518bd11b5
iuke <edamm@itba.edu.ar>
2024-09-26 15:51:51 +0200

    quick debug removal

--- server/app/javascript/controllers/outages_dragger_controller.js
+++ server/app/javascript/controllers/outages_dragger_controller.js
@@ -25,7 +25,6 @@ export default class extends Controller {
   }
   
   handleScroll(e) {
-    console.log('scrolling...')
     this.hideEverything();
     this.isDragging = false;
     this.dragEndX = null;
