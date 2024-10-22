0850d4dbab3f4b94c37aa2fdc37b0b98d5f68093
ignaciograsso <ignacio@exactlylabs.com>
2024-10-22 14:53:16 -0300

    CR changes

--- server/app/controllers/concerns/range_evaluator.rb
+++ server/app/controllers/concerns/range_evaluator.rb
@@ -1,5 +1,9 @@
 module RangeEvaluator
   extend ActiveSupport::Concern
+  LAST_24_HOURS = 'last_24_hours'.freeze
+  LAST_7_DAYS = 'last_7_days'
+  LAST_30_DAYS = 'last_30_days'
+  THIS_YEAR = 'this_year'
 
   def human_filter_to_range(range)
     case range
@@ -16,34 +20,34 @@ module RangeEvaluator
     end
   end
 
-  def get_range_start_date(type = 'last_24_hours')
+  def get_range_start_date(type = LAST_24_HOURS)
     case type
-    when 'last_24_hours'
+    when LAST_24_HOURS
       Time.zone.now - 24.hours
-    when 'last_7_days'
+    when LAST_7_DAYS
       Time.zone.now - 7.days
-    when 'last_30_days'
+    when LAST_30_DAYS
       Time.zone.now - 30.days
-    when 'this_year'
+    when THIS_YEAR
       Time.zone.now.beginning_of_year
     else
       Time.zone.now - 24.hours
     end
   end
 
-  def get_current_and_previous_period(period = 'last_24_hours')
+  def get_current_and_previous_period(period = LAST_24_HOURS)
     current_period_end_date = Time.zone.now
     case period
-    when 'last_24_hours'
+    when LAST_24_HOURS
       curr_diff = 24.hours
       prev_diff = curr_diff
-    when 'last_7_days'
+    when LAST_7_DAYS
       curr_diff = 7.days
       prev_diff = curr_diff
-    when 'last_30_days'
+    when LAST_30_DAYS
       curr_diff = 30.days
       prev_diff = curr_diff
-    when 'this_year'
+    when THIS_YEAR
       curr_diff = Time.zone.now.yday.days
       prev_diff = 1.year
     else
