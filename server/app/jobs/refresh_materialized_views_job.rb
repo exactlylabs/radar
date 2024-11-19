a4feb19503e8a434e012ccbad51fcef742db61fe
Rafael Mendes <rafael@exactlylabs.com>
2024-11-19 11:10:12 -0300

    refresh materialized views running concurrently

--- server/app/jobs/refresh_materialized_views_job.rb
+++ server/app/jobs/refresh_materialized_views_job.rb
@@ -2,9 +2,9 @@ class RefreshMaterializedViewsJob < ApplicationJob
   queue_as :default
 
   def perform()
-    ActiveRecord::Base.connection.execute("REFRESH MATERIALIZED VIEW aggregated_measurements_by_hours")
-    ActiveRecord::Base.connection.execute("REFRESH MATERIALIZED VIEW aggregated_measurements_by_days")
-    ActiveRecord::Base.connection.execute("REFRESH MATERIALIZED VIEW aggregated_pod_measurements_by_hours")
-    ActiveRecord::Base.connection.execute("REFRESH MATERIALIZED VIEW aggregated_pod_measurements_by_days")
+    ActiveRecord::Base.connection.execute("REFRESH MATERIALIZED VIEW CONCURRENTLY aggregated_measurements_by_hours")
+    ActiveRecord::Base.connection.execute("REFRESH MATERIALIZED VIEW CONCURRENTLY aggregated_measurements_by_days")
+    ActiveRecord::Base.connection.execute("REFRESH MATERIALIZED VIEW CONCURRENTLY aggregated_pod_measurements_by_hours")
+    ActiveRecord::Base.connection.execute("REFRESH MATERIALIZED VIEW CONCURRENTLY aggregated_pod_measurements_by_days")
   end
 end
