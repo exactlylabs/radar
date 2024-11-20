816356e0b7f9a3f01d9cf2e205a77db4b61d113a
Rafael Mendes <rafael@exactlylabs.com>
2024-11-20 13:38:37 -0300

    client speed test and mobile scan network models including vector tileable

--- server/app/models/client_speed_test.rb
+++ server/app/models/client_speed_test.rb
@@ -1,10 +1,18 @@
 class ClientSpeedTest < ApplicationRecord
+  include VectorTileable
+
   has_one_attached :result
   belongs_to :widget_client, foreign_key: 'tested_by'
   belongs_to :autonomous_system, optional: true
-  belongs_to :user, optional: true
+  belongs_to :mobile_scan_session, optional: true
+  belongs_to :mobile_user_device, optional: true
 
   before_create :fill_geo_fields
+
+  scope :within_box, -> (x_0, y_0, x_1, y_1) {
+    where("lonlat::geometry && ST_MakeEnvelope(?, ?, ?, ?, 4326)", x_0, y_0, x_1, y_1)
+  }
+  scope :from_user, -> (user) { joins(:mobile_user_device).where(mobile_user_device: {user_id: user.id})}
 
   def read_result
     if self.result.attached?
--- server/app/models/mobile/mobile_scan_network.rb
+++ server/app/models/mobile/mobile_scan_network.rb
@@ -1,12 +1,23 @@
 class MobileScanNetwork < ApplicationRecord
+  include VectorTileable
+
   belongs_to :found_by_session, class_name: "MobileScanSession"
 
   has_many :mobile_scan_session_networks
   has_many :mobile_scan_sessions, through: :mobile_scan_session_networks
 
   enum network_type: {cell: "cell", wifi: "wifi"}
+  scope :within_box, -> (x_0, y_0, x_1, y_1) {
+    where("lonlat::geometry && ST_MakeEnvelope(?, ?, ?, ?, 4326)", x_0, y_0, x_1, y_1)
+  }
 
   SECURITIES_REGEXP = /WEP|WPA/
+
+  def self.from_user(user)
+    joins(:mobile_scan_sessions => :mobile_user_device)
+    .where(mobile_user_device: {user_id: user.id})
+    .group("mobile_scan_networks.id")
+  end
 
   def self.security_from_capabilities(capabilities)
     security_type = case capabilities
@@ -31,6 +42,6 @@ class MobileScanNetwork < ApplicationRecord
         ""
       end
 
-    "#{security_type}-#{network_type}"
+    network_type.present? ? "#{security_type}-#{network_type}" : security_type
   end
 end
