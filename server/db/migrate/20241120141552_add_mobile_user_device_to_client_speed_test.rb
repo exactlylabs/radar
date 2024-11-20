e59a5788fe94514219ba28f81502e4cc5528c620
Rafael Mendes <rafael@exactlylabs.com>
2024-11-20 13:59:08 -0300

    removed unnecessary migration

--- server/db/migrate/20241120141552_add_mobile_user_device_to_client_speed_test.rb
+++
@@ -1,6 +0,0 @@
-class AddMobileUserDeviceToClientSpeedTest < ActiveRecord::Migration[6.1]
-  def change
-    remove_reference :client_speed_tests, :user
-    add_reference :client_speed_tests, :mobile_user_device, foreign_key: true
-  end
-end
