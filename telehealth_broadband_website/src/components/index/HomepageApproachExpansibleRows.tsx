e084e0ffb909914dd62b708dc92d183d6a86a083
iuke <edamm@itba.edu.ar>
2024-12-02 18:21:44 +0100

    evaluation results findings subpage

--- telehealth_broadband_website/src/components/index/HomepageApproachExpansibleRows.tsx
+++ telehealth_broadband_website/src/components/index/HomepageApproachExpansibleRows.tsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
-import styles from './homepage_approach_expansible_rows.module.css';
-import {ExpansibleRow} from "../common/ExpansibleRows.tsx";
+import styles from './styles/homepage_approach_expansible_rows.module.css';
+import {ExpansibleRow} from "../common/wrappers/ExpansibleRows.tsx";
 
 export default function HomepageApproachExpansibleRows() {
   
