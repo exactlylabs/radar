487d426f53960a1d9359e57a3db5ac10e03f8755
MatheusBrunoAlmeida <manancialsup@hotmail.com>
2024-12-09 09:11:45 -0300

    add autentication http file

--- speedtest_mobile/client_mobile_app_v2/speedtest_v2/app/http/authentication.ts
+++ speedtest_mobile/client_mobile_app_v2/speedtest_v2/app/http/authentication.ts
@@ -8,14 +8,13 @@ interface GetTokenResponse {
     token: string;
 }
 
-
-export const sendCode = async (email: string): Promise<SendCodeResponse> => {
+export const sendCode = async (email: string) => {
     return await api.post('/authenticate/new_code', {
         email: email
     })
 }
 
-export const getToken = async (code: string): Promise<GetTokenResponse> => {
+export const getToken = async (code: string) => {
     return await api.post('/authenticate/get_token', {
         code: code
     })
