75382586c5e1353098868ece309081b620bb39d7
MatheusBrunoAlmeida <manancialsup@hotmail.com>
2024-12-09 10:03:58 -0300

    feature add settings page

--- speedtest_mobile/client_mobile_app_v2/speedtest_v2/app/index.tsx
+++ speedtest_mobile/client_mobile_app_v2/speedtest_v2/app/index.tsx
@@ -3,4 +3,5 @@ import { Redirect } from 'expo-router';
 export default function Index() {
   // return <Redirect href="/(onboarding)/step1" />;
   return <Redirect href="/(settings)" />;
+  return <Redirect href="/(settings)" />;
 }
