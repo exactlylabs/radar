c88346fc8041057218083343ce24ac9a658082bb
MatheusBrunoAlmeida <manancialsup@hotmail.com>
2024-12-09 14:25:02 -0300

    removing the interfaces

--- speedtest_mobile/client_mobile_app_v2/speedtest_v2/app/http/authentication.ts
+++ speedtest_mobile/client_mobile_app_v2/speedtest_v2/app/http/authentication.ts
@@ -1,13 +1,5 @@
 import { api } from "@/src/api/api"
 
-interface SendCodeResponse {
-    ok: boolean;
-}
-
-interface GetTokenResponse {
-    token: string;
-}
-
 export const sendCode = async (email: string) => {
     return await api.post('/authenticate/new_code', {
         email: email
