4204197d4a022cf6248ae1c4517e41a530ad9d66
iuke <edamm@itba.edu.ar>
2024-12-13 12:22:57 +0100

    desktop homepage fixes from design review

--- telehealth_broadband_website/src/components/common/wrappers/ExpansibleRows.tsx
+++ telehealth_broadband_website/src/components/common/wrappers/ExpansibleRows.tsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 import styles from './styles/expansible_rows.module.css';
 import type {CardContent} from "../types.ts";
 import plusIcon from "../../../assets/icons/plus.svg";
+import minusIcon from "../../../assets/icons/minus.svg";
 
 interface ExpansibleRowsProps {
   content: CardContent[];
@@ -23,7 +24,19 @@ export function ExpansibleRow({content, isOpen, index, toggleOpen}: ExpansibleRo
         { content.head && <span className={styles.head}>{content.head}</span> }
         <h4 className={styles.title}>{content.title}</h4>
         <button className={styles.button}>
-          <img src={plusIcon.src} width={16} height={16} alt={'plus icon'}/>
+          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="16" height="16" data-type="plus">
+            <path fill="white" transform="translate(1 7.25)"
+                  d="M0.75 0L13.250252 0C13.664466 -7.6089797e-17 14.000252 0.33578643 14.000252 0.75C14.000252 1.1642135 13.664466 1.5 13.250252 1.5L0.75 1.5C0.33578643 1.5 2.5363265e-17 1.1642135 0 0.75C-5.072653e-17 0.33578643 0.33578643 5.072653e-17 0.75 0Z"
+                  fillRule="evenodd"/>
+            <path fill="white" transform="matrix(-4.37114e-08 1 -1 -4.37114e-08 8.75013 0.999874)"
+                  d="M0.75 0L13.250252 0C13.664466 -7.6089797e-17 14.000252 0.33578643 14.000252 0.75C14.000252 1.1642135 13.664466 1.5 13.250252 1.5L0.75 1.5C0.33578643 1.5 2.5363265e-17 1.1642135 0 0.75C-5.072653e-17 0.33578643 0.33578643 5.072653e-17 0.75 0Z"
+                  fillRule="evenodd"/>
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="16" height="16" data-type="minus">
+            <path fill="white" transform="translate(1 7.25)"
+                  d="M0.75 0L13.250252 0C13.664466 0 14.000252 0.33578643 14.000252 0.75C14.000252 1.1642135 13.664466 1.5 13.250252 1.5L0.75 1.5C0.33578643 1.5 0 1.1642135 0 0.75C0 0.33578643 0.33578643 0 0.75 0Z"
+                  fillRule="evenodd"/>
+          </svg>
         </button>
       </div>
       <div className={styles.contentContainer} data-open={isOpen.toString()}>
@@ -36,7 +49,7 @@ export function ExpansibleRow({content, isOpen, index, toggleOpen}: ExpansibleRo
 export default function ExpansibleRows({content}: ExpansibleRowsProps) {
   
   const [currentOpenIndex, setCurrentOpenIndex] = useState<number | null>(null);
-
+  
   const handleToggleOpen = (index: number) => {
     setCurrentOpenIndex(currentOpenIndex === index ? null : index);
   }
