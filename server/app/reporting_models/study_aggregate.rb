317bcd8c1ea0caf40330b17435e5722e1c1ce0b1
Rafael Mendes <rafael@exactlylabs.com>
2026-09-21 19:26:11 -0300

    adds StudyAggregate.find_or_create_for! keyed by study, level, shape and isp

--- server/app/reporting_models/study_aggregate.rb
+++ server/app/reporting_models/study_aggregate.rb
@@ -9,53 +9,21 @@ class StudyAggregate < ActiveRecord::Base
 
   scope :having_location_id, ->(location_id) { joins(:geospace => :locations).where("locations.id = ?", location_id) }
 
+  def self.isp_county_name(org_name, county_name)
+    "#{org_name} -> #{county_name}"
+  end
 
-  def self.populate_from_geospaces!
-    fips = Geospace::STUDY_COUNTIES_FIPS
-    state_fips = Geospace::STUDY_STATES_FIPS
-    geospaces = Geospace.arel_table
-    Geospace.where(geoid: state_fips).each do |state|
-      state.update(study_geospace: true)
-      state_agg = StudyAggregate.find_or_create_by!(
-        name: state.name,
-        geospace: state,
-        level: 'state'
-      )
-      # Do it outside find_or_create_by, because it could have been already created
-      state_agg.update(study_aggregate: true)
-
-      Geospace.where(geoid: fips, namespace: 'county').where(geospaces[:geom].st_intersects(state.geom)).each do |county|
-        county.update(study_geospace: true)
-        county_agg = StudyAggregate.find_or_create_by!(
-          name: county.name,
-          geospace: county,
-          level: 'county',
-          parent_aggregate: state_agg
-        )
-        county_agg.update(study_aggregate: true)
-        Geospace.where(namespace: 'census_place').where(geospaces[:geom].st_intersects(county.geom)).each do |place|
-          place.update(study_geospace: true)
-          place_agg = StudyAggregate.find_or_create_by!(
-            name: place.name,
-            geospace: place,
-            level: 'census_place',
-            parent_aggregate: county_agg
-          )
-          place_agg.update(study_aggregate: true)
-        end
-        GeoTools.get_county_as_orgs(county.geoid).each do |org|
-          as_org = AutonomousSystemOrg.find_or_create_by!(name: org.name)
-          isp_county_agg = StudyAggregate.find_or_create_by!(
-            name: "#{org.name} -> #{county.name}",
-            autonomous_system_org_id: as_org.id,
-            geospace_id: county.id,
-            level: 'isp_county',
-            parent_aggregate: state_agg
-          )
-          isp_county_agg.update(study_aggregate: true)
-        end
-      end
-    end
+  # The row identity is (study, level, shape, isp). Everything else is updated in place,
+  # so tagging a shape into a study later flips the existing row instead of adding one.
+  def self.find_or_create_for!(study:, level:, geospace_id:, name:, parent:, study_shape:, autonomous_system_org_id: nil)
+    aggregate = find_or_initialize_by(
+      study_id: study.id, level: level, geospace_id: geospace_id, autonomous_system_org_id: autonomous_system_org_id
+    )
+    aggregate.name = name
+    aggregate.parent_aggregate = parent
+    aggregate.study_aggregate = study_shape
+    aggregate.save! if aggregate.new_record? || aggregate.changed?
+    aggregate
   end
 end
 
---
+++ server/test/reporting_models/study_aggregate_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+
+class StudyAggregateTest < ActiveSupport::TestCase
+  test "find_or_create_for! creates a row keyed by study, level, shape and isp" do
+    fresno = studies(:fresno)
+    county = geospaces(:fresno_county)
+
+    row = nil
+    assert_difference 'StudyAggregate.count', 1 do
+      row = StudyAggregate.find_or_create_for!(
+        study: fresno, level: 'county', geospace_id: county.id, name: county.name, parent: nil, study_shape: true
+      )
+    end
+
+    assert_equal fresno, row.study
+    assert_equal 'county', row.level
+    assert_equal county, row.geospace
+    assert_equal "Fresno County", row.name
+    assert row.study_aggregate
+    assert_nil row.autonomous_system_org_id
+  end
+
+  test "find_or_create_for! updates the flag and parent of an existing row instead of creating a second one" do
+    fresno = studies(:fresno)
+    state = geospaces(:fresno_state)
+    county = geospaces(:fresno_other_county)
+    state_row = StudyAggregate.find_or_create_for!(study: fresno, level: 'state', geospace_id: state.id, name: state.name, parent: nil, study_shape: true)
+    first = StudyAggregate.find_or_create_for!(study: fresno, level: 'county', geospace_id: county.id, name: county.name, parent: state_row, study_shape: false)
+
+    assert_no_difference 'StudyAggregate.count' do
+      StudyAggregate.find_or_create_for!(study: fresno, level: 'county', geospace_id: county.id, name: county.name, parent: state_row, study_shape: true)
+    end
+
+    assert first.reload.study_aggregate
+    assert_equal state_row, first.parent_aggregate
+  end
+
+  test "isp rows are distinct per isp and named after the org" do
+    fresno = studies(:fresno)
+    county = geospaces(:fresno_county)
+    org1 = autonomous_system_orgs(:as_org1)
+    org2 = autonomous_system_orgs(:as_org2)
+
+    assert_difference 'StudyAggregate.count', 2 do
+      [org1, org2].each do |org|
+        StudyAggregate.find_or_create_for!(
+          study: fresno, level: 'isp_county', geospace_id: county.id,
+          name: StudyAggregate.isp_county_name(org.name, county.name),
+          parent: nil, study_shape: true, autonomous_system_org_id: org.id
+        )
+      end
+    end
+
+    row = StudyAggregate.find_by!(study: fresno, level: 'isp_county', geospace_id: county.id, autonomous_system_org_id: org1.id)
+    assert_equal "Test Org 1 -> Fresno County", row.name
+  end
+end
